f0531e471485d2a97b608713f88d7794f9fa294b
Cyril Galibern <cyril.galibern@free.fr>
2009-10-14 01:46:03 +0200

    Add lib/svcZone.py module Help on class SvcZone in module __main__:
    class SvcZone(svc.Svc)
     |  Define Zone services
     |
     |  Method resolution order:
     |      SvcZone
     |      svc.Svc
     |      resources.Resource
     |      __builtin__.object
     |
     |  Methods defined here:
     |
     |  __init__(self, optional=False, disabled=False)
     |
     |  action(self, action=None)
     |
     |  start(self)
     |      start a zone
     |      check ping
     |      zone ready
     |      start ips
     |      start zone
     |      start VGs
     |      start mounts
     |      start apps
     |
     |  stop(self)
     |      stop a zone:
     |      stop apps
     |      stop mounts
     |      stop VGs
     |      stop zone
     |      stop ips
     |
     |  ----------------------------------------------------------------------
     |  Methods inherited from svc.Svc:
     |
     |  __iadd__(self, r)
     |      svc+=aResourceSet
     |      svc+=aResource
     |
     |  __str__(self)
     |
     |  get_res_sets(self, type)
     |
     |  status(self)
     |      status a service:
     |      status mounts
     |      status VGs
     |      status ips
     |
     |  ----------------------------------------------------------------------
     |  Methods inherited from resources.Resource:
     |
     |  disable(self)
     |
     |  do_action(self, action)
     |      Every resource should define basic doAction: start() stop() status()
     |
     |  enable(self)
     |
     |  is_disabled(self)
     |
     |  is_optional(self)
     |
     |  set_optional(self)
     |
     |  unset_optional(self)
     |
     |  ----------------------------------------------------------------------
     |  Data descriptors inherited from resources.Resource:
     |
     |  __dict__
     |      dictionary for instance variables (if defined)
     |
     |  __weakref__
     |      list of weak references to the object (if defined)
    
        Z=SvcZone()
        Z+=mount.Mount("/mnt1","/dev/sda")
        Z+=mount.Mount("/mnt2","/dev/sdb")
        Z+=ip.Ip("eth0","192.168.0.173")
        Z+=ip.Ip("eth0","192.168.0.174")
    
    Show Z:  Service object=SvcZone type=Zone available resources: ip Mount
      [resSet object=ResourceSet type=Mount [ (object=Mount type=Mount mnt=/mnt1 dev=/dev/sda) (object=Mount type=Mount mnt=/mnt2 dev=/dev/sdb)]]  [resSet object=ResourceSet type=ip [ (object=Ip type=ip ipdev=eth0 adress=192.168.0.173) (object=Ip type=ip ipdev=eth0 adress=192.168.0.174)]]
    start Z:
    starting SvcZone
    Calling action check_ping on ResourceSet
    Action check_ping on object=Ip type=ip ipdev=eth0 adress=192.168.0.173
    call do_action on Ip
    Action check_ping on object=Ip type=ip ipdev=eth0 adress=192.168.0.174
    call do_action on Ip
    Calling action start on ResourceSet
    Action start on object=Ip type=ip ipdev=eth0 adress=192.168.0.173
    ====== exec ifconfig eth0 plumb
    Action start on object=Ip type=ip ipdev=eth0 adress=192.168.0.174
    ====== exec ifconfig eth0 plumb
    Calling action start on ResourceSet
    Action start on object=Mount type=Mount mnt=/mnt1 dev=/dev/sda
    ===== exec mount -F None /dev/sda /mnt1
    Action start on object=Mount type=Mount mnt=/mnt2 dev=/dev/sdb
    ===== exec mount -F None /dev/sdb /mnt2
    stop Z:
    stopping SvcZone
    Calling action stop on ResourceSet
    Action stop on object=Mount type=Mount mnt=/mnt1 dev=/dev/sda
    ====== exec fuser -ck /dev/sda
    ====== exec umount /mnt1
    Action stop on object=Mount type=Mount mnt=/mnt2 dev=/dev/sdb
    ====== exec fuser -ck /dev/sdb
    ====== exec umount /mnt2
    Calling action stop on ResourceSet
    Action stop on object=Ip type=ip ipdev=eth0 adress=192.168.0.173
    ====== exec ifconfig eth0 unplumb
    Action stop on object=Ip type=ip ipdev=eth0 adress=192.168.0.174
    ====== exec ifconfig eth0 unplumb

---
+++ lib/svcZone.py
@@ -0,0 +1,95 @@
+#
+# Copyright (c) 2009 Christophe Varoqui <christophe.varoqui@free.fr>'
+# Copyright (c) 2009 Cyril Galibern <cyril.galibern@free.fr>'
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="cgaliber"
+__date__ ="$11 oct. 2009 21:56:59$"
+
+import svc
+
+class SvcZone(svc.Svc):
+    """ Define Zone services"""
+    def __init__(self,optional=False,disabled=False):
+        svc.Svc.__init__(self,"Zone",optional, disabled)
+    def action(self,action=None):
+        print "Calling action %s on %s" % (action,self.__class__.__name__)
+        if action == "start" : self.start()
+        if action == "stop" : self.start()
+
+    def start(self):
+        """start a zone
+        check ping
+        zone ready
+        start ips
+        start zone
+        start VGs
+        start mounts
+        start apps
+        """
+        print "starting %s" % self.__class__.__name__
+        for ip in self.get_res_sets("ip"):   ip.action("check_ping")
+        for z in self.get_res_sets("zone"):  z.action("ready")
+        for ip in self.get_res_sets("ip"):   ip.action("start")
+        for z in self.get_res_sets("zone"):  z.action("boot")
+        for g in self.get_res_sets("dg"):    g.action("start")
+        for m in self.get_res_sets("Mount"): m.action("start")
+        for a in self.get_res_sets("app"):   a.action("start")
+
+    def stop(self):
+        """stop a zone:
+        stop apps
+        stop mounts
+        stop VGs
+        stop zone
+        stop ips
+        """
+        print "stopping %s" % self.__class__.__name__
+        for a in self.get_res_sets("app"):   a.action("start")
+        for m in self.get_res_sets("Mount"): m.action("stop")
+        for g in self.get_res_sets("dg"):    g.action("stop")
+        for z in self.get_res_sets("zone"):  z.action("stop")
+        for ip in self.get_res_sets("ip"):   ip.action("stop")
+
+if __name__ == "__main__":
+    for c in (SvcZone,) :
+        help(c)
+    import mountSunOS as mount
+    import ipSunOS as ip
+    print """
+    Z=SvcZone()
+    Z+=mount.Mount("/mnt1","/dev/sda")
+    Z+=mount.Mount("/mnt2","/dev/sdb")
+    Z+=ip.Ip("eth0","192.168.0.173")
+    Z+=ip.Ip("eth0","192.168.0.174")
+    """
+
+    Z=SvcZone()
+    Z+=mount.Mount("/mnt1","/dev/sda")
+    Z+=mount.Mount("/mnt2","/dev/sdb")
+    Z+=ip.Ip("eth0","192.168.0.173")
+    Z+=ip.Ip("eth0","192.168.0.174")
+
+    print "Show Z: ", Z
+    print "start Z:"
+    Z.start()
+
+    print "stop Z:"
+    Z.stop()
+
