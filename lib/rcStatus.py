66c7643ba34f91b04b180d89a48c169a8dcfb537
Christophe Varoqui <christophe.varoqui@free.fr>
2009-10-18 12:43:48 +0200

    python style indentation for rcStatus module

--- lib/rcStatus.py
+++ lib/rcStatus.py
@@ -30,54 +30,54 @@ RED = 31
 YELLOW = 33
 
 def _merge(s1, s2):
-	"""Merge too status: WARN and TODO taint UP and DOWN
-	"""
-	if s1 == UNDEF: return s2
-	if (s1, s2) == (UP, UP): return UP
-	if (s1, s2) == (UP, DOWN): return WARN
-	if (s1, s2) == (UP, WARN): return WARN
-	if (s1, s2) == (UP, NA): return UP
-	if (s1, s2) == (UP, TODO): return WARN
-	if (s1, s2) == (DOWN, DOWN): return DOWN
-	if (s1, s2) == (DOWN, WARN): return WARN
-	if (s1, s2) == (DOWN, NA): return DOWN
-	if (s1, s2) == (DOWN, TODO): return WARN
-	if (s1, s2) == (WARN, WARN): return WARN
-	if (s1, s2) == (WARN, NA): return WARN
-	if (s1, s2) == (WARN, TODO): return WARN
-	if (s1, s2) == (NA, NA): return NA
-	if (s1, s2) == (NA, TODO): return WARN
-	if (s1, s2) == (TODO, TODO): return TODO
-	return _merge(s2, s1)
+    """Merge too status: WARN and TODO taint UP and DOWN
+    """
+    if s1 == UNDEF: return s2
+    if (s1, s2) == (UP, UP): return UP
+    if (s1, s2) == (UP, DOWN): return WARN
+    if (s1, s2) == (UP, WARN): return WARN
+    if (s1, s2) == (UP, NA): return UP
+    if (s1, s2) == (UP, TODO): return WARN
+    if (s1, s2) == (DOWN, DOWN): return DOWN
+    if (s1, s2) == (DOWN, WARN): return WARN
+    if (s1, s2) == (DOWN, NA): return DOWN
+    if (s1, s2) == (DOWN, TODO): return WARN
+    if (s1, s2) == (WARN, WARN): return WARN
+    if (s1, s2) == (WARN, NA): return WARN
+    if (s1, s2) == (WARN, TODO): return WARN
+    if (s1, s2) == (NA, NA): return NA
+    if (s1, s2) == (NA, TODO): return WARN
+    if (s1, s2) == (TODO, TODO): return TODO
+    return _merge(s2, s1)
 
 def colorize(color, text):
-	if os.isatty(1):
-		return '\033['+str(color)+'m'+text+'\033[m'
-	else:
-		return text
+    if os.isatty(1):
+        return '\033['+str(color)+'m'+text+'\033[m'
+    else:
+        return text
 
 def status_str(s):
-	if s == UP: return colorize(GREEN, 'UP')
-	if s == DOWN: return colorize(RED, 'DOWN')
-	if s == WARN: return colorize(YELLOW, 'WARN')
-	if s == NA: return 'N/A'
-	if s == TODO: return 'TODO'
-	if s == UNDEF: return 'UNDEF'
+    if s == UP: return colorize(GREEN, 'UP')
+    if s == DOWN: return colorize(RED, 'DOWN')
+    if s == WARN: return colorize(YELLOW, 'WARN')
+    if s == NA: return 'N/A'
+    if s == TODO: return 'TODO'
+    if s == UNDEF: return 'UNDEF'
 
 def print_status(resource, status):
-	import string
-	print '{0:70} {1}'.format(resource, status_str(status))
+    import string
+    print '{0:70} {1}'.format(resource, status_str(status))
 
 class Status(object):
-	"""Class that wraps printing and calculation of resource status
-	"""
-	status = UNDEF
+    """Class that wraps printing and calculation of resource status
+    """
+    status = UNDEF
 
-	def reset(self):
-		self.status = UNDEF
+    def reset(self):
+        self.status = UNDEF
 
-	def __iadd__(self, s):
-		"""Merge a status with current global status
-		"""
-		self.status = _merge(self.status, s)
-                return self
+    def __iadd__(self, s):
+        """Merge a status with current global status
+        """
+        self.status = _merge(self.status, s)
+        return self
