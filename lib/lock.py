d8e6d181a5b7b70e72b8e37f48824af820ac36e3
Christophe Varoqui <christophe.varoqui@free.fr>
2009-12-03 16:23:13 +0100

    Locking fix : set FD_CLOEXEC on the lock file

--- lib/lock.py
+++ lib/lock.py
@@ -8,13 +8,19 @@ def svclock(svc):
     if not os.path.exists(rcEnv.pathlock):
         os.makedirs(rcEnv.pathlock)
     try:
-        svc.lockfd = os.open(lock, os.O_RDWR|os.O_SYNC|os.O_CREAT, 0600)
+        svc.lockfd = os.open(lock, os.O_RDWR|os.O_SYNC|os.O_CREAT, 0644)
     except:
         svc.log.error("can not create lock file %s"%lock)
         raise
 
     try:
+        """ FD_CLOEXEC makes sure the lock is the held by processes
+            we fork from this process
+        """
         fcntl.flock(svc.lockfd, fcntl.LOCK_EX|fcntl.LOCK_NB)
+        flags = fcntl.fcntl(svc.lockfd, fcntl.F_GETFD)
+        flags |= fcntl.FD_CLOEXEC
+        fcntl.fcntl(svc.lockfd, fcntl.F_SETFD, flags)
         os.fsync(svc.lockfd)
         os.write(svc.lockfd, str(os.getpid()))
     except IOError:
