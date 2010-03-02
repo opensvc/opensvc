e33b0b0bb23eb648480db41ca85e35ed8cfff48a
cgaliber <cgaliber@nas.(none)>
2010-03-02 13:57:10 +0100

    remove unused import

--- lib/loopSunOS.py
+++ lib/loopSunOS.py
@@ -16,8 +16,6 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 #
-import os
-import logging
 import resources
 from rcGlobalEnv import *
 import rcStatus
