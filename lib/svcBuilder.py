134747e6600f9ec743e5d201b151519197ae834e
cgaliber <cgaliber@dellopensolaris.(none)>
2010-03-04 11:12:31 +0100

    resApp now use runmethod as prefix to run commands

--- lib/svcBuilder.py
+++ lib/svcBuilder.py
@@ -517,12 +517,12 @@ def add_syncs_rsync(svc, conf):
         svc += r
 
 def add_apps(svc, conf):
-        if svc.svcmode in ['hpvm', 'kvm']:
+        if svc.svcmode in ['hpvm', 'kvm', 'zone']:
             resApp = __import__('resAppVm')
-            r = resApp.Apps(hostname=svc.vmname)
         else:
             resApp = __import__('resApp')
-            r = resApp.Apps()
+
+        r = resApp.Apps(runmethod=svc.runmethod)
         svc += r
 
 def setup_logging():
