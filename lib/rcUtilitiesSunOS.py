585664b0b0ccd43337885e5fb179bb930778ace4
cgaliber <cgaliber@dellopensolaris.(none)>
2010-05-18 04:01:38 +0200

    fix lib/rcUtilitiesSunOS.py

--- lib/rcUtilitiesSunOS.py
+++ lib/rcUtilitiesSunOS.py
@@ -1,7 +1,7 @@
 from rcUtilities import call
 
 def check_ping(addr, timeout=5, count=1):
-    cmd = ['ping', self.addr, "%s" % timeout]
+    cmd = ['ping', addr, "%s" % timeout]
     (ret, out) = call(cmd)
     if ret == 0:
         return True
