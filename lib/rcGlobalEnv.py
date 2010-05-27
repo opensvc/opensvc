53748931c5fa9af8b0a8023dcd45464602d74da2
Christophe Varoqui <christophe.varoqui@opensvc.com>
2010-05-27 18:26:16 +0200

    Test dir existence before usage in postinstall
    Also adapt to Opensolaris /etc/bash/bash_completion.d/ dir

--- lib/rcGlobalEnv.py
+++ lib/rcGlobalEnv.py
@@ -17,6 +17,7 @@
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 #
 import sys
+import os
 
 class rcEnv:
     """Class to store globals
@@ -27,8 +28,12 @@ class rcEnv:
     """program used to execute remote command on other nodes or virtual hosts
     """
     if platform == "sunos5" :
-        rsh = "/usr/bin/ssh -o StrictHostKeyChecking=no -o ForwardX11=no -o BatchMode=yes -n"
-        rcp = "/usr/bin/scp -o StrictHostKeyChecking=no -o ForwardX11=no -o BatchMode=yes -n"
+        if os.path.exists(os.sep, 'usr', 'local', 'bin', 'ssh'):
+            rsh = "/usr/local/bin/ssh -o StrictHostKeyChecking=no -o ForwardX11=no -o BatchMode=yes -o ConnectTimeout=10"
+            rcp = "/usr/local/bin/scp -o StrictHostKeyChecking=no -o ForwardX11=no -o BatchMode=yes -o ConnectTimeout=10"
+        else:
+            rsh = "/usr/bin/ssh -o StrictHostKeyChecking=no -o ForwardX11=no -o BatchMode=yes -n"
+            rcp = "/usr/bin/scp -o StrictHostKeyChecking=no -o ForwardX11=no -o BatchMode=yes -n"
     else :
         rsh = "/usr/bin/ssh -o StrictHostKeyChecking=no -o ForwardX11=no -o BatchMode=yes -o ConnectTimeout=10"
         rcp = "/usr/bin/scp -o StrictHostKeyChecking=no -o ForwardX11=no -o BatchMode=yes -o ConnectTimeout=10"
