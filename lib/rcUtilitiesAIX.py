814a1bf08ef4c994bb58f55c708434b8f95dbe26
Christophe Varoqui <christophe.varoqui@opensvc.com>
2010-06-15 10:11:13 +0200

    AIX support, first pass
    o Postinstall adaptations
    o HostId adaptations
    o Simple init script
    o Ip resources
    o Mount resources

---
+++ lib/rcUtilitiesAIX.py
@@ -0,0 +1,15 @@
+import os
+import re
+from rcUtilities import call
+
+def check_ping(addr, timeout=5, count=1):
+    cmd = ['ping', '-c', repr(count),
+                   '-w', repr(timeout)]
+    if ':' in addr:
+        cmd += ['-a', 'inet6']
+    cmd += [addr]
+    (ret, out) = call(cmd)
+    if ret == 0:
+        return True
+    return False
+
