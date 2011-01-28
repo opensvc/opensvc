adf1e60956ee8e7a13b5cffa8335fa094dc7da00
Christophe Varoqui <christophe.varoqui@opensvc.com>
2011-01-28 13:27:29 +0100

    Add the Linux-HA heartbeat resource
    This one is quite dummy, as heartbeat does not seem to be able
    to report per resource status.
    
    At least we check the daemon status, and we benefit from the
    --cluster safety net. Which is mandatory for the startup script
    not to start the services himself.

---
+++ lib/resHbLinuxHA.py
@@ -0,0 +1,64 @@
+#
+# Copyright (c) 2011 Christophe Varoqui <christophe.varoqui@opensvc.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+import resources as Res
+from rcGlobalEnv import rcEnv
+import os
+import rcStatus
+import rcExceptions as ex
+from rcUtilities import justcall, which
+
+class Hb(Res.Resource):
+    """ HeartBeat ressource
+    """
+    def __init__(self, rid=None, name=None, always_on=set([]),
+                 optional=False, disabled=False, tags=set([])):
+        Res.Resource.__init__(self, rid, "hb.linuxha",
+                              optional=optional, disabled=disabled, tags=tags)
+        self.status_cmd = 'cl_status'
+        self.name = name
+
+    def process_running(self):
+        cmd = [self.status_cmd, 'hbstatus']
+        (out, err, ret) = justcall(cmd)
+        if ret != 0:
+            return False
+        if not 'is running' in out:
+            return False
+        return True
+
+    def __str__(self):
+        return "%s" % (Res.Resource.__str__(self))
+
+    def stop(self):
+        pass
+
+    def start(self):
+        pass
+
+    def _status(self, verbose=False):
+        if not which(self.status_cmd):
+            self.status_log("heartbeat is not installed")
+            return rcStatus.WARN
+        if not self.process_running():
+            self.status_log("heartbeat daemons are not running")
+            return rcStatus.WARN
+        return rcStatus.NA
+
