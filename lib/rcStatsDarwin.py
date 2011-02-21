54e66996895b7ce0a531f55462400844b879e960
Christophe Varoqui <christophe.varoqui@opensvc.com>
2011-02-21 14:54:21 +0100

    Adapt Darwin pushstats for --stats-interval

--- lib/rcStatsDarwin.py
+++ lib/rcStatsDarwin.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python2.6
 #
-# Copyright (c) 2010 Christophe Varoqui <christophe.varoqui@free.fr>'
+# Copyright (c) 2011 Christophe Varoqui <christophe.varoqui@opensvc.com>'
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -18,190 +18,158 @@
 #
 
 import os
-import datetime
 from rcUtilities import call, which
 from rcGlobalEnv import rcEnv
+import rcStats
 
-today = datetime.datetime.today()
-yesterday = today - datetime.timedelta(days=1)
+class StatsProvider(rcStats.StatsProvider):
+    def customfile(self, metric, day):
+        f = os.path.join(rcEnv.pathvar, 'stats', metric+day)
+        if os.path.exists(f):
+            return f
+        return None
 
-def customfile(metric, day):
-    f = os.path.join(rcEnv.pathvar, 'stats', metric+day)
-    if os.path.exists(f):
-        return f
-    return None
+    def cpu(self, d, day, start, end):
+        cols = ['date',
+                'cpu',
+                'usr',
+                'nice',
+                'sys',
+                'idle',
+                'nodename']
+        f = self.sarfile(day)
+        lines = []
+        if f is None:
+            return cols, lines
+        cmd = ['sar', '-u', '-f', f, '-s', start, '-e', end]
+        (ret, buff) = call(cmd, errlog=False)
+        for line in buff.split('\n'):
+            l = line.split()
+            if len(l) != 5:
+                continue
+            if l[1] == '%usr':
+                continue
+            if l[0] == 'Average:':
+                continue
+            (time, usr, nice, sys, idle) = l
+            l = ['%s %s'%(d, time), 'all', usr, nice, sys, idle, self.nodename]
+            lines.append(l)
+        return cols, lines
 
-def sarfile(day):
-    f = os.path.join(os.sep, 'var', 'log', 'sysstat', 'sa'+day)
-    if os.path.exists(f):
-        return f
-    f = os.path.join(os.sep, 'var', 'log', 'sa', 'sa'+day)
-    if os.path.exists(f):
-        return f
-    return None
+    def mem_u(self, d, day, start, end):
+        cols = ['date',
+                'kbmemfree',
+                'kbmemused',
+                'kbbuffers',
+                'kbcached',
+                'kbmemsys',
+                'nodename']
+        fname = self.customfile('mem_u', day)
+        lines = []
+        if fname is None:
+            return cols, lines
+        try:
+            f = open(fname, 'r')
+            buff = f.read()
+            f.close()
+        except:
+            return cols, lines
+        for line in buff.split('\n'):
+            l = line.split()
+            if len(l) != 6:
+                continue
+            (time, free, inactive, active, speculative, wired) = l
+            l = ['%s %s'%(d, time), free, active, speculative, inactive, wired, self.nodename]
+            lines.append(l)
+        return cols, lines
 
-def twodays(fn):
-    if which('sar') is None:
-        return []
-    lines = fn(yesterday)
-    lines += fn(today)
-    return lines
+    def blockdev(self, d, day, start, end):
+        cols = ['date',
+                'dev',
+                'tps',
+                'rsecps',
+                'nodename']
+        f = self.sarfile(day)
+        lines = []
+        if f is None:
+            return cols, lines
+        cmd = ['sar', '-d', '-f', f, '-s', start, '-e', end]
+        (ret, buff) = call(cmd, errlog=False)
+        for line in buff.split('\n'):
+           l = line.split()
+           if len(l) != 4:
+               continue
+           if l[1] == 'device':
+               continue
+           if l[1] == 'Disk:':
+               continue
+           if l[0] == 'Average:':
+               continue
+           l.append(self.nodename)
+           l[0] = '%s %s'%(d, l[0])
+           lines.append(l)
+        return cols, lines
 
-def stats_cpu(file, collect_date=None):
-    return twodays(stats_cpu_day)
+    def netdev(self, d, day, start, end):
+        cols = ['date',
+                'dev',
+                'rxpckps',
+                'rxkBps',
+                'txpckps',
+                'txkBps',
+                'nodename']
 
-def stats_cpu_day(t):
-    d = t.strftime("%Y-%m-%d")
-    day = t.strftime("%d")
-    f = sarfile(day)
-    if f is None:
-        return []
-    cmd = ['sar', '-u', '-f', f]
-    (ret, buff) = call(cmd, errlog=False)
-    lines = []
-    for line in buff.split('\n'):
-        l = line.split()
-        if len(l) != 5:
-            continue
-        if l[1] == '%usr':
-            continue
-        if l[0] == 'Average:':
-            continue
-        (time, usr, nice, sys, idle) = l
-        l = [time, 'all', usr, nice, sys, '0', '0', '0', '0', '0', idle, rcEnv.nodename]
-        l[0] = '%s %s'%(d, l[0])
-        lines.append(l)
-    return lines
-
-def stats_mem_u(file, collect_date=None):
-    return twodays(stats_mem_u_day)
-
-def stats_mem_u_day(t):
-    d = t.strftime("%Y-%m-%d")
-    day = t.strftime("%d")
-    fname = customfile('mem_u', day)
-    if fname is None:
-        return []
-    try:
-        f = open(fname, 'r')
-        buff = f.read()
-        f.close()
-    except:
-        return []
-    lines = []
-    for line in buff.split('\n'):
-        l = line.split()
-        if len(l) != 6:
-            continue
-        (time, free, inactive, active, speculative, wired) = l
-        l = [time, free, active, '0', speculative, inactive, '0', '0', wired, rcEnv.nodename]
-        l[0] = '%s %s'%(d, l[0])
-        lines.append(l)
-    return lines
-
-def stats_proc(file, collect_date=None):
-    return twodays(stats_proc_day)
-
-def stats_proc_day(t):
-    return []
-
-def stats_swap(file, collect_date=None):
-    return twodays(stats_swap_day)
-
-def stats_swap_day(t):
-    return []
-
-def stats_block(file, collect_date=None):
-    return twodays(stats_block_day)
-
-def stats_block_day(t):
-    return []
-
-def stats_blockdev(file, collect_date=None):
-    return twodays(stats_blockdev_day)
-
-def stats_blockdev_day(t):
-    d = t.strftime("%Y-%m-%d")
-    day = t.strftime("%d")
-    f = sarfile(day)
-    if f is None:
-        return []
-    cmd = ['sar', '-d', '-f', f]
-    (ret, buff) = call(cmd, errlog=False)
-    lines = []
-    for line in buff.split('\n'):
-       l = line.split()
-       if len(l) != 4:
-           continue
-       if l[1] == 'device':
-           continue
-       if l[1] == 'Disk:':
-           continue
-       if l[0] == 'Average:':
-           continue
-       l += ['0', '0', '0', '0', '0', '0']
-       l.append(rcEnv.nodename)
-       l[0] = '%s %s'%(d, l[0])
-       lines.append(l)
-    return lines
-
-def stats_netdev(file, collect_date=None):
-    return twodays(stats_netdev_day)
-
-def stats_netdev_day(t):
-    d = t.strftime("%Y-%m-%d")
-    day = t.strftime("%d")
-    f = sarfile(day)
-    if f is None:
-        return []
-    cmd = ['sar', '-n', 'DEV', '-f', f]
-    (ret, buff) = call(cmd, errlog=False)
-    lines = []
-    for line in buff.split('\n'):
-       l = line.split()
-       if len(l) != 6:
-           continue
-       if l[1] in ['IFACE', 'lo0'] :
-           continue
-       if 'dummy' in l[1] or 'vnet' in l[1] or 'veth' in l[1] or \
-          'gif'   in l[1] or 'stf'  in l[1]:
-           continue
-       if l[0] == 'Average:':
-           continue
-       (time, dev, ipckps, ibps, opckps, obps) = l
-       l = [time, dev, ipckps, opckps, ibps, obps, rcEnv.nodename]
-       l[0] = '%s %s'%(d, time)
-       lines.append(l)
-    return lines
+        f = self.sarfile(day)
+        lines = []
+        if f is None:
+            return cols, lines
+        cmd = ['sar', '-n', 'DEV', '-f', f, '-s', start, '-e', end]
+        (ret, buff) = call(cmd, errlog=False)
+        for line in buff.split('\n'):
+           l = line.split()
+           if len(l) != 6:
+               continue
+           if l[1] in ['IFACE', 'lo0'] :
+               continue
+           if 'dummy' in l[1] or 'vnet' in l[1] or 'veth' in l[1] or \
+              'gif'   in l[1] or 'stf'  in l[1]:
+               continue
+           if l[0] == 'Average:':
+               continue
+           l.append(self.nodename)
+           l[0] = '%s %s'%(d, l[0])
+           lines.append(l)
+        return cols, lines
 
 
-def stats_netdev_err(file, collect_date=None):
-    return twodays(stats_netdev_err_day)
-
-def stats_netdev_err_day(t):
-    d = t.strftime("%Y-%m-%d")
-    day = t.strftime("%d")
-    f = sarfile(day)
-    if f is None:
-        return []
-    cmd = ['sar', '-n', 'EDEV', '-f', f]
-    (ret, buff) = call(cmd, errlog=False)
-    lines = []
-    for line in buff.split('\n'):
-       l = line.split()
-       if len(l) != 6:
-           continue
-       if l[1] in ['IFACE', 'lo0'] :
-           continue
-       if 'dummy' in l[1] or 'vnet' in l[1] or 'veth' in l[1] or \
-          'gif'   in l[1] or 'stf'  in l[1]:
-           continue
-       if l[0] == 'Average:':
-           continue
-       (time, dev, ierrps, oerrps, collps, dropps) = l
-       l = [time, dev, ierrps, oerrps, collps, dropps, '0', rcEnv.nodename]
-       l[0] = '%s %s'%(d, time)
-       lines.append(l)
-    return lines
+    def netdev_err(self, d, day, start, end):
+        cols = ['date',
+                'dev',
+                'rxerrps',
+                'txerrps',
+                'collps',
+                'rxdropps',
+                'nodename']
+        f = self.sarfile(day)
+        lines = []
+        if f is None:
+            return cols, lines
+        cmd = ['sar', '-n', 'EDEV', '-f', f, '-s', start, '-e', end]
+        (ret, buff) = call(cmd, errlog=False)
+        for line in buff.split('\n'):
+           l = line.split()
+           if len(l) != 6:
+               continue
+           if l[1] in ['IFACE', 'lo0'] :
+               continue
+           if 'dummy' in l[1] or 'vnet' in l[1] or 'veth' in l[1] or \
+              'gif'   in l[1] or 'stf'  in l[1]:
+               continue
+           if l[0] == 'Average:':
+               continue
+           l.append(self.nodename)
+           l[0] = '%s %s'%(d, l[0])
+           lines.append(l)
+        return cols, lines
 
 
