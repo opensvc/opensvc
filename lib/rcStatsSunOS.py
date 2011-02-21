2558462a5439f607cbf92d961f4163d1470ee33d
Christophe Varoqui <christophe.varoqui@opensvc.com>
2011-02-21 11:05:17 +0100

    Adapt SunOS pushstats to --stats-interval

--- lib/rcStatsSunOS.py
+++ lib/rcStatsSunOS.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python2.6
 #
-# Copyright (c) 2010 Christophe Varoqui <christophe.varoqui@free.fr>'
+# Copyright (c) 2010 Christophe Varoqui <christophe.varoqui@opensvc.com>'
+# Copyright (c) 2011 Christophe Varoqui <christophe.varoqui@opensvc.com>'
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -18,212 +19,172 @@
 #
 
 import os
-import datetime
 from rcUtilities import call, which
-from rcGlobalEnv import rcEnv
+import rcStats
 
-today = datetime.datetime.today()
-yesterday = today - datetime.timedelta(days=1)
+class StatsProvider(rcStats.StatsProvider):
+    def __init__(self, collect_file=None, collect_date=None, interval=2880):
+        rcStats.StatsProvider.__init__(self, collect_file=None,
+                                       collect_date=None, interval=2880)
+        cmd = ['pagesize']
+        (ret, pagesize) = call(cmd)
+        self.pagesize = int(pagesize)
 
-cmd = ['pagesize']
-(ret, pagesize) = call(cmd)
-pagesize = int(pagesize)
+    def sarfile(self, day):
+        f = os.path.join(os.sep, 'var', 'adm', 'sa', 'sa'+day)
+        if os.path.exists(f):
+            return f
+        return None
 
-def sarfile(day):
-    f = os.path.join(os.sep, 'var', 'adm', 'sa', 'sa'+day)
-    if os.path.exists(f):
-        return f
-    return None
+    def cpu(self, d, day, start, end):
+        cols = ['date',
+                'usr',
+                'sys',
+                'iowait',
+                'idle',
+                'cpu',
+                'nodename']
+        f = self.sarfile(day)
+        lines = []
+        if f is None:
+            return cols, lines
+        cmd = ['sar', '-u', '-f', f]
+        (ret, buff) = call(cmd, errlog=False)
+        for line in buff.split('\n'):
+            l = line.split()
+            if len(l) != 5:
+                continue
+            if l[1] == '%usr':
+                continue
+            if l[0] == 'Average':
+                continue
+            l += ['all', self.nodename]
+            l[0] = '%s %s'%(d, l[0])
+            lines.append(l)
+        return cols, lines
 
-def twodays(fn):
-    if which('sar') is None:
-        return []
-    lines = fn(yesterday)
-    lines += fn(today)
-    return lines
+    def mem_u(self, d, day, start, end):
+        cols = ['date',
+                'kbmemfree',
+                'nodename']
+        f = self.sarfile(day)
+        lines = []
+        if f is None:
+            return cols, lines
+        cmd = ['sar', '-r', '-f', f]
+        (ret, buff) = call(cmd)
+        for line in buff.split('\n'):
+            l = line.split()
+            if len(l) != 3:
+                continue
+            if l[1] == 'freemem':
+                continue
+            if l[0] == 'Average':
+                continue
 
-def stats_cpu(file, collect_date=None):
-    return twodays(stats_cpu_day)
+            freemem = int(l[1])*self.pagesize/1024
+            x = ['%s %s'%(d, l[0]), str(freemem), self.nodename]
+            lines.append(x)
+        return cols, lines
 
-def stats_cpu_day(t):
-    d = t.strftime("%Y-%m-%d")
-    day = t.strftime("%d")
-    f = sarfile(day)
-    if f is None:
-        return []
-    cmd = ['sar', '-u', '-f', f]
-    (ret, buff) = call(cmd, errlog=False)
-    lines = []
-    for line in buff.split('\n'):
-        l = line.split()
-        if len(l) != 5:
-            continue
-        if l[1] == '%usr':
-            continue
-        if l[0] == 'Average':
-            continue
-        # SunOS:  date    %usr     %sys %wio                       %idle
-        # xmlrpc: date cpu usr nice sys iowait steal irq soft guest idle nodename
-        x = ['%s %s'%(d, l[0]), 'all', '0', '0', '0', '0', '0', '0', '0', '0', '0', rcEnv.nodename]
-        x[2] = l[1]
-        x[4] = l[2]
-        x[5] = l[3]
-        x[10] = l[4]
-        lines.append(x)
-    return lines
+    def proc(self, d, day, start, end):
+        cols = ['date',
+                'runq_sz',
+                'nodename']
+        f = self.sarfile(day)
+        lines = []
+        if f is None:
+            return cols, lines
+        cmd = ['sar', '-q', '-f', f]
+        (ret, buff) = call(cmd)
+        for line in buff.split('\n'):
+            l = line.split()
+            if len(l) != 5:
+                continue
+            if l[1] == 'runq-sz':
+                continue
+            if l[0] == 'Average':
+                continue
+            x = ['%s %s'%(d, l[0]), l[1], self.nodename]
+            lines.append(x)
+        return cols, lines
 
-def stats_mem_u(file, collect_date=None):
-    return twodays(stats_mem_u_day)
+    def swap(self, d, day, start, end):
+        cols = ['date',
+                'kbswpfree',
+                'nodename']
+        f = self.sarfile(day)
+        lines = []
+        if f is None:
+            return cols, lines
+        cmd = ['sar', '-r', '-f', f]
+        (ret, buff) = call(cmd)
+        for line in buff.split('\n'):
+            l = line.split()
+            if len(l) != 3:
+                continue
+            if l[1] == 'freemem':
+                continue
+            if l[0] == 'Average':
+                continue
 
-def stats_mem_u_day(t):
-    d = t.strftime("%Y-%m-%d")
-    day = t.strftime("%d")
-    f = sarfile(day)
-    if f is None:
-        return []
-    cmd = ['sar', '-r', '-f', f]
-    (ret, buff) = call(cmd)
-    lines = []
-    for line in buff.split('\n'):
-        l = line.split()
-        if len(l) != 3:
-            continue
-        if l[1] == 'freemem':
-            continue
-        if l[0] == 'Average':
-            continue
+            freeswap = int(l[2])/2
+            x = ['%s %s'%(d, l[0]), str(freeswap), self.nodename]
+            lines.append(x)
+        return cols, lines
 
-        """ xmlrpc: date kbmemfree kbmemused pct_memused kbbuffers kbcached kbcommit pct_commit kbmemsys nodename
-        """
-        freemem = int(l[1])*pagesize/1024
-        x = ['%s %s'%(d, l[0]), str(freemem), '0', '0', '0', '0', '0', '0', '0', rcEnv.nodename]
+    def block(self, d, day, start, end):
+        cols = ['date',
+                'rbps',
+                'wbps',
+                'nodename']
+        f = self.sarfile(day)
+        lines = []
+        if f is None:
+            return []
+        cmd = ['sar', '-b', '-f', f]
+        (ret, buff) = call(cmd)
+        for line in buff.split('\n'):
+            l = line.split()
+            if len(l) != 9:
+                continue
+            if l[1] == 'bread/s':
+                continue
+            if l[0] == 'Average':
+                continue
+            x = ['%s %s'%(d, l[0]), l[1], l[4], self.nodename]
+            lines.append(x)
+        return cols, lines
 
-        lines.append(x)
-    return lines
-
-def stats_proc(file, collect_date=None):
-    return twodays(stats_proc_day)
-
-def stats_proc_day(t):
-    d = t.strftime("%Y-%m-%d")
-    day = t.strftime("%d")
-    f = sarfile(day)
-    if f is None:
-        return []
-    cmd = ['sar', '-q', '-f', f]
-    (ret, buff) = call(cmd)
-    lines = []
-    for line in buff.split('\n'):
-        l = line.split()
-        if len(l) != 5:
-            continue
-        if l[1] == 'runq-sz':
-            continue
-        if l[0] == 'Average':
-            continue
-        """ xmlrpc: date runq_sz plist_sz ldavg_1 ldavg_5 ldavg_15 nodename
-        """
-        x = ['%s %s'%(d, l[0]), l[1], '0', '0', '0', '0', rcEnv.nodename]
-        lines.append(x)
-    return lines
-
-def stats_swap(file, collect_date=None):
-    return twodays(stats_swap_day)
-
-def stats_swap_day(t):
-    d = t.strftime("%Y-%m-%d")
-    day = t.strftime("%d")
-    f = sarfile(day)
-    if f is None:
-        return []
-    cmd = ['sar', '-r', '-f', f]
-    (ret, buff) = call(cmd)
-    lines = []
-    for line in buff.split('\n'):
-        l = line.split()
-        if len(l) != 3:
-            continue
-        if l[1] == 'freemem':
-            continue
-        if l[0] == 'Average':
-            continue
-
-        """ xmlrpc: date kbswpfree kbswpused pct_swpused kbswpcad pct_swpcad nodename
-        """
-        freeswap = int(l[2])/2
-        x = ['%s %s'%(d, l[0]), str(freeswap), '0', '0', '0', '0', rcEnv.nodename]
-
-        lines.append(x)
-    return lines
-
-def stats_block(file, collect_date=None):
-    return twodays(stats_block_day)
-
-def stats_block_day(t):
-    d = t.strftime("%Y-%m-%d")
-    day = t.strftime("%d")
-    f = sarfile(day)
-    if f is None:
-        return []
-    cmd = ['sar', '-b', '-f', f]
-    (ret, buff) = call(cmd)
-    lines = []
-    for line in buff.split('\n'):
-        l = line.split()
-        if len(l) != 9:
-            continue
-        if l[1] == 'bread/s':
-            continue
-        if l[0] == 'Average':
-            continue
-
-        """ xmlrpc: date tps rtps wtps rbps wbps nodename
-        """
-        x = ['%s %s'%(d, l[0]), '0', '0', '0', l[1], l[4], rcEnv.nodename]
-
-        lines.append(x)
-    return lines
-
-def stats_blockdev(file, collect_date=None):
-    return twodays(stats_blockdev_day)
-
-def stats_blockdev_day(t):
-    d = t.strftime("%Y-%m-%d")
-    day = t.strftime("%d")
-    f = sarfile(day)
-    if f is None:
-        return []
-    cmd = ['sar', '-d', '-f', f]
-    (ret, buff) = call(cmd, errlog=False)
-    lines = []
-    for line in buff.split('\n'):
-        l = line.split()
-        if len(l) == 8:
-            date = l[0]
-        if len(l) == 7:
-            l = [date] + l
-        if len(l) != 8:
-            continue
-        if l[1] == 'device':
-            continue
-        if l[0] == 'Average':
-            continue
-        """ xmlrpc: 22:05:01 DEV tps rd_sec/s wr_sec/s avgrq-sz avgqu-sz await svctm %util
-                    00:00:00 device %busy avque r+w/s blks/s avwait avserv
-        """
-        x = ['%s %s'%(d, l[0]), l[1], l[4], '0', '0', '0', l[3], l[6], l[7], l[2], rcEnv.nodename]
-        lines.append(x)
-    return lines
-
-def stats_netdev(file, collect_date=None):
-    return twodays(stats_netdev_day)
-
-def stats_netdev_day(t):
-    return []
-
-def stats_netdev_err(file, collect_date=None):
-    return twodays(stats_netdev_err_day)
-
-def stats_netdev_err_day(t):
-    return []
+    def blockdev(self, d, day, start, end):
+        cols = ['date',
+                'dev',
+                'pct_util',
+                'avgqu_sz',
+                'rsecps',
+                'await',
+                'svctm',
+                'nodename']
+        f = self.sarfile(day)
+        lines = []
+        if f is None:
+            return cols, lines
+        cmd = ['sar', '-d', '-f', f]
+        (ret, buff) = call(cmd, errlog=False)
+        for line in buff.split('\n'):
+            l = line.split()
+            if len(l) == 8:
+                date = l[0]
+            if len(l) == 7:
+                l = [date] + l
+            if len(l) != 8:
+                continue
+            if l[1] == 'device':
+                continue
+            if l[0] == 'Average':
+                continue
+            # 00:00:00 device %busy avque r+w/s [blks/s] avwait avserv
+            x = ['%s %s'%(d, l[0]), l[1], l[2], l[3], l[4], l[6], l[7], self.nodename]
+            lines.append(x)
+        return cols, lines
 
