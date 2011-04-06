27a46a6eabe0bafe597916d7f6fb615c4800febb
Christophe Varoqui <christophe.varoqui@opensvc.com>
2011-04-06 17:15:43 +0200

    Allow to define geoloc asset info in node.conf, section node
    loc_country
    loc_city
    loc_addr
    loc_zip
    loc_building
    loc_floor
    loc_room
    loc_rack
    
    document in usr/share/doc/node.conf

--- lib/rcAsset.py
+++ lib/rcAsset.py
@@ -73,6 +73,54 @@ class Asset(object):
         except:
             return None
 
+    def get_loc_country(self):
+        try:
+            return self.node.config.get('node', 'loc_country')
+        except:
+            return None
+
+    def get_loc_city(self):
+        try:
+            return self.node.config.get('node', 'loc_city')
+        except:
+            return None
+
+    def get_loc_addr(self):
+        try:
+            return self.node.config.get('node', 'loc_addr')
+        except:
+            return None
+
+    def get_loc_building(self):
+        try:
+            return self.node.config.get('node', 'loc_building')
+        except:
+            return None
+
+    def get_loc_floor(self):
+        try:
+            return self.node.config.get('node', 'loc_floor')
+        except:
+            return None
+
+    def get_loc_room(self):
+        try:
+            return self.node.config.get('node', 'loc_room')
+        except:
+            return None
+
+    def get_loc_rack(self):
+        try:
+            return self.node.config.get('node', 'loc_rack')
+        except:
+            return None
+
+    def get_loc_zip(self):
+        try:
+            return self.node.config.get('node', 'loc_zip')
+        except:
+            return None
+
     def get_asset_dict(self):
         d = {}
         d['nodename'] = rcEnv.nodename
@@ -91,6 +139,30 @@ class Asset(object):
         d['serial'] = self.get_serial()
         d['model'] = self.get_model()
         d['environnement'] = self.get_environnement()
+        loc_country = self.get_loc_country()
+        if loc_country is not None:
+            d['loc_country'] = loc_country
+        loc_city = self.get_loc_city()
+        if loc_city is not None:
+            d['loc_city'] = loc_city
+        loc_building = self.get_loc_building()
+        if loc_building is not None:
+            d['loc_building'] = loc_building
+        loc_room = self.get_loc_room()
+        if loc_room is not None:
+            d['loc_room'] = loc_room
+        loc_rack = self.get_loc_rack()
+        if loc_rack is not None:
+            d['loc_rack'] = loc_rack
+        loc_addr = self.get_loc_addr()
+        if loc_addr is not None:
+            d['loc_addr'] = loc_addr
+        loc_floor = self.get_loc_floor()
+        if loc_floor is not None:
+            d['loc_floor'] = loc_floor
+        loc_zip = self.get_loc_zip()
+        if loc_zip is not None:
+            d['loc_zip'] = loc_zip
         team_responsible = self.get_team_responsible()
         if team_responsible is not None:
             d['team_responsible'] = team_responsible
