1aba10d7787cafa33b1e160d006a16f5add2a4ef
Christophe Varoqui <christophe.varoqui@opensvc.com>
2011-07-12 11:23:40 +0200

    Don't instanciate Node() from rcNode
    to avoid starting unecessary threads

--- lib/rcNode.py
+++ lib/rcNode.py
@@ -22,13 +22,18 @@ import re
 from rcGlobalEnv import *
 import logging
 import rcExceptions as ex
+import ConfigParser
 
 def node_get_hostmode():
-    import node
-    n = node.Node()
-    if n.config.has_section('node'):
-        return n.config.get('node', 'host_mode')
-    return n.config.get('DEFAULT', 'host_mode')
+    nodeconf = os.path.join(os.path.dirname(__file__), '..', 'etc', 'node.conf')
+    config = ConfigParser.RawConfigParser()
+    config.read(nodeconf)
+    if config.has_section('node'):
+        if config.has_option('node', 'host_mode'):
+            return config.get('node', 'host_mode')
+    elif config.has_option('DEFAULT', 'host_mode'):
+        return config.get('DEFAULT', 'host_mode')
+    return 'TST'
 
 def discover_node():
 	"""Fill rcEnv class with information from node discovery
