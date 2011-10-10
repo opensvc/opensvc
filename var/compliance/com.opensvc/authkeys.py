e246504aaf8ce7c5464f03ae1a76686110d312bb
Christophe Varoqui <christophe.varoqui@opensvc.com>
2011-10-10 17:35:53 +0200

    authkeys compliance helper module enhancements
    support "action" and "authfile" in the rule dictionnary
    
    "action":
     values: add|del
     default: add
    
    "authfile":
     values: authorized_keys|authorized_keys2
     default: authorized_keys2
    
    Example:
    
    $ export OSVC_COMP_AUTHKEY_1='{"authfile": "foo", "action": "del", "key": "ssh-dss AAAAB3Nza...gkrxdFg+dQ== cvaroqui", "user": "cvaroqui"}'
    $ ./authkeys.py OSVC_COMP_AUTHKEY_  fix
    unsupported authfile: foo (default to authorized_keys2)
    key 'ssh-dss AAAAB3Nza...gkrxdFg+dQ== cvaroqui' is already not installed for user cvaroqui

--- var/compliance/com.opensvc/authkeys.py
+++ var/compliance/com.opensvc/authkeys.py
@@ -1,7 +1,14 @@
 #!/opt/opensvc/bin/python
 """ 
 module use OSVC_COMP_AUTHKEY_... vars
-which define {"user": "foo", "key": "XXXX..."} where:
+which define
+{
+ "action": "add",                # optional, defaults to "add"
+ "authfile": "authorized_keys",  # optional, defaults to "authorized_keys2"
+ "user": "foo",                  # mandatory
+ "key": "XXXX..."                # mandatory
+}
+where:
 - user: the username the key authorize to log as
 - key: a single pub key to authorize
 """
@@ -17,7 +24,7 @@ sys.path.append(os.path.dirname(__file__))
 from comp import *
 
 class CompAuthKeys(object):
-    def __init__(self, prefix='OSVC_COMP_AUTHKEY_'):
+    def __init__(self, prefix='OSVC_COMP_AUTHKEY_', authfile="authorized_keys2"):
         self.prefix = prefix.upper()
         self.authkeys = []
         for k in [key for key in os.environ if key.startswith(self.prefix)]:
@@ -31,6 +38,26 @@ class CompAuthKeys(object):
             raise NotApplicable()
 
         self.installed_keys_d = {}
+        if authfile not in ("authorized_key", "authorized_keys2"):
+            print >>sys.stderr, "unsupported authfile:", authfile, "(use authorized_key or authorized_keys2)"
+            raise NotApplicable()
+        self.authfile = authfile
+
+    def sanitize(self, ak):
+        if 'user' not in ak:
+            print >>sys.stderr, "no user set in rule"
+            return False
+        if 'key' not in ak:
+            print >>sys.stderr, "no key set in rule"
+            return False
+        if 'action' not in ak:
+            ak['action'] = 'add'
+        if 'authfile' not in ak:
+            ak['authfile'] = self.authfile
+        if ak['authfile'] not in ("authorized_key", "authorized_keys2"):
+            print >>sys.stderr, "unsupported authfile:", ak['authfile'], "(default to", self.authfile+")"
+            ak['authfile'] = self.authfile
+        return ak
 
     def fixable(self):
         return RET_NA
@@ -58,16 +85,42 @@ class CompAuthKeys(object):
         return self.installed_keys_d[user]
 
     def check_authkey(self, ak, verbose=True):
+        ak = self.sanitize(ak)
         installed_keys = self.get_installed_keys(ak['user'])
-        if ak['key'] not in installed_keys:
-            if verbose:
-                print >>sys.stderr, 'key', self.truncate_key(ak['key']), 'is not installed for user', ak['user']
+        if ak['action'] == 'add':
+            if ak['key'] not in installed_keys:
+                if verbose:
+                    print >>sys.stderr, 'key', self.truncate_key(ak['key']), 'must be installed for user', ak['user']
+                r = RET_ERR
+            else:
+                if verbose:
+                    print 'key', self.truncate_key(ak['key']), 'is correctly installed for user', ak['user']
+                r = RET_OK
+        elif ak['action'] == 'del':
+            if ak['key'] in installed_keys:
+                if verbose:
+                    print >>sys.stderr, 'key', self.truncate_key(ak['key']), 'must be uninstalled for user', ak['user']
+                r = RET_ERR
+            else:
+                if verbose:
+                    print 'key', self.truncate_key(ak['key']), 'is correctly not installed for user', ak['user']
+                r = RET_OK
+        else:
+            print >>sys.stderr, "unsupported action:", ak['action']
             return RET_ERR
-        if verbose:
-                print 'key', self.truncate_key(ak['key']), 'is installed for user', ak['user']
-        return RET_OK
+        return r
 
     def fix_authkey(self, ak):
+        ak = self.sanitize(ak)
+        if ak['action'] == 'add':
+            return self.add_authkey(ak)
+        elif ak['action'] == 'del':
+            return self.del_authkey(ak)
+        else:
+            print >>sys.stderr, "unsupported action:", ak['action']
+            return RET_ERR
+
+    def add_authkey(self, ak):
         if self.check_authkey(ak, verbose=False) == RET_OK:
             print 'key', self.truncate_key(ak['key']), 'is already installed for user', ak['user']
             return RET_OK
@@ -99,6 +152,41 @@ class CompAuthKeys(object):
             if not ak['key'].endswith('\n'):
                 f.write('\n')
             print 'key', self.truncate_key(ak['key']), 'installed for user', ak['user']
+
+        return RET_OK
+
+    def del_authkey(self, ak):
+        if self.check_authkey(ak, verbose=False) == RET_OK:
+            print 'key', self.truncate_key(ak['key']), 'is already not installed for user', ak['user']
+            return RET_OK
+
+        base = os.path.join(os.path.expanduser("~"+ak['user']), '.ssh')
+        ps = [os.path.join(base, 'authorized_keys'),
+              os.path.join(base, 'authorized_keys2')]
+
+        for p in ps:
+            if not os.path.exists(base):
+                return RET_OK
+
+            if not os.path.exists(p):
+                return RET_OK
+
+            with open(p, 'r') as f:
+                l = f.read().split('\n')
+
+            n = len(l)
+            while True:
+                try:
+                    l.remove(ak['key'].replace('\n', ''))
+                except ValueError:
+                    break
+            if len(l) == n:
+                # nothing changed
+                continue
+
+            with open(p, 'w') as f:
+                f.write('\n'.join(l))
+                print 'key', self.truncate_key(ak['key']), 'uninstalled for user', ak['user']
 
         return RET_OK
 
