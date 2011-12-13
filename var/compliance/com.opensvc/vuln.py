3cbc2a8f463a77186ec06475ff18d163a5ed373b
Christophe Varoqui <christophe.varoqui@opensvc.com>
2011-12-13 18:20:37 +0100

    Add check support for vuln compliance object on SunOS
    Fix support still to be done.

--- var/compliance/com.opensvc/vuln.py
+++ var/compliance/com.opensvc/vuln.py
@@ -25,7 +25,7 @@ class CompVuln(object):
         self.fix_list = []
         self.sysname, self.nodename, x, x, self.machine = os.uname()
 
-        if self.sysname not in ['Linux', 'HP-UX', 'AIX']:
+        if self.sysname not in ['Linux', 'HP-UX', 'AIX', 'SunOS']:
             print >>sys.stderr, 'module not supported on', self.sysname
             raise NotApplicable()
 
@@ -71,11 +71,52 @@ class CompVuln(object):
             self.fix_pkg = self.aix_fix_pkg
             self.fixable_pkg = self.aix_fixable_pkg
             self.fix_all = self.aix_fix_all
+        elif vendor in ['Oracle']:
+            self.get_installed_packages = self.sol_get_installed_packages
+            self.fix_pkg = self.sol_fix_pkg
+            self.fixable_pkg = self.sol_fixable_pkg
+            self.fix_all = self.sol_fix_all
         else:
             print >>sys.stderr, vendor, "not supported"
             raise NotApplicable()
 
         self.installed_packages = self.get_installed_packages()
+
+    def sol_fix_pkg(self, pkg):
+        return RET_NA
+
+    def sol_fixable_pkg(self, pkg):
+        return RET_ERR
+
+    def sol_fix_all(self):
+        return RET_NA
+
+    def sol_get_installed_packages(self):
+        p = Popen(['pkginfo', '-l'], stdout=PIPE)
+        (out, err) = p.communicate()
+        if p.returncode != 0:
+            print >>sys.stderr, 'can not fetch installed packages list'
+            return {}
+        return self.sol_parse_pkginfo(out)
+
+    def sol_parse_pkginfo(self, out):
+        l = {}
+        for line in out.split('\n'):
+            v = line.split(':')
+            if len(v) != 2:
+                continue
+            f = v[0].strip()
+            if f == "PKGINST":
+                pkgname = v[1].strip()
+            elif f == "ARCH":
+                pkgarch = v[1].strip()
+            elif f == "VERSION":
+                pkgvers = v[1].strip()
+                if pkgname in l:
+                    l[pkgname] += [(pkgvers, pkgarch)]
+                else:
+                    l[pkgname] = [(pkgvers, pkgarch)]
+        return l
 
     def aix_fix_pkg(self, pkg):
         return RET_NA
