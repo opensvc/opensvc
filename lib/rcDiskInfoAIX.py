ba66d4c3266ba9c547af18d4ef12253ae05d465e
Christophe Varoqui <christophe.varoqui@opensvc.com>
2012-04-05 20:00:50 +0200

    Add AIX device tree library
    Feed the tree with
    
    - lsdev -cC disk
    - lvm configuration
    
    Also fallback to vio disk id for disks without wwid.

--- lib/rcDiskInfoAIX.py
+++ lib/rcDiskInfoAIX.py
@@ -43,13 +43,25 @@ class diskInfo(rcDiskInfo.diskInfo):
         cmd = ['bootinfo', '-s', lname]
         out, err, ret = justcall(cmd)
         if ret == 0:
-            size = str(int(out.split()[-1]))
+            size = int(out.strip())
         else:
-            size = '0'
+            size = 0
         
         wwid = self.odmget(lname, 'ww_name').replace('0x', '')
+        if wwid == 'unknown':
+            wwid = self.get_vscsi_id(lname)
 
         self.h[lname] = dict(vid=vid, pid=pid, wwid=wwid, size=size)
+
+    def get_vscsi_id(self, lname):
+        cmd = ['lscfg', '-l', lname]
+        (ret, out, err) = call(cmd)
+        if ret != 0:
+            return 'unknown'
+        l = out.split()
+        if len(l) < 2:
+            return 'unknown'
+        return l[1]
 
     def odmget(self, lname, attr):
         cmd = ['odmget', '-q', 'name='+lname+' AND attribute='+attr, 'CuAt']
@@ -66,9 +78,9 @@ class diskInfo(rcDiskInfo.diskInfo):
         return dev
 
     def get(self, dev, type):
+        dev = self.devkey(dev)
         if dev not in self.h:
             self.scan(dev)
-        dev = self.devkey(dev)
         return self.h[dev][type]
 
     def disk_id(self, dev):
