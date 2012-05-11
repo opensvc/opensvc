84c901d65943ece055b5e6c2be7d7ca630ee2d45
Christophe Varoqui <christophe.varoqui@opensvc.com>
2012-05-11 14:35:43 +0200

    Blacklist pass-through block devices from HP-UX write_mksf()
    Block devices ending with .pt do not have corresponding char
    device, which provoke an error in the presync function of the
    HP-UX Vg resource. As those devices are of no interest to
    the Vg resource, blacklist them.

--- lib/resVgHP-UX.py
+++ lib/resVgHP-UX.py
@@ -86,7 +86,7 @@ class Vg(resDg.Dg):
                 if len(line) == 0:
                     return
                 a = line.split(':')[0]
-                if '/dev/pt/pt' not in a and '/dev/rdisk/disk' not in a and self.dsf_name(a) in dsf_names:
+                if '/dev/pt/pt' not in a and '/dev/rdisk/disk' not in a and not a.endswith(".pt") and self.dsf_name(a) in dsf_names:
                     cmd = ['scsimgr', 'get_attr', '-D', self.dev2char(a), '-a', 'wwid', '-p']
                     (ret, out, err) = self.call(cmd)
                     if ret != 0:
