d508461b48720a476012273f323a42778a761436
Christophe Varoqui <christophe.varoqui@opensvc.com>
2012-05-20 17:06:26 +0200

    Add a checker to monitor the Linux mcelog growth
    The mcelog file contains cpu and memory hardware hardware errors
    dumps. It is useful to be alerted upon addition in this file to
    be able to do proactive maintenance on server hardware.

--- lib/checks.py
+++ lib/checks.py
@@ -47,6 +47,7 @@ class checks(check):
         self.register('checkRaidMegaRaid')
         self.register('checkRaidSas2')
         self.register('checkFmFmadm')
+        self.register('checkMce')
         self.register_local_checkers()
 
     def __iadd__(self, c):
