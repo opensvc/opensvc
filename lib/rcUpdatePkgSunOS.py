548653f044291e6a36aee6f854e43715c77c91bb
Christophe Varoqui <christophe.varoqui@opensvc.com>
2012-06-04 17:44:10 +0200

    Add 'nodemgr updatepkg' command
    This new command updates the opensvc package on the node.
    
    It depends on a node.repo node.conf parameter, pointing to
    a ftp or http repository hosting opensvc packages.
    The target package must be defined as 'current'.
    
    The repo file tree must comply to the following organization:
    
    ROOT
    +- packages
     +- pkg
      +- opensvc-1.4-1.pkg
      +- opensvc-1.4-2.pkg
      +- current -> opensvc-1.4-2.pkg
     +- rpm
     +- deb
     +- depot
     +- tbz
    
    The package fetching code is cross os, and the update methods are
    os specific. This first patch implements the SunOS update method.
    
    Conflicts:
    
    	lib/node.py

---
+++ lib/rcUpdatePkgSunOS.py
@@ -0,0 +1,16 @@
+from subprocess import *
+
+repo_subdir = "pkg"
+
+def update(fpath):
+    cmd = ['pkgrm', '-n', 'opensvc']
+    print ' '.join(cmd)
+    p = Popen(cmd)
+    p.communicate()
+    if p.returncode != 0:
+        return 1
+    cmd = "yes | pkgadd -G -d %s all"%fpath
+    print cmd
+    p = Popen(cmd, shell=True)
+    p.communicate()
+    return p.returncode
