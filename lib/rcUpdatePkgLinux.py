6c086b03f3979c379e6ebcde87c5407d66252b80
Christophe Varoqui <christophe.varoqui@opensvc.com>
2012-06-05 10:27:28 +0200

    Allow rpm downgrade on 'nodemgr updatepkg'

--- lib/rcUpdatePkgLinux.py
+++ lib/rcUpdatePkgLinux.py
@@ -18,7 +18,7 @@ def update_deb(fpath):
     return p.returncode
 
 def update_rpm(fpath):
-    cmd = ['rpm', '-Uvh', fpath]
+    cmd = ['rpm', '-Uvh', fpath, '--force']
     print ' '.join(cmd)
     p = Popen(cmd)
     p.communicate()
