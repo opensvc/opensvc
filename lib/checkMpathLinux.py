94dbeae0e90ea939008a39f28032ef8c39377c11
Christophe Varoqui <christophe.varoqui@opensvc.com>
2012-08-14 09:15:09 +0200

    Support older versions of Linux multipath-tools
    The Linux mpath checker relied on multipathd "show topo"
    command to count multipaths paths. This command is not
    available on rhel 4 systems. Implement a fallback to
    multipath -l for this case.

--- lib/checkMpathLinux.py
+++ lib/checkMpathLinux.py
@@ -27,9 +27,66 @@ class check(checks.check):
                 return svc.svcname
         return ''
 
+    def do_check_old(self):
+        """
+	mpath1 (3600508b4000971ca0000f00010650000)
+	[size=404 GB][features="1 queue_if_no_path"][hwhandler="0"]
+	\_ round-robin 0 [active]
+	 \_ 0:0:0:2 sda 8:0   [active]
+	 \_ 1:0:0:2 sde 8:64  [active]
+	 \_ 1:0:1:2 sdf 8:80  [active]
+	 \_ 0:0:1:2 sdi 8:128 [active]
+	\_ round-robin 0 [enabled]
+	 \_ 0:0:2:2 sdc 8:32  [active]
+	 \_ 0:0:3:2 sdd 8:48  [active]
+	 \_ 1:0:3:2 sdh 8:112 [active]
+	 \_ 1:0:2:2 sdb 8:16  [active]
+        """
+        cmd = ['multipath', '-l']
+        (out, err, ret) = justcall(cmd)
+        if ret != 0:
+            return self.undef
+        lines = out.split('\n')
+        if len(lines) < 1:
+            return self.undef
+        r = []
+        wwid = None
+        dev = None
+        for line in lines:
+            if len(line) > 0 and not '\_ ' in line and not line.startswith('['):
+                # new mpath
+                # - store previous
+                # - reset path counter
+                if wwid is not None:
+                    r.append({'chk_instance': wwid,
+                              'chk_value': str(n),
+                              'chk_svcname': self.find_svc(dev),
+                             })
+                n = 0
+                l = line.split()
+                if len(l) == 2:
+                    wwid = l[1][2:-1]
+                elif len(l) == 1:
+                    wwid = l[0][1:]
+                else:
+                    wwid = None
+            if "[active]" in line and line.startswith(' '):
+                n += 1
+                dev = "/dev/"+line.split()[2]
+        if wwid is not None:
+            r.append({'chk_instance': wwid,
+                      'chk_value': str(n),
+                      'chk_svcname': self.find_svc(dev),
+                     })
+        return r
+
     def do_check(self):
         cmd = ['multipathd', '-kshow topo']
         (out, err, ret) = justcall(cmd)
+        if 'list|show' in out:
+            # multipathd does not support 'show topo'
+            # try parsing 'multipath -l' output
+            return self.do_check_old()
         if ret != 0:
             return self.undef
         lines = out.split('\n')
