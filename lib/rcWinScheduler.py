c5341a6443576d5f99be302c1c7e339804d549c1
Christophe Varoqui <christophe.varoqui@opensvc.com>
2012-08-16 16:23:34 +0200

    Plug a scheduler in the Windows opensvc service
    Simply schedule bin/svcmon and bin/cron/opensvc every 10 minutes.

--- lib/rcWinScheduler.py
+++ lib/rcWinScheduler.py
@@ -23,6 +23,10 @@ import win32con
 import win32event
 import win32evtlogutil
 import os
+import servicemanager
+
+import datetime
+from subprocess import *
 
 class OsvcSched(win32serviceutil.ServiceFramework):
 
@@ -33,16 +37,18 @@ class OsvcSched(win32serviceutil.ServiceFramework):
     def __init__(self, args):
         win32serviceutil.ServiceFramework.__init__(self, args)
         self.hWaitStop = win32event.CreateEvent(None, 0, 0, None)
+	now = datetime.datetime.now()
+        self.next_task10 = now + datetime.timedelta(minutes=1)
+
 
     def SvcStop(self):
         self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
         win32event.SetEvent(self.hWaitStop)
 
     def SvcDoRun(self):
-        import servicemanager
         servicemanager.LogMsg(servicemanager.EVENTLOG_INFORMATION_TYPE,servicemanager.PYS_SERVICE_STARTED,(self._svc_name_, ''))
 
-        self.timeout = 3000
+        self.timeout = 60000
 
         while 1:
             # Wait for service stop signal, if I timeout, loop again
@@ -53,8 +59,25 @@ class OsvcSched(win32serviceutil.ServiceFramework):
                 servicemanager.LogInfoMsg("%s - STOPPED"%self._svc_name_)
                 break
             else:
-                servicemanager.LogInfoMsg("%s - ALIVE"%self._svc_name_)
+                #servicemanager.LogInfoMsg("%s - ALIVE"%self._svc_name_)
+		self.SvcDoJob()
 
+    def SvcDoJob(self):
+        now = datetime.datetime.now()
+	if now > self.next_task10:
+	    self.run('task10')
+	    self.next_task10 = now + datetime.timedelta(minutes=10)
+
+    def run(self, task):
+        if task == "task10":
+            servicemanager.LogInfoMsg("run svcmon")
+	    cmd = ["c:\Program Files\opensvc\svcmon.cmd", "--updatedb"]
+            p = Popen(cmd, stdout=None, stderr=None, stdin=None)
+	    p.communicate()
+            servicemanager.LogInfoMsg("run internal scheduler")
+	    cmd = ["c:\Program Files\opensvc\cron.cmd"]
+            p = Popen(cmd, stdout=None, stderr=None, stdin=None)
+	    p.communicate()
 
 def ctrlHandler(ctrlType):
     return True
