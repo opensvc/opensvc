1d41eeb4b61ba84ff39d5ba2de39e0a6eb201926
Christophe Varoqui <christophe.varoqui@opensvc.com>
2012-10-25 15:19:17 +0200

    Postinstall enhancements for Windows
    - remove old msi cfg file
    - create tmp/ and log/ dirs in the detected pathsvc instead of
      hardcoded /opt/opensvc/
    - workaround pathbin value loss

--- bin/postinstall.cmd
+++ bin/postinstall.cmd
@@ -1,4 +1,5 @@
 @echo off
-set OPENSVCPATH=%ProgramFiles%\opensvc
+set POSTSCRIPT=%1
 for /F "tokens=*" %%i in ('dir /s /l /b /ad /on "%ProgramFiles%\python2*"') do set PYTHONPATH=%%i\python.exe
-if exist "%PYTHONPATH%" ("%PYTHONPATH%" "%OPENSVCPATH%\bin\postinstall")
+if exist "%PYTHONPATH%" ("%PYTHONPATH%" %POSTSCRIPT%) else exit 1
+exit 0
