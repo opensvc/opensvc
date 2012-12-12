b0ec6c3ba49d500ec92cf850194cacf1a5b1dff2
Christophe Varoqui <christophe.varoqui@opensvc.com>
2012-12-12 11:32:28 +0100

    Replace HP-UX cstm by cprop
    cstm is deprecated, and impose an ugly workaround for boot
    conditions.
    
    Use cprop instead, to count mem banks and slots.

--- lib/rcAssetHP-UX.py
+++ lib/rcAssetHP-UX.py
@@ -25,64 +25,14 @@ import datetime
 class Asset(rcAsset.Asset):
     def __init__(self, node):
         rcAsset.Asset.__init__(self, node)
-        # echo "selclass qualifier memory;info;wait;infolog"|cstm
-        process = Popen(['cstm'], stdin=PIPE, stdout=PIPE, stderr=None)
-        if self.skip_cstm_info():
-            (out, err) = process.communicate(input='selclass qualifier memory;infolog')
-        else:
-            (out, err) = process.communicate(input='selclass qualifier memory;info;wait;infolog')
-        if process.returncode != 0:
-            self.memory = []
-        else:
-            self.memory = out.split('\n')
 
-        (out, err, ret) = justcall(['print_manifest'])
+        out, err, ret = justcall(['print_manifest'])
         if ret != 0:
             self.manifest = []
         else:
             self.manifest = out.split('\n')
 
         self.parse_memory()
-
-    def skip_cstm_info(self):
-        d = self.get_last_boot()
-        if d is None:
-            return True
-        delta = datetime.datetime.now() - d
-        threshold = datetime.timedelta(minutes=60)
-        if delta < threshold:
-            return True
-        return False
-
-    def get_last_boot(self):
-        cmd = ['who', '-b']
-        out, err, ret = justcall(cmd)
-
-        """
-           .       system boot  Nov 30 08:58
-        """
-        l = out.split()
-        idx = l.index("boot")
-        l = l[idx+1:]
-        buff = ' '.join(l)
-        d = None
-        now = datetime.datetime.now()
-
-        if len(l) == 1:
-            buff = now.strftime("%Y %b ") + buff
-        elif len(l) == 3:
-            buff = now.strftime("%Y ") + buff
-        elif len(l) == 4:
-            pass
-        else:
-            return None
-
-        try:
-            d = datetime.datetime.strptime(buff, "%Y %b %d %H:%M")
-        except:
-            pass
-
-        return d
 
     def _get_mem_bytes(self):
         cmd = ['swapinfo', '-Mq']
@@ -92,58 +42,23 @@ class Asset(rcAsset.Asset):
         return str(int(out)//1024)
 
     def parse_memory(self):
-        """
-	   DIMM Slot      Size (MB)
-	   ---------      ---------
-		  0A           2048
-		  3D              0
-	   ---------      ---------
-
-        or
-
-	   DIMM Location          Size(MB)     DIMM Location          Size(MB)
-	   --------------------   --------     --------------------   --------
-	   Ext 0 DIMM 0A          2048         Ext 0 DIMM 0B          2048    
-	   Ext 0 DIMM 5C          ----         Ext 0 DIMM 5D          ----    
-
-	   Ext 0 Total: 32768 (MB)
-        """
-        if len(self.memory) == 0:
-            return '0'
         self.banks =  0
         self.slots =  0
-        begin = 0
-        end = 0
-        for i, l in enumerate(self.memory):
-            if 'DIMM ' in l:
-                begin = i+2
-        if begin == 0:
+
+        cmd = ['cprop', '-summary', '-c', 'Memory']
+        out, err, ret = justcall(cmd)
+
+        if ret != 0:
             return '0'
-        for i in range(begin, len(self.memory)):
-            e = self.memory[i].split()
-            if len(self.memory[i]) == 0 or '--' in e[0]:
-                end = i
-                break
-        if end == 0:
-            return '0'
-        for i in range(begin, end):
-            e = self.memory[i].split()
-            n = len(e)
-            if n == 2:
-                # old format
+
+        in_banks = True
+
+        for line in out.split('\n'):
+            if 'Empty Slots' in line:
+                in_banks = False
+            elif 'Instance' in line:
                 self.slots += 1
-                if e[1] != '0':
-                    self.banks += 1
-                continue
-            if n >= 5:
-                # new format 1st col
-                self.slots += 1
-                if not '--' in e[4]:
-                    self.banks += 1
-            if n == 10:
-                # new format 2nd col
-                self.slots += 1
-                if not '--' in e[9]:
+                if in_banks:
                     self.banks += 1
 
     def _get_mem_banks(self):
