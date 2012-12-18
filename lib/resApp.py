3af82ace1c484bd9bc9f164feec29b2b9055284e
Christophe Varoqui <christophe.varoqui@opensvc.com>
2012-12-18 21:45:26 +0100

    Allow apps to be started on 1-node services even if instance fs+vg is down
    The fs+vg status evaluation id meant to make sure the apps are
    not started on the passive node of a failover cluster. Single node
    services don't need this safety net.
    
    A typical case of single node service is the encap part of a
    single container virtual service.

--- lib/resApp.py
+++ lib/resApp.py
@@ -92,7 +92,7 @@ class Apps(Res.Resource):
             if verbose: self.status_log("%s does not exist"%self.svc.initd)
             return False
         status = self.svc.group_status(excluded_groups=set(["sync", "app", "disk", "hb"]))
-        if str(status["overall"]) != "up":
+        if str(status["overall"]) != "up" and len(self.svc.nodes) > 1:
             self.log.debug("abort resApp status because ip+fs status is %s"%status["overall"])
             if verbose: self.status_log("ip+fs status is %s, skip check"%status["overall"])
             return False
