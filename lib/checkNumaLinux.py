935cd11f8ec0357996872312e0383728430321ff
Christophe Varoqui <christophe.varoqui@opensvc.com>
2013-01-31 11:38:15 +0100

    NUMA checker enhancement
    Use cpu per NUMA nodes to compute ideal memory target per node.
    
    For example, this situation would report 0% deviation:
    
    node 0 cpu: 2
    node 0 size: 8000 MB
    node 0 cpu: 1
    node 1 size: 4000 MB

--- lib/checkNumaLinux.py
+++ lib/checkNumaLinux.py
@@ -24,28 +24,33 @@ class check(checks.check):
     chk_type = "numa"
 
     def do_check(self):
-        meminfo = {}
+        nodeinfo = {}
         memtotal = 0
         n_nodes = 0
+        n_cpu = 0
         for npath in glob.glob("/sys/devices/system/node/node*"):
+            node_n_cpu = len(glob.glob(npath+"/cpu*"))
             node = os.path.basename(npath)
             with open(npath+"/meminfo", 'r') as f:
                 lines = f.read().strip('\n').split('\n')
                 for line in lines:
                     if 'MemTotal' in line:
                         try:
-                            meminfo[node] = int(line.split()[-2])
+                            node_mem = int(line.split()[-2])
                         except:
                             continue
-                        memtotal += meminfo[node]
+                        memtotal += node_mem
                         n_nodes += 1
+                        n_cpu += node_n_cpu
+                        nodeinfo[node] = {"mem": node_mem, "cpu": node_n_cpu}
                         break
         r = []
         if n_nodes < 2:
             return r
-        memavg = memtotal / n_nodes
-        for node, mem in meminfo.items():
-            deviation = math.fabs(100. * (mem - memavg) // memavg)
+        target_per_cpu = memtotal / n_cpu
+        for node, info in nodeinfo.items():
+            target = target_per_cpu * info['cpu']
+            deviation = math.fabs(100. * (info['mem'] - target) // target)
             r.append({
                   'chk_instance': node+'.mem.leveling',
                   'chk_value': str(deviation),
