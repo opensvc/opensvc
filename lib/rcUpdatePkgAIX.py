915f3e885ed26fb64db255966c739cd38ed5ba90
Christophe Varoqui <christophe.varoqui@opensvc.com>
2013-03-16 12:13:42 +0100

    Don't use verbose and hash mode upon rpm updatepkg (AIX)
    These modes are not nicely rendered in the collector action
    queue log.

--- lib/rcUpdatePkgAIX.py
+++ lib/rcUpdatePkgAIX.py
@@ -3,8 +3,8 @@ from subprocess import *
 repo_subdir = "rpms"
 
 def update(fpath):
-    cmd = ['rpm', '-Uvh', fpath, '--force', '--ignoreos', '--nodeps']
-    print ' '.join(cmd)
+    cmd = ['rpm', '-U', fpath, '--force', '--ignoreos', '--nodeps']
+    print(' '.join(cmd))
     p = Popen(cmd)
     p.communicate()
     return p.returncode
