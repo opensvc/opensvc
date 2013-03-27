759bc82eba666f9539dfcaf78855461268006fdd
Christophe Varoqui <christophe.varoqui@opensvc.com>
2013-03-27 18:18:04 +0100

    Add quiet mode to msiexec on "nodemgr updatepkg"

--- lib/rcUpdatePkgWindows.py
+++ lib/rcUpdatePkgWindows.py
@@ -3,7 +3,7 @@ from subprocess import *
 repo_subdir = "msi"
 
 def update(fpath):
-    cmd = ['msiexec', '/i', fpath, 'REINSTALL=ALL', 'REINSTALLMODE=vomus']
+    cmd = ['msiexec', '/quiet', '/i', fpath, 'REINSTALL=ALL', 'REINSTALLMODE=vomus']
     print(' '.join(cmd))
     p = Popen(cmd)
     p.communicate()
