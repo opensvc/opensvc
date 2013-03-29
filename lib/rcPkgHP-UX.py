fc8230a89fde21fff10bc394d91ff0c023ac8f76
Christophe Varoqui <christophe.varoqui@opensvc.com>
2013-03-29 16:07:56 +0100

    Support new pushpkg fields on HP-UX package format too
    Use "-a mod_time", and also report bundles in addition
    to products, now that the collector has a pkg type column
    to differenciate the two.

--- lib/rcPkgHP-UX.py
+++ lib/rcPkgHP-UX.py
@@ -17,20 +17,31 @@
 #
 from rcUtilities import call, which
 from rcGlobalEnv import rcEnv
+import datetime
 
 def listpkg():
     if which('swlist') is None:
         return []
-    cmd = ['swlist', '-l', 'product']
+    lines = []
+    for t in ('product', 'bundle'):
+        lines += listpkg_t(t)
+    return lines
+
+def listpkg_t(t):
+    cmd = ['swlist', '-l', t, '-a', 'revision', '-a', 'mod_time']
     (ret, out, err) = call(cmd, errlog=False, cache=True)
     lines = []
     for line in out.split('\n'):
         l = line.split()
-        if len(l) < 2:
+        if len(l) < 3:
             continue
         if line[0] == '#':
             continue
-        x = [rcEnv.nodename, l[0], l[1], '']
+        try:
+            l[2] = datetime.datetime.fromtimestamp(int(l[2])).strftime("%Y-%m-%d %H:%M:%S")
+        except:
+            l[2] = ""
+        x = [rcEnv.nodename, l[0], l[1], '', t, l[2]]
         lines.append(x)
     return lines
 
