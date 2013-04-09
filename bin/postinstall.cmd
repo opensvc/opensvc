bf61d6e8b78bb14f91159361ef9f88c4299b6d04
Christophe Varoqui <christophe.varoqui@opensvc.com>
2013-04-09 16:50:51 +0200

    Simplify windows postinstall wrapper
    nested "if exist" do not work on win2003 cmd. Luckily, the nested
    test was not necessary.

--- bin/postinstall.cmd
+++ bin/postinstall.cmd
@@ -2,5 +2,5 @@
 set POSTSCRIPT=%1
 for /F "tokens=*" %%i in ('dir /s /l /b /ad /on "%ProgramFiles%\python2*"') do set PYTHONPATH=%%i\python.exe
 if not exist "%PYTHONPATH%" ( for /F "tokens=*" %%i in ('dir /s /l /b /ad /on "%SYSTEMDRIVE%\python2*"') do set PYTHONPATH=%%i\python.exe )
-if exist "%PYTHONPATH%" (if exist "%POSTSCRIPT%" ("%PYTHONPATH%" %POSTSCRIPT%)) else exit 1
+if exist "%PYTHONPATH%" ("%PYTHONPATH%" %POSTSCRIPT%) else exit 1
 exit 0
