2f312f122a6f5891d2c3891e1f7faaeb2bec6572
Christophe Varoqui <christophe.varoqui@opensvc.com>
2013-10-14 15:21:18 +0200

    Add IBM DS8xxx array flashcopy driver
    This driver support the syncresync action and status evaluation.
    
    It implements the following sanity checks and alerts:
    - verifies the flashcopy link is persistent
    - verifies the flashcopy link is recording
    - verifies the links defined in the resource are present in the
    array
    
    In auth.conf:
    
    [my-ds8xxx]
    type = ibmds
    username = opensvc
    pwfile = /opt/opensvc/var/opensvc.pwfile
    hmc1 = 1.2.3.4
    hmc2 = 1.2.3.5
    
    In service .env file:
    
    [sync#1]
    type = ibmdssnap
    array = my-ds8xxx
    pairs = 01DD:02DD 01DE:02DE

--- lib/rcIbmDs.py
+++ lib/rcIbmDs.py
@@ -2,6 +2,7 @@ import rcExceptions as ex
 import os
 import ConfigParser
 import tempfile
+import sys
 from subprocess import *
 
 pathlib = os.path.dirname(__file__)
@@ -12,10 +13,27 @@ pathvar = os.path.realpath(os.path.join(pathlib, '..', 'var'))
 if pathbin not in os.environ['PATH']:
     os.environ['PATH'] += ":"+pathbin
 
-def dscli(cmd, hmc1, hmc2, username, pwfile):
-    _cmd = ['/opt/ibm/dscli/dscli', '-hmc1', hmc1, '-hmc2', hmc2, '-user', username, '-pwfile', pwfile]
+def dscli(cmd, hmc1, hmc2, username, pwfile, log=None):
+    if len(hmc1) != 0:
+       _hmc1 = ['-hmc1', hmc1]
+    else:
+       _hmc1 = []
+    if len(hmc2) != 0:
+       _hmc2 = ['-hmc2', hmc2]
+    else:
+       _hmc2 = []
+    _cmd = ['/opt/ibm/dscli/dscli'] + _hmc1 + _hmc2 +['-user', username, '-pwfile', pwfile]
+    if log is not None:
+        log.info(cmd + ' | ' + ' '.join(_cmd))
     p = Popen(_cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
     out, err = p.communicate(input=cmd)
+    out = out.replace("dscli>", "")
+    err = err.replace("dscli>", "")
+    if log is not None:
+        if len(out) > 0:
+            log.info(out)
+        if len(err) > 0:
+            log.error(err)
     if p.returncode != 0:
         print >>sys.stderr, out, err
         raise ex.excError("dscli command execution error")
@@ -63,6 +81,11 @@ class IbmDss(object):
     def __iter__(self):
         return self
 
+    def get(self, array):
+        for o in self.arrays:
+            if o.name == array:
+                return o
+
     def next(self):
         if self.index == len(self.arrays):
             raise StopIteration
@@ -78,8 +101,8 @@ class IbmDs(object):
         self.hmc2 = hmc2
         self.keys = ['combo']
 
-    def dscli(self, cmd):
-        return dscli(cmd, self.hmc1, self.hmc2, self.username, self.pwfile)
+    def dscli(self, cmd, log=None):
+        return dscli(cmd, self.hmc1, self.hmc2, self.username, self.pwfile, log=log)
 
     def get_combo(self):
         cmd = """setenv -banner off -header on -format delim
