00f3c63fbb0ef3100ef4c05a893fad3cfefa91f0
Christophe Varoqui <christophe.varoqui@opensvc.com>
2014-01-19 17:50:53 +0100

    [compliance] keyval parser enhancements
    - suppport key unsetting
    - support keys ordering

--- var/compliance/com.opensvc/keyval_parser.py
+++ var/compliance/com.opensvc/keyval_parser.py
@@ -14,28 +14,64 @@ class Parser(object):
         self.data = {}
         self.changed = False
         self.nocf = False
+        self.keys = []
+        self.lastkey = '__lastkey__'
+        self.comments = {self.lastkey: []}
         self.load()
         self.bkp = path + '.' + str(datetime.datetime.now())
 
     def __str__(self):
         s = ""
-        for k in sorted(self.data.keys()):
-            v = self.data[k]
-            s += k + " " + str(v) + '\n'
+        for k in self.keys:
+            if k in self.comments:
+                s += '\n'.join(self.comments[k]) + '\n'
+            for v in self.data[k]:
+                s += k + " " + str(v) + '\n'
+        if len(self.comments[self.lastkey]) > 0:
+            s += '\n'.join(self.comments[self.lastkey]) + '\n'
         return s
 
-    def set(self, key, value):
-        self.data[key] = value
+    def truncate(self, key, max):
+        if key not in self.data:
+            return
+        n = len(self.data[key])
+        if n <= max:
+            return
+        self.data[key] = self.data[key][:max]
         self.changed = True
 
-    def unset(self, key):
-        if key in self.data:
-            del(self.data[key])
+    def set(self, key, value, instance=0):
+        if key not in self.data:
+            self.data[key] = [value]
+            self.keys.append(key)
+        elif instance >= len(self.data[key]):
+            extra = instance + 1 - len(self.data[key])
+            for i in range(len(self.data[key]), instance-1):
+                self.data[key].append(None)
+            self.data[key].append(value)
+        else:
+            self.data[key].insert(instance, value)
         self.changed = True
 
-    def get(self, key):
+    def unset(self, key, value=None):
         if key in self.data:
+            if value is not None and value.strip() != "":
+                self.data[key].remove(value)
+            else:
+                self.data[key] = []
+            if len(self.data[key]) == 0:
+                del(self.data[key])
+                if key in self.keys:
+                    self.keys.remove(key)
+        self.changed = True
+
+    def get(self, key, instance=0):
+        if key not in self.data:
+            return
+        if instance is None:
             return self.data[key]
+        if instance < len(self.data[key]):
+            return self.data[key][instance]
         return
 
     def load(self):
@@ -73,7 +109,8 @@ class Parser(object):
             with open(self.path, 'w') as f:
                 f.write(str(self))
             print "%s rewritten"%self.path
-        except:
+        except Exception as e:
+            print >>sys.stderr, e
             self.restore()
             raise ParserError()
 
@@ -81,8 +118,9 @@ class Parser(object):
         for line in buff.split("\n"):
             line = line.strip()
 
-            # discard comment line
-            if line.startswith('#'): 
+            # store comment line and continue
+            if line.startswith('#') or len(line) == 0: 
+                self.comments[self.lastkey].append(line)
                 continue
 
             # strip end-of-line comment
@@ -103,18 +141,25 @@ class Parser(object):
             key = l[0]
             value = line[len(key):].strip()
 
+            self.comments[key] = self.comments[self.lastkey]
+            self.comments[self.lastkey] = []
+
             try:
                 value = int(value)
             except:
                 pass
 
-            self.data[key] = value
+            if key not in self.keys:
+                self.keys.append(key)
+            if key not in self.data:
+                self.data[key] = []
+            self.data[key].append(value)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print >>sys.stderr, "wrong number of arguments"
         sys.exit(1)
-    o = Conf(sys.argv[1])
+    o = Parser(sys.argv[1])
     o.get("Subsystem")
     o.set("Subsystem", "foo")
     o.unset("PermitRootLogin")
