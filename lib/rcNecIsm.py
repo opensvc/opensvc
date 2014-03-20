919b254dc7b39e8b37a40af80521ac5f3727e5ab
Christophe Varoqui <christophe.varoqui@opensvc.com>
2014-03-20 14:45:16 +0100

    Support Optima arrays and iSMcc_view commandset in NecIsm library

--- lib/rcNecIsm.py
+++ lib/rcNecIsm.py
@@ -2,28 +2,37 @@ from rcUtilities import which, justcall
 import rcExceptions as ex
 import os
 
-class NecIsms(object):
+class Nec(object):
     arrays = []
-    def __init__(self, objects=[]):
-        self.objects = objects
-        if len(objects) > 0:
-            self.filtering = True
-        else:
-            self.filtering = False
-        self.index = 0
-        if which('iSMview') is None:
-            print 'Can not find iSMview programs in PATH'
-            raise ex.excError
-        out, err, ret = justcall(['iSMview', '-d'])
+    def get_bin(self):
+        candidates = ['iSMcc_view', 'iSMview']
+        for bin in candidates:
+            if which(bin) is not None:
+                self.bin = bin
+                break
+        if self.bin is None:
+            raise ex.excError('Can not find %s program in PATH' % ' or '.join(candidates))
+
+    def get_arrays(self):
+        cmd = [self.bin, '-d']
+        out, err, ret = justcall(cmd)
         if ret != 0:
-            print err
-            raise ex.excError
+            self.refresh_vollist()
+        out, err, ret = justcall(cmd)
+        if ret != 0:
+            raise ex.excError(err)
 
         """
 
 --- Disk Array List ---
 Product ID        Disk Array Name                   Resource State  Monitoring
 D1-10             D1_10                             ready           running
+
+
+--- Disk Array List ---
+Product ID        Disk Array Name                   Resource State
+Optima3600        Optima7_LMW                       ready
+
 
         """
         lines = out.split('\n')
@@ -35,28 +44,48 @@ D1-10             D1_10                             ready           running
             if 'Product ID' in line:
                 continue
             l = line.split()
-            if len(l) != 4:
+            if len(l) < 3:
                 continue
             if self.filtering and l[1] not in self.objects:
                 continue
             self.arrays.append(NecIsm(l[1]))
 
+
+class NecIsms(Nec):
+    def __init__(self, objects=[]):
+        self.objects = objects
+        if len(objects) > 0:
+            self.filtering = True
+        else:
+            self.filtering = False
+        self.index = 0
+        self.bin = None
+        self.get_bin()
+        self.get_arrays()
+
     def __iter__(self):
         return self
 
+    def refresh_vollist(self):
+        if which('iSMvollist') is None:
+            return
+        cmd = ['iSMvollist', '-r']
+        out, err, ret = justcall(cmd)
+ 
     def next(self):
         if self.index == len(self.arrays):
             raise StopIteration
         self.index += 1
         return self.arrays[self.index-1]
 
-class NecIsm(object):
+class NecIsm(Nec):
     def __init__(self, name):
         self.keys = ['all']
         self.name = name
+        self.get_bin()
 
     def _cmd(self, cmd):
-        cmd = ['iSMview'] + cmd + [self.name]
+        cmd = [self.bin] + cmd + [self.name]
         return justcall(cmd)
 
     def get_all(self):
