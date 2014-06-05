1cfeec9fbfbe7911e4bc7f608c58979a50d01b13
Christophe Varoqui <christophe.varoqui@opensvc.com>
2014-06-05 18:09:06 +0200

    Tru64 scsi3 pr needs to register each nexus
    Lookup each disk nexus with hwmgr and use scu to register
    the host key on them.

--- lib/resScsiReservOSF1.py
+++ lib/resScsiReservOSF1.py
@@ -38,7 +38,9 @@ class ScsiReserv(resScsiReserv.ScsiReserv):
                                           disabled=disabled, tags=tags,
                                           optional=optional)
         self.prtype = 'wero'
-        self.disks = map(lambda x: x.replace('/disk/', '/rdisk/'), disks)
+        self.disks = map(lambda x: str(x.replace('/disk/', '/rdisk/')), disks)
+        self.disk_id = {}
+        self.itn = {}
 
     def scsireserv_supported(self):
         if which('scu') is None:
@@ -48,28 +50,120 @@ class ScsiReserv(resScsiReserv.ScsiReserv):
     def ack_unit_attention(self, d):
         return 0
 
+    def get_disk_ids(self):
+        if len(self.disk_id) > 0:
+            return
+        cmd = [ 'hwmgr', 'show', 'scsi' ]
+        (ret, out, err) = self.call(cmd)
+        if ret != 0:
+            raise ex.excError(err)
+        for line in out.split('\n'):
+            v = line.split()
+            if len(v) < 7:
+                continue
+            if v[3] != "disk":
+                continue
+            if not v[7].startswith("dsk"):
+                continue
+            self.disk_id[v[7]+'c'] = v[0].strip(":")
+
+    def get_itns(self):
+        if len(self.itn) > 0:
+            return
+        self.get_disk_ids()
+        for disk in self.disk_id:
+            self.get_itn(disk)
+
+    def get_itn(self, disk):
+        if disk in self.itn:
+            return
+        self.itn[disk] = []
+        if disk not in self.disk_id:
+            return
+        id = self.disk_id[disk]
+        cmd = [ 'hwmgr', 'show', 'scsi', '-id', id, '-full' ]
+        (ret, out, err) = self.call(cmd)
+        if ret != 0:
+            return
+        for line in out.split('\n'):
+            v = line.split()
+            if len(v) != 4:
+                continue
+            data = {}
+            for i, s in enumerate(("bus", "target", "lun")):
+                try:
+                    j = int(v[i])
+                    data[s] = v[i]
+                except:
+                    continue
+            self.itn[disk].append(data)
+
+    def set_nexus(self, itn):
+        return "set nexus bus %(bus)s target %(target)s lun %(lun)s ; " % itn
+
+    def pipe_scu(self, cmd):
+        self.log.info(cmd + ' | scu')
+        p = Popen(['scu'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        out, err = p.communicate(input=cmd)
+        #if len(out) > 0:
+        #    self.log.info(out)
+        if len(err) > 0:
+            self.log.error(out)
+        if p.returncode:
+            raise ex.excError
+
     def disk_registered(self, disk):
         cmd = [ 'scu', '-f', disk, 'show', 'keys' ]
         (ret, out, err) = self.call(cmd)
         if ret != 0:
             self.log.error("failed to read registrations for disk %s" % disk)
-        if self.hostid in out:
-            return True
-        return False
+        if out.count(self.hostid) == 0:
+            return False
+        return True
 
     def disk_register(self, disk):
-        cmd = [ 'scu', '-f', disk, 'preserve', 'register', 'skey', self.hostid ]
-        (ret, out, err) = self.vcall(cmd)
-        if ret != 0:
-            self.log.error("failed to register key %s with disk %s" % (self.hostid, disk))
-        return ret
+        self.get_itns()
+        basedisk = os.path.basename(disk)
+        if basedisk not in self.itn:
+            self.log.error("no nexus information for disk %s"%disk)
+            return 1
+        r = 0
+        for itn in self.itn[basedisk]:
+            r += self.__disk_register(itn)
+        if r > 0:
+            r = 1
+        return r
+
+    def __disk_register(self, itn):
+        cmd = self.set_nexus(itn) + 'preserve register skey ' + self.hostid
+        try:
+            self.pipe_scu(cmd)
+        except ex.excError as e:
+            self.log.error("failed to register key %s with nexus %s" % (self.hostid, ':'.join(itn.values())))
+            return 1
+        return 0
 
     def disk_unregister(self, disk):
-        cmd = [ 'scu', '-f', disk, 'preserve', 'register', 'skey', '0', 'key', self.hostid ]
-        (ret, out, err) = self.vcall(cmd)
-        if ret != 0:
-            self.log.error("failed to unregister key %s with disk %s" % (self.hostid, disk))
-        return ret
+        self.get_itns()
+        basedisk = os.path.basename(disk)
+        if basedisk not in self.itn:
+            self.log.error("no nexus information for disk %s"%disk)
+            return 1
+        r = 0
+        for itn in self.itn[basedisk]:
+            r += self.__disk_unregister(itn)
+        if r > 0:
+            r = 1
+        return r
+
+    def __disk_unregister(self, itn):
+        cmd = self.set_nexus(itn) +  'preserve register skey 0 key ' + self.hostid
+        try:
+            self.pipe_scu(cmd)
+        except ex.excError as e:
+            self.log.error("failed to unregister key %s with nexus %s" % (self.hostid, ':'.join(itn.values())))
+            return 1
+        return 0
 
     def get_reservation_key(self, disk):
         cmd = [ 'scu', '-f', disk, 'show', 'reservation' ]
@@ -113,3 +207,6 @@ class ScsiReserv(resScsiReserv.ScsiReserv):
             self.log.error("failed to preempt reservation for disk %s" % disk)
         return ret
 
+if __name__ == "__main__":
+    o = ScsiReserv(rid="vg#0", disks=set(["/dev/disk/dsk46c"]))
+    print(o.get_reservation_key("/dev/disk/dsk46c"))
