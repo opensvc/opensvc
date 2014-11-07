33f1cbd04a514703b59108a3bee31b74825346ab
Arnaud Veron <arnaud.veron@opensvc.com>
2014-11-07 18:32:58 +0100

    Fix Solaris updatepkg process when zones are configured
    use adminfile to avoid questions during pkgadd
    check how opensvc package was installed (Global only or global + non global)
    
    If initial opensvc pkgadd was done without [-G], package is installed in every zone, global and non global, nodemgr updatepkg will remove opensvc from all zones, and reinstall in all zones
    If initial opensvc pkgadd was done with [-G], package is installed only in the current zone, nodemgr updatepkg will remove opensvc from current zone, and reinstall in current zone

--- lib/rcUpdatePkgSunOS.py
+++ lib/rcUpdatePkgSunOS.py
@@ -1,8 +1,25 @@
 from subprocess import *
-from rcUtilitiesSunOS import get_os_ver
+from rcUtilitiesSunOS import get_solaris_version
 import os
 
 repo_subdir = "sunos-pkg"
+
+def gen_adminfile():
+    filename = "/var/tmp/opensvc.adminfile"
+    f = open (filename, 'w')
+    f.write("mail=\n")
+    f.write("instance=overwrite\n")
+    f.write("partial=nocheck\n")
+    f.write("runlevel=nocheck\n")
+    f.write("idepend=nocheck\n")
+    f.write("rdepend=nocheck\n")
+    f.write("space=nocheck\n")
+    f.write("setuid=nocheck\n")
+    f.write("conflict=nocheck\n")
+    f.write("action=nocheck\n")
+    f.write("basedir=default\n")
+    f.close()
+    return filename
 
 def update(fpath):
     # check downloaded package integrity
@@ -13,16 +30,32 @@ def update(fpath):
     if p.returncode != 0:
         return 1
 
+    # check if initially installed with pkgadd -G
+    file = '/var/sadm/install/gz-only-packages'
+    GlobalOnly = False
+    if os.path.isfile(file):
+	f = open(file)
+	for line in f:
+	    if line.startswith("opensvc"):
+		print("OpenSVC package was previously installed with pkgadd -G\n")
+		GlobalOnly = True
+
     cmd = ['pkgrm', '-n', 'opensvc']
     print(' '.join(cmd))
     p = Popen(cmd)
     p.communicate()
     if p.returncode != 0:
         return 1
-    if get_os_ver() < 10:
+    osver = get_solaris_version()
+    if osver < 10.0:
         opts = ''
     else:
-        opts = '-G'
-    cmd = 'echo y | pkgadd %s -d %s all' % (opts, fpath)
+	if GlobalOnly is True:
+            opts = '-G'
+        else:
+	    opts = ''
+    admin = gen_adminfile()
+    opts += " -a %s " % admin
+    cmd = 'pkgadd %s -d %s all' % (opts, fpath)
     print(cmd)
     return os.system(cmd)
