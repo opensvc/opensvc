ffe53a204de7929b18b2073b6e34ae1c1c712139
Arnaud Veron <arnaud.veron@opensvc.com>
2014-11-07 18:15:44 +0100

    Add function to provide uniform Solaris version number
    Solaris 2.6 is SunOS 5.6 : osver = 6.0
    Solaris 7 is SunOS 5.7   : osver = 7.0
    Solaris 8 is SunOS 5.8   : osver = 8.0
    ...
    Solaris 11 is SunOS 5.11 : osver = 11.0
    Solaris 11U1 is SunOS 5.11 : osver = 11.1
    Solaris 11U2 is SunOS 5.11 : osver = 11.2
    
    uname -v reports "Generic_147148-26" up to Solaris 10
    uname -v reports "11.x" starting from Solaris 11

--- lib/rcUtilitiesSunOS.py
+++ lib/rcUtilitiesSunOS.py
@@ -20,3 +20,33 @@ def get_os_ver():
     except:
         osver = 0
     return osver
+
+# Solaris 2.6 is SunOS 5.6 : osver = 6.0
+# Solaris 7 is SunOS 5.7   : osver = 7.0
+# Solaris 8 is SunOS 5.8   : osver = 8.0
+# ...
+# Solaris 11 is SunOS 5.11 : osver = 11.0
+# Solaris 11U1 is SunOS 5.11 : osver = 11.1
+# Solaris 11U2 is SunOS 5.11 : osver = 11.2
+def get_solaris_version():
+    cmd = ['uname', '-r']
+    out, err, ret = justcall(cmd)
+    if ret != 0:
+        return self.undef
+    lines = out.split('\n')
+    if len(lines) == 0:
+        return self.undef
+    try:
+        base, osver = lines[0].split('.')
+	osver = int(osver)
+    except:
+        osver = 0
+    
+    if osver >= '11':
+	cmd = ['uname', '-v']
+	out, err, ret = justcall(cmd)
+	if ret == 0:
+            lines = out.split('\n')
+	    osver = lines[0]
+
+    return float(osver)
