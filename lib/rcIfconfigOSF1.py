76b71ceab1fb8dd999ca9b8ce7d909a62fa481b1
Christophe Varoqui <christophe.varoqui@opensvc.com>
2015-01-06 18:08:48 +0100

    Fix OSF1 ip parsing
    Correctly init the ifconfig class so that the mcast_data is
    correctly initialized before use.

--- lib/rcIfconfigOSF1.py
+++ lib/rcIfconfigOSF1.py
@@ -38,7 +38,7 @@ def ipv4_bitmask(s):
 class ifconfig(rcIfconfig.ifconfig):
 
     def __init__(self, mcast=False):
-        self.intf = []
+        rcIfconfig.ifconfig.__init__(self, mcast=mcast)
         out = Popen(['ifconfig', '-a'], stdin=None, stdout=PIPE,stderr=PIPE,close_fds=True).communicate()[0]
         self.parse(out)
 
