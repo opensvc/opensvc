7693baf6c95ba5465bfa9b3a2474fe2b80066ef2
Christophe Varoqui <christophe.varoqui@opensvc.com>
2015-01-07 08:00:27 +0100

    Add 'nodemgr updatepkg' support on Tru64
    Use a simple tar file to avoid the kits mess.
    
    Add the tarfile creation script, and add more candidates to
    python interpreter paths in postinstall.

---
+++ lib/rcUpdatePkgOSF1.py
@@ -0,0 +1,27 @@
+from subprocess import *
+import os
+import tarfile
+
+repo_subdir = "tar"
+
+def update(fpath):
+    oldpath = os.getcwd()
+    os.chdir(os.sep)
+    tar = tarfile.open(fpath)
+    try:
+        tar.extractall()
+        tar.close()
+    except:
+        try:
+            os.unlink(fpath)
+        except:
+            pass
+        print("failed to unpack", file=sys.stderr)
+        return 1
+    try:
+        os.unlink(fpath)
+    except:
+        pass
+
+    cmd = '/opt/opensvc/bin/python /opt/opensvc/bin/postinstall'
+    return os.system(cmd)
