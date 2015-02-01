dec1f506c0b5eaf7f5b02ca28da34f333e228f0f
Christophe Varoqui <christophe.varoqui@opensvc.com>
2015-02-01 16:53:51 +0100

    Fix svcmgr create
    svcBuilder::create() must not return a dict

--- lib/svcBuilder.py
+++ lib/svcBuilder.py
@@ -2923,6 +2923,7 @@ def create(svcname, resources=[], interactive=False, provision=False):
         os.makedirs(svcinitdir)
     fix_app_link(svcname)
     fix_exe_link(os.path.join('..', 'bin', 'svcmgr'), svcname)
+    return {"ret": 0}
 
 def update(svcname, resources=[], interactive=False, provision=False):
     fix_default_section(svcname)
