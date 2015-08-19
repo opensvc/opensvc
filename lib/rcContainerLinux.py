09c809bef991685e3fbcbfe92cf099cc01829bc8
Christophe Varoqui <christophe.varoqui@opensvc.com>
2015-08-19 23:21:53 +0200

    Refactor the rcContainerLinux to support systemd-style cgroup mounts
    systemd mounts the cgroup fs once per option (memory, cpu, cpuset,
    ...), plus one for its own use. The current code only supported
    a single cgroup fs mount, with all options active.
    
    This patch implement support for the systemd-style cgroup fs mounts,
    so that containerization works on recent Linux distributions,
    shipping with systemd.
    
    It also implements the "service -> subset -> rid" cgroup hierarchy
    support and registers cgroups and tasks with systemd, so that
    systemd-cgls and systemd-cgtop correctly show opensvc services
    hierachies and resource usage.
    
    Example systemd-cgls:
    
    ├─testmd
    │ ├─app:ga2
    │ │ ├─app.4
    │ │ │ └─32110 dd if=/dev/urandom of=/dev/null bs=1M count=1000
    │ │ └─app.3
    │ │   └─32119 dd if=/dev/urandom of=/dev/null bs=1M count=1000
    │ └─app:ga1
    │   ├─app.2
    │   │ └─32035 dd if=/dev/urandom of=/dev/null bs=1M count=1000
    │   └─app.1
    │     └─32043 dd if=/dev/urandom of=/dev/null bs=1M count=1000
    
    Example systemd-cgtop with cpu shares capping on the subsets:
    
    Path                                     Tasks   %CPU   Memory  Input/s Output/s
    
    /                                           98  211.9     2.8G        -        -
    /testmd                                      -  196.8     1.0M        -        -
    /testmd/app:ga2                              -  112.1   516.0K        -        -
    /testmd/app:ga2/app.4                        1   89.9   364.0K        -        -
    /testmd/app:ga1                              -   84.7   508.0K        -        -
    /testmd/app:ga1/app.2                        1   69.9   284.0K        -        -
    /testmd/app:ga2/app.3                        1   22.2   152.0K        -        -
    /testmd/app:ga1/app.1                        1   14.8    96.0K        -        -
    
    Defined capping:
    
    [subset#app:ga1]
    container_cpu_shares = 10
    
    [subset#app:ga2]
    container_cpu_shares = 20

--- lib/rcContainerLinux.py
+++ lib/rcContainerLinux.py
@@ -4,7 +4,7 @@ from rcUtilities import justcall
 
 default_cgroup_mntpt = '/cgroup'
 
-def get_cgroup_mntpt():
+def get_cgroup_mntpt(t):
     (out,err,ret) = justcall(['mount'])
     if ret != 0:
         return None
@@ -12,7 +12,7 @@ def get_cgroup_mntpt():
         l = line.split()
         if len(l) < 6:
             continue
-        if l[4] == 'cgroup':
+        if l[4] == 'cgroup' and t in l[5]:
             return l[2]
     return None
 
@@ -34,114 +34,151 @@ def cgroup_capable(res):
     res.log.info("system does not support containerization")
     return False
 
-def set_cgroup(res, name, value, force=False):
+def set_task(o, t):
+    cgp = get_cgroup_path(o, t)
+    path = os.path.join(cgp, "tasks")
+    pid = str(os.getpid())
+    with open(path, 'r') as f:
+        buff = f.read()
+    if pid in buff.split():
+        return
+    with open(path, 'w') as f:
+        f.write(pid)
+
+def set_cgroup(o, t, name, key, force=False):
+    if not hasattr(o, "containerize_settings"):
+        return
+    if key not in o.containerize_settings:
+        return
+    value = o.containerize_settings[key]
+    cgp = get_cgroup_path(o, t)
     if value is None:
         return
-    if not force and get_cgroup(res, name).strip() == value:
+    if not force and get_cgroup(o, t, name).strip() == str(value):
         return
-    path = os.path.join(res.cgroup, name)
+    path = os.path.join(cgp, name)
     if not os.path.exists(path):
-        res.log.error("can not find %s"%path)
-        raise ex.excError
-    res.log.info('/bin/echo %s > %s'%(value, path))
-    with open(path, 'w') as f:
-        f.write(str(value))
+        raise ex.excError("can not find %s"%path)
+    if hasattr(o, "log"):
+        log = o.log
+    elif hasattr(o, "svc"):
+        log = o.svc.log
+    try:
+        with open(path, 'w') as f:
+            f.write(str(value))
+        log.info('/bin/echo %s > %s'%(value, path))
+    except Exception as e:
+        log.warning("failed to set container setting %s to %s" % (value, path))
 
-def get_cgroup(res, name):
-    path = os.path.join(res.cgroup, name)
+def get_cgroup(o, t, name):
+    cgp = get_cgroup_path(o, t)
+    path = os.path.join(cgp, name)
     if not os.path.exists(path):
-        res.log.error("can not find %s"%path)
-        raise ex.excError
+        raise ex.excError("can not find %s"%path)
     with open(path, 'r') as f:
         buff = f.read()
     return buff
 
-def set_mem_cgroup(res):
-    if hasattr(res.svc, 'container_mem_limit'):
-        mem_limit = int(res.svc.container_mem_limit)
+def set_mem_cgroup(o):
+    if not hasattr(o, "containerize_settings"):
+        return
+
+    if 'mem_limit' in o.containerize_settings:
+        mem_limit = int(o.containerize_settings['mem_limit'])
     else:
         mem_limit = None
 
-    if hasattr(res.svc, 'container_vmem_limit'):
-        vmem_limit = int(res.svc.container_vmem_limit)
+    if 'vmem_limit' in o.containerize_settings:
+        vmem_limit = int(o.containerize_settings['vmem_limit'])
     else:
         vmem_limit = None
+
+    if hasattr(o, "log"):
+        log = o.log
+    elif hasattr(o, "svc"):
+        log = o.svc.log
 
     #
     # validate memory limits sanity and order adequately the resize
     # depending on increase/decrease of limits
     #
+    try:
+        cur_vmem_limit = int(get_cgroup(o, 'memory', 'memory.max_usage_in_bytes'))
+        vmem_key = 'memory.max_usage_in_bytes'
+    except:
+        cur_vmem_limit = int(get_cgroup(o, 'memory', 'memory.memsw.limit_in_bytes'))
+        vmem_key = 'memory.memsw.limit_in_bytes'
+
     if mem_limit is not None and vmem_limit is not None:
         if mem_limit > vmem_limit:
-            res.log.error("container_vmem_limit must be greater than container_mem_limit")
+            log.error("container_vmem_limit must be greater than container_mem_limit")
             raise ex.excError
-        cur_vmem_limit = int(get_cgroup(res, 'memory.memsw.limit_in_bytes'))
         if mem_limit > cur_vmem_limit:
-            set_cgroup(res, 'memory.memsw.limit_in_bytes', vmem_limit)
-            set_cgroup(res, 'memory.limit_in_bytes', mem_limit)
+            set_cgroup(o, 'memory', 'memory.memsw.limit_in_bytes', 'vmem_limit')
+            set_cgroup(o, 'memory', vmem_key, 'mem_limit')
         else:
-            set_cgroup(res, 'memory.limit_in_bytes', mem_limit)
-            set_cgroup(res, 'memory.memsw.limit_in_bytes', vmem_limit)
+            set_cgroup(o, 'memory', 'memory.limit_in_bytes', 'mem_limit')
+            set_cgroup(o, 'memory', vmem_key, 'vmem_limit')
     elif mem_limit is not None:
-        cur_vmem_limit = int(get_cgroup(res, 'memory.memsw.limit_in_bytes'))
         if mem_limit > cur_vmem_limit:
-            res.log.error("container_mem_limit must not be greater than current container_vmem_limit (%d)"%cur_vmem_limit)
+            log.error("container_mem_limit must not be greater than current container_vmem_limit (%d)"%cur_vmem_limit)
             raise ex.excError
-        set_cgroup(res, 'memory.limit_in_bytes', mem_limit)
+        set_cgroup(o, 'memory', 'memory.limit_in_bytes', 'mem_limit')
     elif vmem_limit is not None:
-        cur_mem_limit = int(get_cgroup(res, 'memory.limit_in_bytes'))
+        cur_mem_limit = int(get_cgroup(o, 'memory', 'memory.limit_in_bytes'))
         if vmem_limit < cur_mem_limit:
-            res.log.error("container_vmem_limit must not be lesser than current container_mem_limit (%d)"%cur_mem_limit)
+            log.error("container_vmem_limit must not be lesser than current container_mem_limit (%d)"%cur_mem_limit)
             raise ex.excError
-        set_cgroup(res, 'memory.memsw.limit_in_bytes', vmem_limit)
+        set_cgroup(o, 'memory', vmem_key, 'vmem_limit')
+
+def get_cgroup_path(o, t):
+    cgroup_mntpt = get_cgroup_mntpt(t)
+    if hasattr(o, "svcname"):
+        svcname = o.svcname
+    else:
+        svcname = o.svc.svcname
+    if cgroup_mntpt is None:
+        raise ex.excError("cgroup fs with option %s is not mounted" % t)
+    elements = [cgroup_mntpt, svcname]
+    if hasattr(o, "rset") and o.rset is not None:
+        elements.append(o.rset.rid)
+    if hasattr(o, "rid") and o.rid is not None:
+        elements.append(o.rid.replace("#", "."))
+    cgp = os.path.join(*elements)
+    if hasattr(o, "log"):
+        log = o.log
+    elif hasattr(o, "svc"):
+        log = o.svc.log
+    if not os.path.exists(cgp):
+        log.info("create cgroup %s" % cgp)
+        os.makedirs(cgp)
+    return cgp
 
 def containerize(res):
-    if res.svc.svcmode == 'lxc':
-        return
     if not cgroup_capable(res):
         return
 
-    cgroup_mntpt = get_cgroup_mntpt()
-    if cgroup_mntpt is None:
-        cgroup_mntpt = default_cgroup_mntpt
-        if not os.path.exists(cgroup_mntpt):
-            res.log.info('mkdir %s'%cgroup_mntpt)
-            os.makedirs(cgroup_mntpt)
-        (ret, out, err) = res.vcall(['mount', '-t', 'cgroup', 'none', cgroup_mntpt])
-        if ret != 0:
-            raise ex.excError
+    _containerize(res.svc)
+    _containerize(res.rset)
+    _containerize(res)
 
-    res.cgroup = os.path.join(cgroup_mntpt, res.svc.svcname)
-    if not os.path.exists(res.cgroup):
-        os.makedirs(res.cgroup)
-
-    if hasattr(res.svc, 'container_cpus'):
-        v_cpus = str(res.svc.container_cpus)
-    else:
-        # default to all cpus
-        import multiprocessing
-        v_cpus = '0-%d'%(multiprocessing.cpu_count()-1)
-
-    if hasattr(res.svc, 'container_mems'):
-        v_mems = str(res.svc.container_mems)
-    else:
-        # default to all mems
-        v_mems = '0'
-
-    if hasattr(res.svc, 'container_cpu_share'):
-        cpu_share = res.svc.container_cpu_share
-    else:
-        cpu_share = None
-
-    pid = os.getpid()
-
+def _containerize(o):
+    if o is None:
+        return
     try:
-        set_cgroup(res, 'cpuset.cpus', v_cpus)
-        set_cgroup(res, 'cpu.shares', cpu_share)
-        set_cgroup(res, 'cpuset.mems', v_mems)
-        set_mem_cgroup(res)
-        set_cgroup(res, 'tasks', pid, force=True)
-    except:
-        res.log.error("containerization in '%s' cgroup failed"%res.svc.svcname)
-        raise ex.excError
-    res.log.info("container '%s' settings: cpu[%s], mem[%s]"%(res.svc.svcname, v_cpus, v_mems))
+        set_task(o, 'systemd')
+        set_task(o, 'cpu')
+        set_task(o, 'cpuset')
+        set_task(o, 'memory')
+        set_task(o, 'blkio')
+        set_task(o, 'freezer')
+        set_cgroup(o, 'cpuset', 'cpuset.cpus', 'cpus')
+        set_cgroup(o, 'cpu', 'cpu.shares', 'cpu_shares')
+        set_cgroup(o, 'cpuset', 'cpuset.mems', 'mems')
+        set_mem_cgroup(o)
+    except Exception as e:
+        try:
+            name = o.svcname
+        except:
+            name = o.rid
+        raise ex.excError("containerization in '%s' cgroup failed: %s"%(name, str(e)))
