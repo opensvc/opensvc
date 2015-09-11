opensvc/opensvc

commit 0eab9b9156984eac3802f2e4f25f2c115e7b33d8

Don't add to linux vg devlist lv with no devpath
Those trigger legitimate errors in devs_to_disks() called by the
resource disklist function.

lib/resVgLinux.py
@@ -226,7 +226,11 @@ class Vg(resDg.Dg):
         (ret, out, err) = self.call(cmd, cache=True)
         if ret == 0:
             lvs = out.split()
-            devs = map(lambda x: "/dev/"+self.name+"/"+x, lvs)
+            for lv in lvs:
+                devs = []
+                lvp = "/dev/"+self.name+"/"+lv
+                if os.path.exists(lvp):
+                    devs.append(lvp)
             self.devs |= set(devs)
 
         if len(self.devs) > 0:

commit 295dd33f02f56db5e65506ba897659ec553b67bd

Fix devlist() regression on Linux vg
The regression was introduced by commit
0eab9b9156984eac3802f2e4f25f2c115e7b33d8

The effect was that only the last dev was appended to the devlist.

Reported by Stephane Lebihan.
Fix authored by Sébastien Guiheneuf.

lib/resVgLinux.py
@@ -233,8 +233,8 @@ class Vg(resDg.Dg):
         (ret, out, err) = self.call(cmd, cache=True)
         if ret == 0:
             lvs = out.split()
+            devs = []
             for lv in lvs:
-                devs = []
                 lvp = "/dev/"+self.name+"/"+lv
                 if os.path.exists(lvp):
                     devs.append(lvp)