18ce812b2de7491cad9cfb61f0ad57251b6dc389
Christophe Varoqui <christophe.varoqui@opensvc.com>
2016-04-12 07:36:47 +0200

    Linux Loop device provisioner enhancements
    - use the rcUtilities size convert function
    - round to 512 bytes
    - seek size-1 instead of size to create a loop of the exact
    requested size
    - trap provisioning parameter lookup errors and display them
    nicely (ie, not in a stack file)

--- lib/provLoopLinux.py
+++ lib/provLoopLinux.py
@@ -1,25 +1,29 @@
 from provisioning import Provisioning
 import os
 import rcExceptions as ex
+from rcUtilities import convert_size
 
 class ProvisioningLoop(Provisioning):
     def __init__(self, r):
         Provisioning.__init__(self, r)
-        self.path = r.svc.config.get(self.r.rid, 'file')
-        self.size = r.svc.config.get(self.r.rid, 'size')
 
     def provisioner(self):
+        try:
+            self.path = self.r.svc.config.get(self.r.rid, 'file')
+            self.size = self.r.svc.config.get(self.r.rid, 'size')
+        except Exception as e:
+            raise ex.excError(str(e))
         d = os.path.dirname(self.path)
         try:
             if not os.path.exists(d):
                 self.r.log.info("create directory %s"%d)
                 os.makedirs(d)
             with open(self.path, 'w') as f:
-                self.r.log.info("create file %s, size %sMB"%(self.path, self.size))
-                f.seek(int(self.size)*1024*1024)
+                self.r.log.info("create file %s, size %s"%(self.path, self.size))
+                f.seek(convert_size(self.size, _to='b', _round=512)-1)
                 f.write('\0')
-        except:
-            self.r.log.error("Failed to create %s"%self.path)
+        except Exception as e:
+            self.r.log.error("Failed to create %s: %s"% (self.path, str(e)))
             raise ex.excError
 
         self.remove_keywords(["size"])
