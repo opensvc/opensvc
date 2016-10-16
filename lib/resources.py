a3ae59332f6c17e697810b209e1d786620d94bc9
Christophe Varoqui <christophe.varoqui@opensvc.com>
2016-10-16 18:11:32 +0200

    Allow the presync action on resources tagged "noaction"
    So that the sync#i0 sync actions do not fail because files
    contributed by resources tagged "noaction" are not present due
    to presync being blocked by the tag.

--- lib/resources.py
+++ lib/resources.py
@@ -8,6 +8,10 @@ import time
 import shlex
 from rcGlobalEnv import rcEnv
 
+allow_action_with_noaction = [
+  "presync",
+]
+
 class Resource(object):
     """Define basic resource
     properties: type, optional=optional, disabled=disabled, tags=tags
@@ -191,7 +195,9 @@ class Resource(object):
             self.log.debug('skip encap resource action: action=%s res=%s'%(action, self.rid))
             return
 
-        if 'noaction' in self.tags and not hasattr(self, "delayed_noaction"):
+        if 'noaction' in self.tags and \
+           not hasattr(self, "delayed_noaction") and \
+           not action in allow_action_with_noaction:
             self.log.debug('skip resource action (noaction tag): action=%s res=%s'%(action, self.rid))
             return
 
