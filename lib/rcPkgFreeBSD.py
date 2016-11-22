c32a40277e3effc0bd8f186b60fd03286e04c7a7
Christophe Varoqui <christophe.varoqui@opensvc.com>
2016-11-22 12:36:00 +0100

    Implement 'pkg'-based pushpkg on FreeBSD version where available
    And use the generic cache.

--- lib/rcPkgFreeBSD.py
+++ lib/rcPkgFreeBSD.py
@@ -1,13 +1,19 @@
-from rcUtilities import call, which
+from rcUtilities import justcall, which, cache
 from rcGlobalEnv import rcEnv
 
 def listpkg():
+    lines = list_pkg_info()
+    lines += list_pkg_query()
+    return lines
+
+@cache("pkg_info")
+def list_pkg_info():
     if which('pkg_info') is None:
         return []
     cmd = ['pkg_info']
-    (ret, out, err) = call(cmd, errlog=False, cache=True)
+    out, err, ret = justcall(cmd)
     lines = []
-    for line in out.split('\n'):
+    for line in out.splitlines():
         l = line.split()
         if len(l) < 2:
             continue
@@ -18,5 +24,21 @@ def listpkg():
         lines.append(x)
     return lines
 
+@cache("pkg_query")
+def list_pkg_query():
+    if which('pkg') is None:
+        return []
+    cmd = ['pkg', 'query', '-a', '%n;;%v;;%q']
+    out, err, ret = justcall(cmd)
+    lines = []
+    for line in out.splitlines():
+        l = line.split(';;')
+        if len(l) < 3:
+            continue
+        x = [rcEnv.nodename] + l
+        lines.append(x)
+    return lines
+
 def listpatch():
     return []
+
