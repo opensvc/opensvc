8557d11bff086e34ce88f486d030a84962b6dca0
Christophe Varoqui <christophe.varoqui@opensvc.com>
2016-11-24 08:08:13 +0100

    Use setsid() instead of setpgrp() on stop/shutdown detaching
    lxc command can fail in a way their Popen() and the detached
    agent processes end up in T state while the detaching process
    still wait() for its termination.
    
    setsid() on the detached process let it manager its own children
    signals better.

--- lib/svcmgr.py
+++ lib/svcmgr.py
@@ -354,7 +354,7 @@ def main():
     if not options.daemon and (action.startswith("stop") or action in ("shutdown", "unprovision", "switch")):
         try:
             import subprocess
-            p = subprocess.Popen([sys.executable] + sys.argv + ["--daemon"], stdout=None, stderr=None, stdin=None, close_fds=True, cwd=os.sep, preexec_fn=os.setpgrp)
+            p = subprocess.Popen([sys.executable] + sys.argv + ["--daemon"], stdout=None, stderr=None, stdin=None, close_fds=True, cwd=os.sep, preexec_fn=os.setsid)
             p.wait()
             err = p.returncode
         except (ex.excSignal, KeyboardInterrupt) as e:
