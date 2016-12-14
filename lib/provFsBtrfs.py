3a914ac7bd317b5198824b806379df0e219ea25b
Christophe Varoqui <christophe.varoqui@opensvc.com>
2016-12-14 22:16:20 +0100

    Fix btrfs provisionner with multiple subvols
    When multiple subvols of the same fs need provisionning, stick
    to the first fs resource label that was written to the btrfs
    metadata.
    
    Also be more careful to umount the temp mount in case exceptions
    are raised between mount and umount.

--- lib/provFsBtrfs.py
+++ lib/provFsBtrfs.py
@@ -21,7 +21,14 @@ class ProvisioningFs(provFs.ProvisioningFs):
     def label(self):
         return self.r.svc.svcname + '.' + self.r.rid.replace("#", ".")
 
-    def get_subvol(self):
+    def current_label(self, mnt):
+        cmd = ["btrfs", "filesystem", "label", mnt]
+        ret, out, err = self.r.call(cmd, errlog=False)
+        if ret == 0 and len(out.strip()) > 0:
+            return out.strip()
+
+    @lazy
+    def subvol(self):
         l = self.r.mntOpt.split(",")
         for e in l:
             if not e.startswith("subvol="):
@@ -34,17 +41,24 @@ class ProvisioningFs(provFs.ProvisioningFs):
         self.r.vcall(cmd)
         os.removedirs(mnt)
 
-    def write_label(self):
-        self.r.svc.config.set(self.r.rid, "dev", "LABEL="+self.raw_label)
+    def write_label(self, mnt):
+        current_label = self.current_label(mnt)
+        if current_label is not None:
+            label = current_label
+            raw_label = current_label.replace(self.r.svc.svcname, "{svcname}")
+        else:
+            label = self.label
+            raw_label = self.raw_label
+        self.r.svc.config.set(self.r.rid, "dev", "LABEL="+raw_label)
         self.r.svc.write_config()
-        self.r.device = "LABEL="+self.label
-        self.wait_label()
+        self.r.device = "LABEL="+label
+        self.wait_label(label)
 
-    def wait_label(self):
+    def wait_label(self, label):
         if which("findfs") is None:
             self.r.log.info("findfs program not found, wait arbitrary 20 seconds for label to be usable")
             time.sleep(20)
-        cmd = ["findfs", "LABEL="+self.label]
+        cmd = ["findfs", "LABEL="+label]
         for i in range(20):
             out, err, ret = justcall(cmd)
             self.r.log.debug("%s\n%s\n%s" % (" ".join(cmd), out, err))
@@ -61,28 +75,25 @@ class ProvisioningFs(provFs.ProvisioningFs):
             raise ex.excError
 
     def create_subvol(self):
-        subvol = self.get_subvol()
-        if subvol is None:
+        if self.subvol is None:
             return
         mnt = tempfile.mkdtemp()
 
         self.mount(mnt)
-        self.write_label()
-
-        # create subvol
-        path = os.path.join(mnt, subvol)
-
-        if os.path.exists(path):
+        try:
+            self.write_label(mnt)
+            self._create_subvol(mnt)
+        finally:
             self.cleanup(mnt)
-            return
 
+    def _create_subvol(self, mnt):
+        path = os.path.join(mnt, self.subvol)
+        if os.path.exists(path):
+            return
         cmd = ["btrfs", "subvol", "create", path]
         ret, out, err = self.r.vcall(cmd)
         if ret != 0:
-            self.cleanup(mnt)
             raise ex.excError
-
-        self.cleanup(mnt)
 
     def provisioner(self):
         self.r.device = conf_get_string_scope(self.r.svc, self.r.svc.config, self.r.rid, "dev")
