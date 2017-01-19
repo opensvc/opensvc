b5727f27dc8c7c5878de2352ae04fb17ec0ccb89
Christophe Varoqui <christophe.varoqui@opensvc.com>
2017-01-19 15:52:31 +0100

    Fix the pinfo() and perror() when args is not list nor string

--- var/compliance/com.opensvc/comp.py
+++ var/compliance/com.opensvc/comp.py
@@ -38,13 +38,17 @@ class EndRecursion(Exception):
      pass
 
 def pinfo(*args, **kwargs):
-    if len(args) == 0 or len(args[0]) == 0:
+    if is_string(args) and len(args):
+        return
+    if isinstance(args, list) and (len(args) == 0 or len(args[0]) == 0):
         return
     kwargs["file"] = sys.stdout
     print(*args, **kwargs)
 
 def perror(*args, **kwargs):
-    if len(args) == 0 or len(args[0]) == 0:
+    if is_string(args) and len(args):
+        return
+    if isinstance(args, list) and (len(args) == 0 or len(args[0]) == 0):
         return
     kwargs["file"] = sys.stderr
     print(*args, **kwargs)
