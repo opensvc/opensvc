3e97b6c2e00f11cea6d8723366f6e2752ca38364
Christophe Varoqui <christophe.varoqui@opensvc.com>
2017-03-10 17:23:08 +0100

    Commit the symmetrix array driver WIP
    Working commands so far:
    
    * list tdevs
    * list views
    * list pools
    * add tdev --size <size>
    * del tdev
    * resize tdev --size <size>

--- lib/rcSymmetrix.py
+++ lib/rcSymmetrix.py
@@ -8,7 +8,7 @@ from xml.etree.ElementTree import XML, fromstring
 
 import rcExceptions as ex
 from rcGlobalEnv import rcEnv, Storage
-from rcUtilities import justcall, which
+from rcUtilities import justcall, which, convert_size
 from rcOptParser import OptParser
 from optparse import Option
 
@@ -29,6 +29,9 @@ OPT = Storage({
     "pools": Option(
         "--pools", action="append", dest="pools",
         help="A pool hosting the TDEV. Multiple --pools can be set."),
+    "size": Option(
+        "--size", action="store", dest="size",
+        help="The disk size, expressed as a size expression like 1g, 100mib, ..."),
 })
 
 GLOBAL_OPTS = [
@@ -40,41 +43,59 @@ DEPRECATED_ACTIONS = []
 ACTIONS = {
     "Add actions": {
         "add_disk": {
-            "msg": "Add and present a thin device",
+            "msg": "Add and present a thin device.",
             "options": [
-                OPT.name,
                 OPT.size,
                 OPT.mappings,
                 OPT.pools,
             ],
         },
+        "add_tdev": {
+            "msg": "Add a thin device. No pool bindings, no masking.",
+            "options": [
+                OPT.size,
+            ],
+        },
     },
     "Delete actions": {
         "del_disk": {
-            "msg": "Unpresent and delete a thin device",
+            "msg": "Unpresent and delete a thin device.",
             "options": [
-                OPT.name,
+                OPT.dev,
+            ],
+        },
+        "del_tdev": {
+            "msg": "Delete a thin device. No unmasking.",
+            "options": [
                 OPT.dev,
             ],
         },
     },
     "Modify actions": {
-        "resize_disk": {
-            "msg": "Resize a thin device",
+        "resize_tdev": {
+            "msg": "Resize a thin device.",
             "options": [
-                OPT.name,
                 OPT.dev,
                 OPT.size,
             ],
         },
     },
     "List actions": {
+        "list_pools": {
+            "msg": "List device pools.",
+        },
+        "list_tdevs": {
+            "msg": "List thin devices.",
+        },
+        "list_views": {
+            "msg": "List views, eg. groups of initiators/targets/devices.",
+        },
     },
 }
 
 
 class Arrays(object):
-    syms = []
+    arrays = []
 
     def find_symcli_path(self):
         symcli_bin = which("symcli")
@@ -154,16 +175,21 @@ class Arrays(object):
             for symm in tree.getiterator('Symm_Info'):
                 model = symm.find('model').text
                 if model.startswith('VMAX'):
-                    self.syms.append(Vmax(name, symcli_path, symcli_connect, username, password))
+                    self.arrays.append(Vmax(name, symcli_path, symcli_connect, username, password))
                 elif 'DMX' in model or '3000-M' in model:
-                    self.syms.append(Dmx(name, symcli_path, symcli_connect, username, password))
+                    self.arrays.append(Dmx(name, symcli_path, symcli_connect, username, password))
                 else:
                     print("unsupported sym model: %s" % model, file=sys.stderr)
 
         del(conf)
 
+    def get_array(self, name):
+        for array in self.arrays:
+            if array.sid == name:
+                return array
+
     def __iter__(self):
-        for array in self.syms:
+        for array in self.arrays:
             yield(array)
 
 
@@ -194,22 +220,29 @@ class Sym(object):
 	elif "SYMCLI_CONNECT" in os.environ:
             del os.environ["SYMCLI_CONNECT"]
 
-    def symcmd(self, cmd):
+    def symcmd(self, cmd, xml=True):
         self.set_environ()
-        cmd += ['-sid', self.sid, '-output', 'xml_element']
+        cmd += ['-sid', self.sid]
+        if xml:
+            cmd += ['-output', 'xml_element']
+        print(cmd)
         return justcall(cmd)
 
-    def symcfg(self, cmd):
+    def symcfg(self, cmd, xml=True):
         cmd = ['/usr/symcli/bin/symcfg'] + cmd
-        return self.symcmd(cmd)
+        return self.symcmd(cmd, xml=xml)
 
-    def symdisk(self, cmd):
+    def symdisk(self, cmd, xml=True):
         cmd = ['/usr/symcli/bin/symdisk'] + cmd
-        return self.symcmd(cmd)
+        return self.symcmd(cmd, xml=xml)
 
-    def symdev(self, cmd):
+    def symconfigure(self, cmd, xml=True):
+        cmd = ['/usr/symcli/bin/symconfigure'] + cmd
+        return self.symcmd(cmd, xml=xml)
+
+    def symdev(self, cmd, xml=True):
         cmd = ['/usr/symcli/bin/symdev'] + cmd
-        return self.symcmd(cmd)
+        return self.symcmd(cmd, xml=xml)
 
     def get_sym_info(self):
         out, err, ret = self.symcfg(["list"])
@@ -263,6 +296,89 @@ class Sym(object):
         out, err, ret = self.symcfg(['list', '-slo', '-detail', '-v'])
         return out
 
+    def parse_xml(self, buff, key=None, as_list=[], exclude=[]):
+        tree = fromstring(buff)
+        data = []
+        def parse_elem(elem, as_list=[], exclude=[]):
+            d = {}
+            for e in list(elem):
+                if e.tag in exclude:
+                    continue
+                if e.text.startswith("\n"):
+                    child = parse_elem(e, as_list, exclude)
+                    if e.tag in as_list:
+                        if e.tag not in d:
+                            d[e.tag] = []
+                        d[e.tag].append(child)
+                    else:
+                        d[e.tag] = child
+                else:
+                    d[e.tag] = e.text
+            return d
+
+        for elem in tree.getiterator(key):
+            data.append(parse_elem(elem, as_list, exclude))
+
+        return data
+
+    def list_pools(self, **kwargs):
+        print(json.dumps(self.get_pools(), indent=4))
+
+    def get_pools(self, **kwargs):
+        out = self.get_sym_pool_info()
+        data = self.parse_xml(out, key="DevicePool")
+        return data
+
+    def list_tdevs(self, **kwargs):
+        print(json.dumps(self.get_tdevs(), indent=4))
+
+    def get_tdevs(self, **kwargs):
+        out = self.get_sym_tdev_info()
+        data = self.parse_xml(out, key="Device", as_list=["pool"])
+        return data
+
+    def list_views(self, **kwargs):
+        print(json.dumps(self.get_views(), indent=4))
+
+    def get_views(self, **kwargs):
+        out = self.get_sym_view_aclx()
+        data = self.parse_xml(out, key="View_Info", as_list=["Initiators", "Director_Identification", "SG", "Device", "dev_port_info"], exclude=["Initiator_List"])
+        return data
+
+    def get_initiator_views(self, wwn):
+        out = self.get_symaccess(["list", "-type", "initiator", "-wwn", wwn])
+        data = self.parse_xml(out, key="view_name")
+        return data
+
+    def get_view(self, view):
+        out = self.get_symaccess(["show", "view", view])
+        data = self.parse_xml(out, key="View_Info")
+        return data
+
+    def get_mapping_storage_groups(self, wwn, target):
+        l = []
+        for view in self.get_initiator_views(wwn):
+            view_data = self.get_view(view)
+            if "port_info" not in view_data:
+                continue
+            if "Director_Identification" not in view_data["port_info"]:
+                continue
+            ports = [e["port_wwn"] for e in view_data["port_info"]["Director_Identification"]]
+            if target not in ports:
+                continue
+            l.append(view_data["stor_grpname"])
+        return l
+
+    def translate_mappings(self, mappings):
+        sgs = []
+        for mapping in mappings:
+            elements = mapping.split(":")
+            hba_id = elements[0]
+            targets = elements[-1].split(",")
+            for target in targets:
+                sgs += self.get_mapping_storage_groups(hba_id, target)
+        return sgs
+
 class Vmax(Sym):
     def __init__(self, sid, symcli_path, symcli_connect, username, password):
         Sym.__init__(self, sid, symcli_path, symcli_connect, username, password)
@@ -286,13 +402,16 @@ class Vmax(Sym):
         else:
             self.aclx = None
 
-    def symaccesscmd(self, cmd):
+    def symaccesscmd(self, cmd, xml=True):
         self.set_environ()
         cmd = ['/usr/symcli/bin/symaccess'] + cmd
         if self.aclx is None:
-            cmd += ['-sid', self.sid, '-output', 'xml_element']
+            cmd += ['-sid', self.sid]
         else:
-            cmd += ['-file', self.aclx, '-output', 'xml_element']
+            cmd += ['-file', self.aclx]
+        if xml:
+            cmd += ['-output', 'xml_element']
+        print(cmd)
         return justcall(cmd)
 
     def get_sym_pg_aclx(self):
@@ -315,17 +434,80 @@ class Vmax(Sym):
         out, err, ret = self.symaccesscmd(cmd)
         return out
 
-    def add_tdev(self, name, size):
-        out, err, ret = self.symdev(["create", "-tdev", "-cap", size, "-captype", "mb"])
+    def add_tdev(self, size=None, **kwargs):
+        """
+	     create dev count=<n>,
+		  size = <n> [MB | GB | CYL],
+		  emulation=<EmulationType>,
+		  config=<DevConfig>
+		  [, preallocate size = <ALL>
+		    [, allocate_type = PERSISTENT]]
+		  [, remote_config=<DevConfig>, ra_group=<n>]
+		  [, sg=<SgName> [, remote_sg=<SgName>]]
+		  [, mapping to dir <director_num:port>
+		    [starting] target = <scsi_target>,
+		    lun=<scsi_lun>, vbus=<fibre_vbus>
+		    [starting] base_address = <cuu_address>[,...]]
+		  [, device_attr =
+		    <SCSI3_PERSIST_RESERV | DIF1 |
+		      AS400_GK>[,...]]
+		  [, device_name='<DeviceName>'[,number=<n | SYMDEV> ]];
+        """
+
+        if size is None:
+            raise Exception("The '--size' parameter is mandatory")
+        size = convert_size(size, _to="MB")
+	_cmd = "create dev count=1, size= %d MB, emulation=FBA, config=TDEV;" % (size)
+        cmd = ["-cmd", _cmd, "commit", "-noprompt"]
+        out, err, ret = self.symconfigure(cmd, xml=False)
+        if ret != 0:
+            raise ex.excError(err)
+        for line in out.splitlines():
+            line = line.strip()
+            if line.startswith("New symdev:"):
+                l = line.split()
+                if len(l) < 3:
+                    raise ex.excError("unable to determine the created SymDevName")
+                return {"dev_name": l[2]}
+        raise ex.excError("unable to determine the created SymDevName")
+
+    def resize_tdev(self, dev=None, size=None, **kwargs):
+        if dev is None:
+            raise Exception("The '--dev' parameter is mandatory")
+        if size is None:
+            raise Exception("The '--size' parameter is mandatory")
+        size = convert_size(size, _to="MB")
+        cmd = ["modify", dev, "-tdev", "-cap", str(size), "-captype", "mb", "-noprompt"]
+        out, err, ret = self.symdev(cmd, xml=False)
         return out, err, ret
 
-    def modify_tdev(self, dev, size):
-        out, err, ret = self.symdev(["modify", dev, "-tdev", "-cap", size, "-captype", "mb"])
+    def del_tdev(self, dev=None, **kwargs):
+        if dev is None:
+            raise Exception("The '--dev' parameter is mandatory")
+        cmd = ["delete", dev, "-noprompt"]
+        out, err, ret = self.symdev(cmd, xml=False)
         return out, err, ret
 
-    def delete_dev(self, dev, size):
-        out, err, ret = self.symdev(["delete", dev])
+    def add_tdev_to_pool(self, dev, pool):
+        cmd = ["bind", dev, "-pool", pool, "-noprompt"]
+        out, err, ret = self.symdev(cmd)
         return out, err, ret
+
+    def add_tdev_to_sg(self, dev, sg):
+        cmd = ["-name", sg, "-type", "storage", "add", dev, "-noprompt"]
+        out, err, ret = self.symaccess(cmd)
+        return out, err, ret
+
+    def add_disk(self, size=None, pools=[], mappings={}, **kwargs):
+        sgs = self.translate_mappings(mappings)
+        if len(sgs) == 0:
+            raise ex.excError("no storage group found for the requested mappings")
+        data = self.add_tdev(size, **kwargs)
+        for pool in pools:
+            self.add_tdev_to_pool(data["dev_name"], pool)
+        for sg in sgs:
+            self.add_tdev_to_sg(data["dev_name"], sg)
+        return data
 
 class Dmx(Sym):
     def __init__(self, sid):
@@ -344,19 +526,22 @@ class Dmx(Sym):
         else:
             self.maskdb = None
 
-    def symaccesscmd(self, cmd):
+    def symaccesscmd(self, cmd, xml=True):
         self.set_environ()
         cmd = ['/usr/symcli/bin/symaccess'] + cmd
         if self.maskdb is None:
-            cmd += ['-sid', self.sid, '-output', 'xml_element']
+            cmd += ['-sid', self.sid]
         else:
-            cmd += ['-f', self.maskdb, '-output', 'xml_element']
+            cmd += ['-f', self.maskdb]
+        if xml:
+            cmd += ['-output', 'xml_element']
         return justcall(cmd)
 
     def get_sym_maskdb(self):
         cmd = ['list', 'database']
         out, err, ret = self.symaccesscmd(cmd)
         return out
+
 
 
 def do_action(action, array_name=None, node=None, **kwargs):
