ca233ec35df640f236149261ba235426a39e1aea
Christophe Varoqui <christophe.varoqui@opensvc.com>
2017-03-19 18:34:03 +0100

    Enable the globing support in the SunOS disk.raw driver
    And support symlinks to the devpath in devlist/disklist.

--- lib/resDiskRawSunOS.py
+++ lib/resDiskRawSunOS.py
@@ -1,53 +1,38 @@
-import resDiskRaw
+from __future__ import print_function
+
 import os
-import rcStatus
 import re
 
+import rcStatus
+import resDiskRaw
+
 class Disk(resDiskRaw.Disk):
-    def __init__(self,
-                 rid=None,
-                 devs=set([]),
-                 user=None,
-                 group=None,
-                 perm=None,
-                 create_char_devices=False,
-                 **kwargs):
-
-        devs = list(devs)
-        for i, dev in enumerate(devs):
-            if ":" in dev:
-                suffix = dev[dev.index(":"):]
-                dev = dev[0:dev.index(":")]
-            else:
-                suffix = ""
-            if re.match("^.*[sp][0-9]*$", dev) is not None:
-                # partition, substitute s2 to given part
-                regex = re.compile("[sp][0-9]*$", re.UNICODE)
-                dev = regex.sub("s2", dev)
-            else:
-                # base device, append s2
-                dev += 's2'
-            devs[i] = dev + suffix
-
-        resDiskRaw.Disk.__init__(self,
-                                 rid=rid,
-                                 devs=set(devs),
-                                 user=user,
-                                 group=group,
-                                 perm=perm,
-                                 create_char_devices=False,
-                                 **kwargs)
-
     def disklist(self):
+        devs = self.devlist()
+        l = set([])
+        for dev in devs:
+            if re.match("^/dev/rdsk/c[0-9]*", dev) is None:
+                continue
+            if not os.path.exists(dev):
+                continue
+
+            if re.match('^.*s[0-9]*$', dev) is None:
+                dev += "s2"
+            else:
+                regex = re.compile('s[0-9]*$', re.UNICODE)
+                dev = regex.sub('s2', dev)
+
+            l.add(dev)
+        return l
+
+    def devlist(self):
+        self.validate_devs()
         l = set([])
         for dev in self.devs:
-            if re.match("^/dev/rdsk/c[0-9]*", dev) is not None:
-                if os.path.exists(dev):
-                    if re.match('^.*s[0-9]*$', dev) is None:
-                        dev += "s2"
-                    else:
-                        regex = re.compile('s[0-9]*$', re.UNICODE)
-                        dev = regex.sub('s2', dev)
-                    l.add(dev)
+            if not os.path.exists(dev):
+                continue
+            if os.path.islink(dev) and not dev.startswith("/dev"):
+                dev = os.readlink(dev)
+            l.add(dev)
         return l
 
