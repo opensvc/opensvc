c406eda72f838ad5673bc5543b82204fbbb56868
Christophe Varoqui <christophe.varoqui@opensvc.com>
2017-04-11 00:32:42 +0200

    Set 'UTF-8' as a fallback encoding when sys.stdin.encoding is None
    This fixes the following stack when "nodemgr set" is used from
    debian postinst script.
    
    Traceback (most recent call last):
      File "/usr/share/opensvc/lib/nodemgr.py", line 101, in <module>
        ret = main()
      File "/usr/share/opensvc/lib/nodemgr.py", line 93, in main
        return _main(node, argv=argv)
      File "/usr/share/opensvc/lib/nodemgr.py", line 73, in _main
        err = node.action(action)
      File "/usr/share/opensvc/lib/node.py", line 719, in action
        ret = getattr(self, action)()
      File "/usr/share/opensvc/lib/node.py", line 1561, in set
        self.write_config()
      File "/usr/share/opensvc/lib/node.py", line 575, in write_config
        self.config.write(tmpf)
      File "/usr/share/opensvc/lib/rcConfigParser.py", line 31, in write
        value = value.decode(sys.stdin.encoding)
    TypeError: decode() argument 1 must be string, not None
    
    Reported by Jean-Michel Kelbert.

--- lib/rcConfigParser.py
+++ lib/rcConfigParser.py
@@ -15,11 +15,12 @@ if sys.version_info[0] < 3:
             """
             Write an .ini formatted representation of the configuration.
             """
+            encoding = sys.stdin.encoding if sys.stdin.encoding else 'UTF-8'
             if self._defaults:
                 fp.write("[%s]\n" % ConfigParser.DEFAULTSECT)
                 for (key, value) in self._defaults.items():
                     if type(value) != unicode:
-                        value = value.decode(sys.stdin.encoding)
+                        value = value.decode(encoding)
                     fp.write("%s = %s\n" % (key, value.replace('\n', '\n\t')))
                 fp.write("\n")
             for section in self._sections:
@@ -28,7 +29,7 @@ if sys.version_info[0] < 3:
                     if key == "__name__":
                         continue
                     if type(value) != unicode:
-                        value = value.decode(sys.stdin.encoding)
+                        value = value.decode(encoding)
                     if (value is not None) or (self._optcre == self.OPTCRE):
                         key = " = ".join((key, value.replace('\n', '\n\t')))
 
