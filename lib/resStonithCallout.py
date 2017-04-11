e76c413b32e92fa520565a81f8f8b308727c5ed2
Christophe Varoqui <christophe.varoqui@opensvc.com>
2017-04-11 11:58:25 +0200

    Fix the stonith resource drivers
    Stacking on an invalid number of arguments passed at init.
    The rid must be passed to the parent as a keyword argument, not
    an argument.
    
    Reported by Alvin Bomba.

--- lib/resStonithCallout.py
+++ lib/resStonithCallout.py
@@ -5,7 +5,7 @@ from rcGlobalEnv import rcEnv
 
 class Stonith(resStonith.Stonith):
     def __init__(self, rid=None, cmd="/bin/false", **kwargs):
-        resStonith.Stonith.__init__(self, rid, type="stonith.callout", **kwargs)
+        resStonith.Stonith.__init__(self, rid=rid, type="stonith.callout", **kwargs)
         self.cmd = cmd
 
     def _start(self):
--- lib/resStonithIlo.py
+++ lib/resStonithIlo.py
@@ -4,7 +4,7 @@ from rcGlobalEnv import rcEnv
 
 class Stonith(resStonith.Stonith):
     def __init__(self, rid=None, name=None, **kwargs):
-        resStonith.Stonith.__init__(self, rid, type="stonith.ilo", **kwargs)
+        resStonith.Stonith.__init__(self, rid=rid, type="stonith.ilo", **kwargs)
         self.name = name
         self.username, self.password, self.key = self.creds()
 
