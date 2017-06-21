648a4535e0945e33e2f83e229591ee441bcac999
Christophe Varoqui <christophe.varoqui@opensvc.com>
2017-06-21 11:40:58 +0200

    Fix ping-pong effect on print action with "-c"
    Only "--cluster" was stripped from the remote command to
    avoid ping-ponging.

--- lib/svc.py
+++ lib/svc.py
@@ -3771,7 +3771,7 @@ class Svc(object):
                 peers = set(self.drpnodes)
             peers -= set([rcEnv.nodename])
 
-        args = [arg for arg in sys.argv[1:] if arg != "--cluster"]
+        args = [arg for arg in sys.argv[1:] if arg not in ("-c", "--cluster")]
         if self.options.docker_argv and len(self.options.docker_argv) > 0:
             args += self.options.docker_argv
 
