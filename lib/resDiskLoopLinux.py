59166e7fb32ccd0d4a8c743358569972988223a1
Christophe Varoqui <christophe.varoqui@opensvc.com>
2017-07-27 17:12:40 +0200

    Add a devlist method to the linux loop driver

--- lib/resDiskLoopLinux.py
+++ lib/resDiskLoopLinux.py
@@ -62,6 +62,10 @@ class Disk(Res.Disk):
     def __init__(self, rid, loopFile, **kwargs):
         Res.Disk.__init__(self, rid, loopFile, **kwargs)
 
+    def devlist(self):
+        self.loop = file_to_loop(self.loopFile)
+        return set(self.loop)
+
     def provision(self):
         m = __import__("provDiskLoopLinux")
         prov = m.ProvisioningDisk(self)
