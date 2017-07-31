109b698ba0af52f80789f8cc33fb30867ee80e08
Christophe Varoqui <christophe.varoqui@opensvc.com>
2017-07-31 10:36:36 +0200

    Add a "fcache" decorator
    Which has the same behaviour as the lazy properties, but for functions.
    
    A lot of functions implement their own result caching. This new
    decorator will allow to simplify those.

--- lib/rcUtilities.py
+++ lib/rcUtilities.py
@@ -109,6 +109,44 @@ if os.name == 'nt':
 else:
     close_fds = True
 
+#############################################################################
+#
+# Cached functions
+#
+#############################################################################
+def fcache(fn):
+    """
+    A decorator for caching the result of a function
+    """
+    attr_name = '_fcache_' + fn.__name__
+    def _fcache(self):
+        if not hasattr(self, attr_name):
+            setattr(self, attr_name, fn(self))
+        return getattr(self, attr_name)
+    return _fcache
+
+def fcache_initialized(self, attr):
+    """
+    Return True if the function has already been cached
+    """
+    attr_name = '_fcache_' + attr
+    if hasattr(self, attr_name):
+        return True
+    return False
+
+def unset_fcache(self, attr):
+    """
+    Unset <attr> function cache
+    """
+    attr_name = '_lazy_' + attr
+    if hasattr(self, attr_name):
+        delattr(self, attr_name)
+
+#############################################################################
+#
+# Lazy properties
+#
+#############################################################################
 def lazy(fn):
     """
     A decorator for on-demand initialization of a property
