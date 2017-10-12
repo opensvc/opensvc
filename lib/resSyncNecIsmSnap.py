3c6967a90ce89782bc959d1bee7e0c482eafa195
Christophe Varoqui <christophe.varoqui@opensvc.com>
2017-10-12 14:37:37 +0200

    Fix the sync.necismsnap check on the data returned by sc_query_ld
    Need to check the len of data["sv"] and not the len of data.

--- lib/resSyncNecIsmSnap.py
+++ lib/resSyncNecIsmSnap.py
@@ -55,7 +55,7 @@ class syncNecIsmSnap(resSync.Sync):
         except ex.excError:
             self.status_log("unable to query SV:%s" % sv)
             return
-        if len(data) == 0:
+        if len(data["sv"]) == 0:
             self.status_log("SV:%s not found" % sv)
             return
         svinfo = data['sv'][0]
