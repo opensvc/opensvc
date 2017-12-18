3fda622961b65e703e4fc1f788b6fb42d16031e4
Christophe Varoqui <christophe.varoqui@opensvc.com>
2017-12-18 21:33:26 +0100

    Change the 'crash' linux toc method to echo 'c' in sysrq-triggger

--- lib/rcOsLinux.py
+++ lib/rcOsLinux.py
@@ -3,8 +3,9 @@ import rcOs
 
 class Os(rcOs.Os):
     def reboot(self):
-        with open("/proc/sysrq-trigger", "w") as f:
-            f.write("b")
+        with open("/proc/sysrq-trigger", "w") as ofile:
+            ofile.write("b")
 
     def crash(self):
-        shutil.copyfile('/dev/zero', '/dev/mem')
+        with open("/proc/sysrq-trigger", "w") as ofile:
+            ofile.write("c")
