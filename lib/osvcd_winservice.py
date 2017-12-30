85e7c1fad4e7a10cf2f3a4073703bece1f90e748
Christophe Varoqui <christophe.varoqui@opensvc.com>
2017-12-30 16:06:20 +0100

    Remove the windows listener implementation, prepare for a full-featured osvcd svc

--- lib/osvcd_winservice.py
+++ lib/osvcd_winservice.py
@@ -1,4 +1,4 @@
-'''
+"""
 
  Author: Alex Baker
  Date: 7th July 2008
@@ -13,8 +13,7 @@
 
  C:\>python aservice.py  --username <username> --password <PASSWORD> --startup auto install
 
-'''
-
+"""
 
 import win32service
 import win32serviceutil
@@ -24,65 +23,45 @@ import win32event
 import win32evtlogutil
 import os
 import servicemanager
-
-import datetime
-from subprocess import *
-
 import sys
-from rcListener import listener
-from rcGlobalEnv import rcEnv
 
-class OsvcSched(win32serviceutil.ServiceFramework):
+from osvcd import Daemon
 
-    _svc_name_ = "OsvcSched"
-    _svc_display_name_ = "OpenSVC job scheduler"
-    _svc_description_ = "Schedule the OpenSVC jobs"
+class OsvcAgent(win32serviceutil.ServiceFramework):
+
+    _svc_name_ = "OsvcAgent"
+    _svc_display_name_ = "OpenSVC agent"
+    _svc_description_ = "Orchestration, HA, inventoring, monitoring, config mgmt"
 
     def __init__(self, args):
         win32serviceutil.ServiceFramework.__init__(self, args)
         self.hWaitStop = win32event.CreateEvent(None, 0, 0, None)
-        now = datetime.datetime.now()
-        self.next_task10 = now + datetime.timedelta(minutes=1)
-
 
     def SvcStop(self):
         self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
-        sys.stop_listener = True
+        sys.stop_agent = True
         win32event.SetEvent(self.hWaitStop)
 
     def SvcDoRun(self):
         servicemanager.LogMsg(servicemanager.EVENTLOG_INFORMATION_TYPE,servicemanager.PYS_SERVICE_STARTED,(self._svc_name_, ''))
-        a = listener()
+        daemon = Daemon()
+        daemon.run()
 
         self.timeout = 60000
 
-        while 1:
+        while True:
             # Wait for service stop signal, if I timeout, loop again
             rc = win32event.WaitForSingleObject(self.hWaitStop, self.timeout)
             # Check to see if self.hWaitStop happened
             if rc == win32event.WAIT_OBJECT_0:
                 # Stop signal encountered
                 servicemanager.LogInfoMsg("%s - STOPPED"%self._svc_name_)
+                daemon.stop()
                 break
             else:
                 #servicemanager.LogInfoMsg("%s - ALIVE"%self._svc_name_)
-                self.SvcDoJob()
-
-    def SvcDoJob(self):
-        now = datetime.datetime.now()
-        if now > self.next_task10:
-            self.run_task10()
-            self.next_task10 = now + datetime.timedelta(minutes=10)
-
-    def run_task10(self):
-        cmd = [rcEnv.paths.svcmon, "--updatedb"]
-        servicemanager.LogInfoMsg("run %s" % ' '.join(cmd))
-        p = Popen(cmd, stdout=None, stderr=None, stdin=None)
-        p.communicate()
-        servicemanager.LogInfoMsg("run internal scheduler")
-        cmd = [rcEnv.paths.cron]
-        p = Popen(cmd, stdout=None, stderr=None, stdin=None)
-        p.communicate()
+                #self.SvcDoJob()
+                pass
 
 def ctrlHandler(ctrlType):
     return True
