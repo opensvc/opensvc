470a41cb1946a21c0ff13402af2646ead8acd890
Christophe Varoqui <christophe.varoqui@opensvc.com>
2018-01-05 12:48:17 +0100

    Add a test suite for the rcStatus module
    Which completes the coverage of this module.

---
+++ lib/tests/test_rcstatus.py
@@ -0,0 +1,58 @@
+from __future__ import print_function
+
+from rcStatus import Status, colorize_status, status_value, status_str
+import rcColor
+
+rcColor.use_color = "no"
+
+def test_colorize_status():
+    sta1 = Status()
+    ret = colorize_status(sta1, lpad=0)
+    assert ret == "undef"
+
+    sta1 = 0
+    ret = colorize_status(sta1, lpad=0)
+    assert ret == "up"
+
+    sta1 = None
+    ret = colorize_status(sta1, lpad=0)
+    assert ret == "undef"
+
+def test_status_value():
+    ret = status_value("foo")
+    assert ret is None
+
+def test_status_str():
+    ret = status_str(40)
+    assert ret is None
+
+def test_status_class():
+    try:
+        sta1 = Status("foo", "up")
+    except Exception:
+        return
+    assert False
+
+def test_status_hash():
+    data = {}
+    sta1 = Status()
+    data[sta1] = True
+    assert data[sta1] == True
+
+def test_status_ops():
+    sta1 = Status()
+    sta2 = Status("up")
+    assert str(sta1 + sta2) == "up"
+
+    try:
+        sta2 += "foo"
+        assert False
+    except Exception:
+        assert True
+
+    sta1.status = "corrupt"
+    try:
+        sta1 += sta2
+        assert False
+    except Exception:
+        assert True
