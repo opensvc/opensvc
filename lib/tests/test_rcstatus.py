86641b74ecfad7ff09231840bd628703e2fa527a
Christophe Varoqui <christophe.varoqui@opensvc.com>
2018-01-06 00:39:10 +0100

    Switch status and scheduler test suites to classes
    And add docstrings.

--- lib/tests/test_rcstatus.py
+++ lib/tests/test_rcstatus.py
@@ -5,54 +5,73 @@ import rcColor
 
 rcColor.use_color = "no"
 
-def test_colorize_status():
-    sta1 = Status()
-    ret = colorize_status(sta1, lpad=0)
-    assert ret == "undef"
+class TestStatus:
+    def test_colorize_status(self):
+        """
+        Status, colorization
+        """
+        sta1 = Status()
+        ret = colorize_status(sta1, lpad=0)
+        assert ret == "undef"
 
-    sta1 = 0
-    ret = colorize_status(sta1, lpad=0)
-    assert ret == "up"
+        sta1 = 0
+        ret = colorize_status(sta1, lpad=0)
+        assert ret == "up"
 
-    sta1 = None
-    ret = colorize_status(sta1, lpad=0)
-    assert ret == "undef"
+        sta1 = None
+        ret = colorize_status(sta1, lpad=0)
+        assert ret == "undef"
 
-def test_status_value():
-    ret = status_value("foo")
-    assert ret is None
+    def test_status_value(self):
+        """
+        Status, invalid status numeric value
+        """
+        ret = status_value("foo")
+        assert ret is None
 
-def test_status_str():
-    ret = status_str(40)
-    assert ret is None
+    def test_status_str(self):
+        """
+        Status, invalid status string
+        """
+        ret = status_str(40)
+        assert ret is None
 
-def test_status_class():
-    try:
-        sta1 = Status("foo", "up")
-    except Exception:
-        return
-    assert False
-
-def test_status_hash():
-    data = {}
-    sta1 = Status()
-    data[sta1] = True
-    assert data[sta1] == True
-
-def test_status_ops():
-    sta1 = Status()
-    sta2 = Status("up")
-    assert str(sta1 + sta2) == "up"
-
-    try:
-        sta2 += "foo"
+    def test_status_class(self):
+        """
+        Status, invalid init args
+        """
+        try:
+            sta1 = Status("foo", "up")
+        except Exception:
+            return
         assert False
-    except Exception:
-        assert True
 
-    sta1.status = "corrupt"
-    try:
-        sta1 += sta2
-        assert False
-    except Exception:
-        assert True
+    def test_status_hash(self):
+        """
+        Status, use as hash key
+        """
+        data = {}
+        sta1 = Status()
+        data[sta1] = True
+        assert data[sta1] == True
+
+    def test_status_ops(self):
+        """
+        Status, ops
+        """
+        sta1 = Status()
+        sta2 = Status("up")
+        assert str(sta1 + sta2) == "up"
+
+        try:
+            sta2 += "foo"
+            assert False
+        except Exception:
+            assert True
+
+        sta1.status = "corrupt"
+        try:
+            sta1 += sta2
+            assert False
+        except Exception:
+            assert True
