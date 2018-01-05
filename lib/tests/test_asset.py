de91c2ada0b98c40204bb59c22a7bc94f173e2e2
Christophe Varoqui <christophe.varoqui@opensvc.com>
2018-01-06 00:30:16 +0100

    Switch asset test suite to a class

--- lib/tests/test_asset.py
+++ lib/tests/test_asset.py
@@ -3,39 +3,51 @@ from __future__ import print_function
 import json
 import rcAsset
 from node import Node
+node = Node()
 
-def test_get_connect_to():
-    node = Node()
-    data_s = json.dumps({
-        "networkInterfaces": [
-            {
-                "accessConfigs": [
-                    {
-                        "kind": "compute#accessConfig",
-                        "name": "external-nat",
-                        "natIP": "23.251.137.71",
-                        "type": "ONE_TO_ONE_NAT"
-                    }
-                ],
-                "name": "nic0",
-                "networkIP": "10.132.0.2",
-            }
-        ]
-    })
-    asset = rcAsset.Asset(node)
-    ret = asset._parse_connect_to(data_s)
-    assert ret == "23.251.137.71"
+class TestAsset:
+    def test_011_get_connect_to(self):
+        """
+        asset connect_to on GCE, valid data
+        """
+        data_s = json.dumps({
+            "networkInterfaces": [
+                {
+                    "accessConfigs": [
+                        {
+                            "kind": "compute#accessConfig",
+                            "name": "external-nat",
+                            "natIP": "23.251.137.71",
+                            "type": "ONE_TO_ONE_NAT"
+                        }
+                    ],
+                    "name": "nic0",
+                    "networkIP": "10.132.0.2",
+                }
+            ]
+        })
+        asset = rcAsset.Asset(node)
+        ret = asset._parse_connect_to(data_s)
+        assert ret == "23.251.137.71"
 
-    data_s = json.dumps({
-        "networkInterfaces": [
-        ]
-    })
-    asset = rcAsset.Asset(node)
-    ret = asset._parse_connect_to(data_s)
-    assert ret is None
+    def test_012_get_connect_to(self):
+        """
+        asset connect_to on GCE, empty data
+        """
+        data_s = json.dumps({
+            "networkInterfaces": [
+            ]
+        })
+        asset = rcAsset.Asset(node)
+        ret = asset._parse_connect_to(data_s)
+        assert ret is None
 
-    data_s = "{corrupted}"
-    asset = rcAsset.Asset(node)
-    ret = asset._parse_connect_to(data_s)
-    assert ret is None
+    def test_013_get_connect_to(self):
+        """
+        asset connect_to on GCE, corrupt data
+        """
+        data_s = "{corrupted}"
+        asset = rcAsset.Asset(node)
+        ret = asset._parse_connect_to(data_s)
+        assert ret is None
 
