be70d477b3fce16dd06651ef5ebb86907bbf7759
Christophe Varoqui <christophe.varoqui@opensvc.com>
2018-02-20 12:14:47 +0100

    Add mem stats to SunOS

--- lib/nodeSunOS.py
+++ lib/nodeSunOS.py
@@ -1,4 +1,5 @@
 import node
+from rcUtilities import justcall
 
 class Node(node.Node):
     def shutdown(self):
@@ -8,3 +9,50 @@ class Node(node.Node):
     def _reboot(self):
         cmd = ["init", "6"]
         ret, out, err = self.vcall(cmd)
+
+    def stats_meminfo(self):
+        """
+        Memory sizes are store in MB.
+        Avails are percentages.
+
+        # kstat -n system_pages -p 
+        unix:0:system_pages:availrmem   48190
+        unix:0:system_pages:physmem     257948
+
+        # swap -l 
+        swapfile             dev  swaplo blocs   libres
+        /dev/dsk/c0d0s1     102,1       8 2425800 2425800
+        """
+        import mmap
+
+        raw_data = {}
+        data = {}
+        out, err, ret = justcall(["swap", "-l"])
+        swap_avail = 0
+        swap_total = 0
+        for line in out.splitlines():
+            if line[0] != "/":
+                continue
+            elem = line.split()
+            swap_avail += int(elem[-1]) // 2
+            swap_total += int(elem[-2]) // 2
+        data["swap_avail"] = 100 - swap_avail // swap_total
+        data["swap_total"] = swap_total // 1024
+
+        out, err, ret = justcall(["kstat", "-n", "system_pages", "-p"])
+        for line in out.splitlines():
+            elem = line.split()
+            raw_data[elem[0]] = elem[1]
+
+        data["mem_total"] = int(raw_data["unix:0:system_pages:physmem"]) * mmap.PAGESIZE // 1024 // 1024
+        data["mem_avail"] = 100 * int(raw_data["unix:0:system_pages:availrmem"]) // int(raw_data["unix:0:system_pages:physmem"])
+        return data
+
+    def stats(self):
+        """
+        Aggregate node stats from available sources.
+        """
+        data = node.Node.stats(self)
+        data.update(self.stats_meminfo())
+        return data
+
