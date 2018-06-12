c21b0c56fcfb87e89924df968f549ff869547881
Christophe Varoqui <christophe.varoqui@opensvc.com>
2018-06-12 16:38:47 +0200

    Fix the fs.vxfs provisioner on Linux
    Use mkfs.vxfs instead of newfs on Linux.

--- lib/provFsVxfs.py
+++ lib/provFsVxfs.py
@@ -1,5 +1,9 @@
 import provFs
+from rcUtilities import which
 
 class Prov(provFs.Prov):
-    mkfs = ['newfs', '-F', 'vxfs', '-o', 'largefiles', '-b', '8192']
+    if which("newfs"):
+        mkfs = ['newfs', '-F', 'vxfs', '-o', 'largefiles', '-b', '8192']
+    elif which("mkfs.vxfs"):
+        mkfs = ['mkfs.vxfs', '-o', 'largefiles,bsize=8192']
     info = ['fstyp']
