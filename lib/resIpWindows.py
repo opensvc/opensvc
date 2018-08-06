daae2a5a75f31090aa0516bb00e00e738891e0d8
Christophe Varoqui <christophe.varoqui@opensvc.com>
2018-08-06 10:49:14 +0200

    Fix rpm lint errors
    incorrect-fsf-address /usr/share/opensvc/lib/rcMountsWindows.py
    incorrect-fsf-address /usr/share/opensvc/lib/resIpWindows.py
    incorrect-fsf-address /usr/share/opensvc/lib/resFsWindows.py
    non-executable-script /usr/share/opensvc/lib/checkJstatLinux.py 0644L /usr/bin/python3

--- lib/resIpWindows.py
+++ lib/resIpWindows.py
@@ -1,24 +1,6 @@
-#
-# Copyright (c) 2009 Christophe Varoqui <christophe.varoqui@free.fr>'
-# Copyright (c) 2009 Cyril Galibern <cyril.galibern@free.fr>'
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-#
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
-"Module implement Windows specific ip management"
+"""
+This module implements the Windows ip resource driver
+"""
 
 import resIp as Res
 import rcExceptions as ex
@@ -29,9 +11,8 @@ class Ip(Res.Ip):
         self.log.info("checking %s availability"%self.addr)
         return check_ping(self.addr, timeout=timeout, count=count)
 
-#netsh interface ip add address "Local Area Connection" 33.33.33.33 255.255.255.255
-
     def startip_cmd(self):
+        #netsh interface ip add address "Local Area Connection" 33.33.33.33 255.255.255.255
         if ':' in self.addr:
             if '.' in self.mask:
                 self.log.error("netmask parameter is mandatory for ipv6 adresses")
@@ -58,10 +39,4 @@ class Ip(Res.Ip):
         else:
             cmd = ['netsh', 'interface', 'ipv4', 'delete', 'address', self.ipDev, "addr="+self.addr]
         return self.vcall(cmd)
-
-
-
-if __name__ == "__main__":
-    for c in (Ip,) :
-        help(c)
 
