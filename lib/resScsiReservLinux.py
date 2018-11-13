4ce97fc27b3a9f3145b4eb73c13cc723a66d4761
Christophe Varoqui <christophe.varoqui@opensvc.com>
2018-11-13 07:43:30 +0100

    Report n/a instead of up for scsireserv status if a dev is ro
    Because in this case, we can't clear the reservation until the dev is
    promoted rw.
    
    This happens on srdf devices that became r2 after a failover due to a crash.
    When the crashed node comes up again, the reservation are still held on the
    r2 devices, and they can't be dropped.
    
    Still report the situation as a resource log "info" message.
    
    Example:
    
         |- disk#0pr         ...O... n/a       /dev/sdat, /dev/sdau, /dev/sdcp, /dev/sdcq,
         |                                      /dev/sdem, /dev/sden, /dev/sdgj, /dev/sdgk
         |                                      info: resv held on ro dev /dev/sdgj

--- lib/resScsiReservLinux.py
+++ lib/resScsiReservLinux.py
@@ -1,5 +1,6 @@
 import resScsiReservSg
-from rcUtilitiesLinux import dev_to_paths
+import rcStatus
+from rcUtilitiesLinux import dev_to_paths, dev_is_ro
 
 class ScsiReserv(resScsiReservSg.ScsiReserv):
     def mangle_devs(self, devs):
@@ -7,3 +8,14 @@ class ScsiReserv(resScsiReservSg.ScsiReserv):
         for dev in devs:
             _devs |= set(dev_to_paths(dev))
         return _devs
+
+    def _status(self, verbose=False):
+        ret = resScsiReservSg.ScsiReserv._status(self, verbose=verbose)
+        if ret != rcStatus.UP:
+            return ret
+        self.get_devs()
+        for dev in self.devs:
+            if dev_is_ro(dev):
+                self.status_log("resv held on ro dev %s" % dev, "info")
+                return rcStatus.NA
+        return ret
