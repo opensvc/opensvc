d964c841257328cbd91fe714304c2c212179a244
Christophe Varoqui <christophe.varoqui@opensvc.com>
2018-12-02 21:33:18 +0100

    Align the tls certificate data ressource label to the <type> <desc> standard
    From "certificate.tls" to "tls certificate".

--- lib/resCertificateTls.py
+++ lib/resCertificateTls.py
@@ -3,3 +3,4 @@ from resData import Data
 class Certificate(Data):
     def __init__(self, rid, **kwargs):
         Data.__init__(self, rid, type="certificate.tls", **kwargs)
+        self.label = "tls certificate"
