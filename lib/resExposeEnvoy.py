bd96643192ab79538c6b5e969aea2ce74d141e9e
Christophe Varoqui <christophe.varoqui@opensvc.com>
2018-12-12 10:29:25 +0100

    Show the gateway listener in the expose.envoy resources label
    Example:
    
    collector                                up         warn
    `- instances
       `- aubergine                          up         warn, idle, started
    ...
          |- expose#0               ...../.. n/a        envoy expose 6379/tcp via 10.0.3.10:1111
          |- expose#1               ...../.. n/a        envoy expose 443/tcp via 10.0.3.10:443

--- lib/resExposeEnvoy.py
+++ lib/resExposeEnvoy.py
@@ -3,5 +3,10 @@ from resData import Data
 class Expose(Data):
     def __init__(self, rid, **kwargs):
         Data.__init__(self, rid, type="expose.envoy", **kwargs)
-        self.label = "envoy expose %s/%s" % (self.options.port, self.options.protocol) 
+        self.label = "envoy expose %s/%s via %s:%d" % (
+            self.options.port,
+            self.options.protocol,
+            self.options.listener_addr if self.options.listener_addr else "0.0.0.0",
+            self.options.listener_port
+        ) 
 
