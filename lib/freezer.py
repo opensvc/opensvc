aa6f55a6579288b25e0647bd549ab3de81ea6f75
Christophe Varoqui <christophe.varoqui@opensvc.com>
2019-01-11 10:47:59 +0100

    Fix the freezer when fed a root/svcname

--- lib/freezer.py
+++ lib/freezer.py
@@ -5,6 +5,7 @@ a service is frozen.
 """
 import os
 from rcGlobalEnv import rcEnv
+from rcUtilities import split_svcpath
 
 class Freezer(object):
     """
@@ -86,8 +87,9 @@ class Freezer(object):
         self.node_flag = os.path.join(rcEnv.paths.pathvar, "node", "frozen")
         if name == "node":
             self.flag = self.node_flag
-        elif "/" in name:
-            namespace, name = name.split("/", 1)
-            self.flag = os.path.join(rcEnv.paths.pathvar, "namespaces", namespace, "services", name, "frozen")
         else:
-            self.flag = os.path.join(rcEnv.paths.pathvar, "services", name, "frozen")
+            name, namespace = split_svcpath(name)
+            if namespace:
+                self.flag = os.path.join(rcEnv.paths.pathvar, "namespaces", namespace, "services", name, "frozen")
+            else:
+                self.flag = os.path.join(rcEnv.paths.pathvar, "services", name, "frozen")
