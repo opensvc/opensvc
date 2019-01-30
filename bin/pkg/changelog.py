7203958b7c4fbc01e716947c2430dc86588547ee
Christophe Varoqui <christophe.varoqui@opensvc.com>
2019-01-30 23:02:38 +0100

    Catch BrokenPipeError in bin/pkg/changelog.py
    And support the git standard commit-ish range expressions.
    
    Example:
    
    bin/pkg/changelog.py 1.8..1.9

--- bin/pkg/changelog.py
+++ bin/pkg/changelog.py
@@ -6,23 +6,37 @@ from subprocess import *
 from distutils.version import LooseVersion
 
 try:
-    branch = sys.argv[1]
+    arg = sys.argv[1]
 except:
-    branch = "HEAD"
+    arg = "HEAD"
 
-cmd = ["git", "log", "--oneline", "%s.."%branch]
-proc = Popen(cmd, stdout=PIPE)
-out, _ = proc.communicate()
-commits = {}
-for line in out.decode().splitlines():
-    cid, desc = line.split(" ", 1)
-    commits[cid] = [cid, desc]
-cids = [c for c in commits]
-cmd = ["git", "describe", "--tags"] + cids
-proc = Popen(cmd, stdout=PIPE)
-out, _ = proc.communicate()
-versions = list(out.decode().splitlines())
-for i, cid in enumerate(cids):
-    commits[cid].insert(0, versions[i])
-for commit in sorted(commits.values(), key=lambda x: LooseVersion(x[0]), reverse=True):
-    print("%-18s  %s" % (commit[0], commit[2]))
+def get_commits():
+    cmd = ["git", "log", "--oneline", arg]
+    proc = Popen(cmd, stdout=PIPE)
+    out, _ = proc.communicate()
+    commits = {}
+    for line in out.decode().splitlines():
+        cid, desc = line.split(" ", 1)
+        commits[cid] = [cid, desc]
+    return commits
+
+def get_versions(cids):
+    cmd = ["git", "describe", "--tags"] + cids
+    proc = Popen(cmd, stdout=PIPE)
+    out, _ = proc.communicate()
+    versions = list(out.decode().splitlines())
+    return versions
+
+def main():
+    commits = get_commits()
+    cids = [c for c in commits]
+    versions = get_versions(cids)
+    for i, cid in enumerate(cids):
+        commits[cid].insert(0, versions[i])
+    for commit in sorted(commits.values(), key=lambda x: LooseVersion(x[0]), reverse=True):
+        print("%-18s  %s" % (commit[0], commit[2]))
+
+try:
+    main()
+except BrokenPipeError:
+    pass
