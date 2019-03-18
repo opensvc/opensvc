bafa2007c7bfb22169627692664ba4b874f1cfb4
Christophe Varoqui <christophe.varoqui@opensvc.com>
2019-03-18 19:18:04 +0100

    Switch volmgr module to use VolmgrOptParser instead of SvcmgrOptParser

--- lib/volmgr.py
+++ lib/volmgr.py
@@ -1,12 +1,12 @@
 # coding: utf8
 
 import sys
-from svcmgr_parser import SvcmgrOptParser
+from volmgr_parser import VolmgrOptParser
 import mgr
 
 class Mgr(mgr.Mgr):
     def __init__(self, node=None):
-        mgr.Mgr.__init__(self, parser=SvcmgrOptParser, node=node)
+        mgr.Mgr.__init__(self, parser=VolmgrOptParser, node=node)
 
 if __name__ == "__main__":
     sys.exit(Mgr()())
