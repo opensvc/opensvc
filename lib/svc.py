9fb15903e747a0a324d231457116497a329ebf6d
Christophe Varoqui <christophe.varoqui@opensvc.com>
2019-04-19 14:29:29 +0200

    Don't consider a start as failed if it end up in stdby up
    We only considered the start action a success if the instance ends up in
    "up" or "n/a" availstatus, but instances with all resources flagged
    standby=true end up in "stdby up" availstatus.

--- lib/svc.py
+++ lib/svc.py
@@ -1169,7 +1169,7 @@ class BaseSvc(Crypt, ExtConfigMixin):
                not (action == "delete" and not self.command_is_scoped()):
                 data = self.print_status_data(refresh=True)
                 if action == "start" and not self.command_is_scoped() and \
-                   err == 0 and data.get("avail") not in ("up", "n/a", None) and \
+                   err == 0 and data.get("avail") not in ("up", "stdby up", "n/a", None) and \
                    not self.options.dry_run:
                     # catch drivers reporting no error, but instance not
                     # evaluating as "up", to avoid the daemon entering a
