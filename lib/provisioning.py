718a8e66289f6d241683c745547fea581ca3ae5a
Christophe Varoqui <christophe.varoqui@opensvc.com>
2019-06-05 11:22:45 +0200

    Remove unused remove_keyword() from the provisioning base class

--- lib/provisioning.py
+++ lib/provisioning.py
@@ -17,19 +17,3 @@ class Prov(object):
     def provisioner(self):
         pass
 
-    def remove_keywords(self, keywords=[]):
-        for kw in keywords:
-            self.remove_keyword(kw, write=False)
-        self.r.svc.write_config()
-
-    def remove_keyword(self, keyword, write=True):
-        for o in self.r.svc.config.options(self.r.rid):
-            if o != keyword and not o.startswith(keyword+"@"):
-                continue
-            self.r.log.info("comment out provisioning keyword: %s" % o)
-            val = self.r.svc.config.get(self.r.rid, o)
-            self.r.svc.config.remove_option(self.r.rid, o)
-            self.r.svc.config.set(self.r.rid, "#"+o, val)
-
-        if write:
-            self.r.svc.write_config()
