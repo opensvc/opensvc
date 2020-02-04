317298de92afe9734063bf1740bc9f90266bc4ef
Cyril Galibern <cyril.galibern@opensvc.com>
2020-02-04 16:59:35 +0100

    Tests: tests lock lib
    o lib/tests/test_lock.py:
     o TestLockUnlock::test_lock_unlock[0]
     o TestLockUnlock::test_lock_unlock[1]
     o TestLockUnlock::test_lock_unlock[2]
     o TestLockUnlock::test_lock_unlock[3]
     o TestLockUnlock::test_lock_unlock[4]
     o TestLockUnlock::test_lock_unlock[5]
     o TestLockUnlock::test_lock_unlock[6]
     o TestLockUnlock::test_lock_unlock[7]
     o TestLockUnlock::test_lock_unlock[8]
     o TestLockUnlock::test_lock_unlock[9]
     o TestLockUnlock::test_can_lock_again[0]
     o TestLockUnlock::test_can_lock_again[1]
     o TestLockUnlock::test_can_lock_again[2]
     o TestLockUnlock::test_can_lock_again[3]
     o TestLockUnlock::test_can_lock_again[4]
     o TestLockUnlock::test_can_lock_again[5]
     o TestLockUnlock::test_can_lock_again[6]
     o TestLockUnlock::test_can_lock_again[7]
     o TestLockUnlock::test_can_lock_again[8]
     o TestLockUnlock::test_can_lock_again[9]
     o TestLockUnlock::test_lock_raise_lock_timeout_if_held_by_another_pid[0]
     o TestLockUnlock::test_lock_raise_lock_timeout_if_held_by_another_pid[1]
     o TestLockUnlock::test_lock_raise_lock_timeout_if_held_by_another_pid[2]
     o TestLockUnlock::test_lock_raise_lock_timeout_if_held_by_another_pid[3]
     o TestLockUnlock::test_lock_raise_lock_timeout_if_held_by_another_pid[4]
     o TestLockUnlock::test_lock_raise_lock_timeout_if_held_by_another_pid[5]
     o TestLockUnlock::test_lock_raise_lock_timeout_if_held_by_another_pid[6]
     o TestLockUnlock::test_lock_raise_lock_timeout_if_held_by_another_pid[7]
     o TestLockUnlock::test_lock_raise_lock_timeout_if_held_by_another_pid[8]
     o TestLockUnlock::test_lock_raise_lock_timeout_if_held_by_another_pid[9]
     o TestLockUnlock::test_lock_raise_lock_timeout_if_held_by_another_pid_real_multiprocess[0]
     o TestLockUnlock::test_lock_raise_lock_timeout_if_held_by_another_pid_real_multiprocess[1]
     o TestLockUnlock::test_lock_raise_lock_timeout_if_held_by_another_pid_real_multiprocess[2]
     o TestLockUnlock::test_lock_raise_lock_timeout_if_held_by_another_pid_real_multiprocess[3]
     o TestLockUnlock::test_lock_raise_lock_timeout_if_held_by_another_pid_real_multiprocess[4]
     o TestLockUnlock::test_lock_raise_lock_timeout_if_held_by_another_pid_real_multiprocess[5]
     o TestLockUnlock::test_lock_raise_lock_timeout_if_held_by_another_pid_real_multiprocess[6]
     o TestLockUnlock::test_lock_raise_lock_timeout_if_held_by_another_pid_real_multiprocess[7]
     o TestLockUnlock::test_lock_raise_lock_timeout_if_held_by_another_pid_real_multiprocess[8]
     o TestLockUnlock::test_lock_raise_lock_timeout_if_held_by_another_pid_real_multiprocess[9]
     o TestCmlock::test_try_x_times_to_get_lock_until_it_acquires_lock[0]
     o TestCmlock::test_try_x_times_to_get_lock_until_it_acquires_lock[1]
     o TestCmlock::test_try_x_times_to_get_lock_until_it_acquires_lock[2]
     o TestCmlock::test_try_x_times_to_get_lock_until_it_acquires_lock[3]
     o TestCmlock::test_try_x_times_to_get_lock_until_it_acquires_lock[4]
     o TestCmlock::test_try_x_times_to_get_lock_until_it_acquires_lock[5]
     o TestCmlock::test_try_x_times_to_get_lock_until_it_acquires_lock[6]
     o TestCmlock::test_try_x_times_to_get_lock_until_it_acquires_lock[7]
     o TestCmlock::test_try_x_times_to_get_lock_until_it_acquires_lock[8]
     o TestCmlock::test_try_x_times_to_get_lock_until_it_acquires_lock[9]
     o TestCmlock::test_no_run_x_acquired_fails[0]
     o TestCmlock::test_no_run_x_acquired_fails[1]
     o TestCmlock::test_no_run_x_acquired_fails[2]
     o TestCmlock::test_no_run_x_acquired_fails[3]
     o TestCmlock::test_no_run_x_acquired_fails[4]
     o TestCmlock::test_no_run_x_acquired_fails[5]
     o TestCmlock::test_no_run_x_acquired_fails[6]
     o TestCmlock::test_no_run_x_acquired_fails[7]
     o TestCmlock::test_no_run_x_acquired_fails[8]
     o TestCmlock::test_no_run_x_acquired_fails[9]
     o TestLockExceptions::test_timeout_exc
     o TestLockExceptions::test_acquire_exc

--- lib/tests/test_lock.py
+++ lib/tests/test_lock.py
@@ -1,48 +1,99 @@
-import sys
 import os
-mod_d = os.path.realpath(os.path.join(os.path.dirname(__file__), ".."))
-sys.path.insert(0, mod_d)
 
 import lock
+import pytest
 
-class TestLock:
-    def test_lock(self):
-        """
-        Locking
-        """
-        fpath = "/tmp/test.lock"
 
-        def inline_lock():
-            return lock.lock(lockfile=fpath, timeout=0, intent="test")
+@pytest.fixture(scope='function')
+def sleep(mocker):
+    mocker.patch('lock.time.sleep')
 
+
+@pytest.mark.ci
+@pytest.mark.usefixtures('sleep')
+@pytest.mark.parametrize('timeout', range(10))
+class TestLockUnlock:
+    @staticmethod
+    def test_lock_unlock(tmp_file, timeout):
+        assert os.path.exists(tmp_file) is False
+        lock_fd = lock.lock(lockfile=tmp_file, timeout=timeout, intent="test")
+        assert os.path.exists(tmp_file) is True
+        lock.unlock(lock_fd)
+
+    @staticmethod
+    def test_can_lock_again(tmp_file, timeout):
+        assert lock.lock(lockfile=tmp_file, timeout=timeout, intent="test") > 0
+        for _ in range(timeout):
+            assert lock.lock(lockfile=tmp_file, timeout=timeout, intent="test") is None
+
+    @staticmethod
+    def test_lock_raise_lock_timeout_if_held_by_another_pid(mocker, tmp_file, timeout):
+        assert lock.lock(lockfile=tmp_file, timeout=timeout, intent="foo") > 0
+        mocker.patch('lock.os.getpid', return_value=-1)
+        with pytest.raises(lock.LockTimeout, match="holder pid .*, holder intent "):
+            lock.lock(lockfile=tmp_file, timeout=timeout, intent="test")
+
+    @staticmethod
+    def test_lock_raise_lock_timeout_if_held_by_another_pid_real_multiprocess(tmp_file, timeout):
         def worker():
             import sys
             try:
-                sys.exit(inline_lock())
+                sys.exit(lock.lock(lockfile=tmp_file, timeout=timeout, intent="test"))
             except lock.LockTimeout:
                 sys.exit(255)
 
-        def proc_lock():
-            from multiprocessing import Process
-            proc = Process(target=worker)
-            proc.start()
-            proc.join()
-            return proc.exitcode
+        assert lock.lock(lockfile=tmp_file, timeout=timeout, intent="test") > 0
+        from multiprocessing import Process
+        proc = Process(target=worker)
+        proc.start()
+        proc.join()
+        assert proc.exitcode == 255
 
-        lockfd = inline_lock()
-        assert lockfd > 0
 
-        # already lock owner
-        relockfd = inline_lock()
-        assert relockfd == None
+@pytest.mark.ci
+@pytest.mark.usefixtures('sleep')
+@pytest.mark.parametrize('timeout', range(10))
+class TestCmlock:
+    @staticmethod
+    def test_try_x_times_to_get_lock_until_it_acquires_lock(mocker, tmp_file, timeout):
+        runs = []
+        if timeout == 0:
+            side_effects = [None]
+            expected_lock_nowait = 1
+        else:
+            side_effects = [lock.LockAcquire({"pid": 0, "intent": ""})] * (timeout - 1)
+            # noinspection PyTypeChecker
+            side_effects.append(None)
+            expected_lock_nowait = timeout
 
-        # lock conflict
-        assert proc_lock() == 255
+        # mocker.patch('lock.os.getpid', return_value=-1)
+        lock_nowait = mocker.patch('lock.lock_nowait', side_effect=side_effects)
 
-        # release
-        lock.unlock(lockfd)
+        with lock.cmlock(lockfile=tmp_file, timeout=timeout):
+            runs.append(1)
 
-    def test_timeout_exc(self):
+        assert len(runs) == 1
+        assert lock_nowait.call_count == expected_lock_nowait
+
+    @staticmethod
+    def test_no_run_x_acquired_fails(mocker, tmp_file, timeout):
+        side_effects = [lock.LockAcquire({"pid": 0, "intent": ""})] * (timeout + 1)
+        mocker.patch('lock.os.getpid', return_value=-1)
+        lock_nowait = mocker.patch('lock.lock_nowait', side_effect=side_effects)
+
+        runs = []
+        with pytest.raises(lock.LockTimeout):
+            with lock.cmlock(lockfile=tmp_file, timeout=timeout):
+                runs.append(1)
+
+        assert len(runs) == 0
+        assert lock_nowait.call_count == max(timeout, 1)
+
+
+@pytest.mark.ci
+class TestLockExceptions:
+    @staticmethod
+    def test_timeout_exc():
         """
         LockTimeOut exception
         """
@@ -52,7 +103,8 @@ class TestLock:
             assert exc.intent == "test"
             assert exc.pid == 20000
 
-    def test_acquire_exc(self):
+    @staticmethod
+    def test_acquire_exc():
         """
         LockAcquire exception
         """
@@ -61,4 +113,3 @@ class TestLock:
         except lock.LockAcquire as exc:
             assert exc.intent == "test"
             assert exc.pid == 20000
-
