40f492041ce37cf407de00c04fea3fcc53f0eaff
Cyril Galibern <cyril.galibern@opensvc.com>
2020-02-17 15:50:57 +0100

    Test: Allow test_fs flag on Solaris

--- lib/tests/test_fs_flag.py
+++ lib/tests/test_fs_flag.py
@@ -8,15 +8,17 @@ import pytest
 @pytest.mark.ci
 class TestFsFlagFileName:
     @staticmethod
-    @pytest.mark.parametrize('fs_flag_class,namespace,expected_flag_file', [
-        (FsFlagLinux, 'ns1', '/dev/shm/opensvc/ns1/svc/svcname/fs#flag1.flag'),
-        (FsFlagLinux, None, '/dev/shm/opensvc/svc/svcname/fs#flag1.flag'),
-        (FsFlagSunOS, 'ns2', '/var/run/opensvc/ns2/svc/svcname/fs#flag1.flag'),
-        (FsFlagSunOS, None, '/var/run/opensvc/svc/svcname/fs#flag1.flag'),
+    @pytest.mark.parametrize('fs_flag_class,namespace,expected_flag_files', [
+        (FsFlagLinux, 'ns1', ['/dev/shm/opensvc/ns1/svc/svcname/fs#flag1.flag']),
+        (FsFlagLinux, None, ['/dev/shm/opensvc/svc/svcname/fs#flag1.flag']),
+        (FsFlagSunOS, 'ns2', ['/var/run/opensvc/ns2/svc/svcname/fs#flag1.flag',
+                              '/system/volatile/opensvc/ns2/svc/svcname/fs#flag1.flag']),
+        (FsFlagSunOS, None, ['/var/run/opensvc/svc/svcname/fs#flag1.flag',
+                             '/system/volatile/opensvc/svc/svcname/fs#flag1.flag']),
     ])
-    def test_is_correctly_defined(fs_flag_class, namespace, expected_flag_file):
+    def test_is_correctly_defined(fs_flag_class, namespace, expected_flag_files):
         svc = Svc('svcname', namespace=namespace, volatile=True)
         fs_flag = fs_flag_class(rid='fs#flag1')
         svc += fs_flag
         assert fs_flag.type == 'fs.flag'
-        assert fs_flag.flag_f == expected_flag_file
+        assert fs_flag.flag_f in expected_flag_files
