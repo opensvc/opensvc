e7b5b10d8024edb28409cacbd0760e9a5489d001
Cyril Galibern <cyril.galibern@opensvc.com>
2020-02-20 12:21:08 +0100

    Fix 'print status' errors when terminal columns are too low
    We do our best to detect correct terminal column value,
    with 78 as minimum value.
    
    environment variable COLUMNS will be used when stty method fails.
    
    Error example when terminal detected columns was 0:
       $ om svcname print status
       invalid width -4 (must be > 0)

--- lib/rcUtilities.py
+++ lib/rcUtilities.py
@@ -909,21 +909,31 @@ def to_cidr(s):
 
 
 def term_width():
-    default = int(os.environ.get("COLUMNS", 78))
+    min_columns = 78
+    detected_columns = _detect_term_width()
+    if detected_columns >= min_columns:
+        return detected_columns
+    else:
+        env_columns = int(os.environ.get("COLUMNS", 0))
+        if env_columns >= min_columns:
+            return env_columns
+        else:
+            return min_columns
+
+
+def _detect_term_width():
     try:
         # python 3.3+
         return os.get_terminal_size().columns
     except (AttributeError, OSError):
         pass
-    if rcEnv.sysname == "Windows":
-        return default
-    if which("stty") is None:
-        return default
-    out, err, ret = justcall(['stty', '-a'])
-    m = re.search(r'columns\s+(?P<columns>\d+);', out)
-    if m:
-        return int(m.group('columns'))
-    return default
+    if rcEnv.sysname != "Windows" and which("stty") is not None:
+        out, err, ret = justcall(['stty', '-a'])
+        if ret == 0:
+            m = re.search(r'columns\s+(?P<columns>\d+);', out)
+            if m:
+                return int(m.group('columns'))
+    return 0
 
 
 def get_cache_d():
