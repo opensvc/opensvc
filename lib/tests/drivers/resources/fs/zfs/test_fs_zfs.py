b6f611d7799491b12a440fc64c25a262a9f8ec30
Cyril Galibern <cyril.galibern@opensvc.com>
2020-03-24 18:21:26 +0100

    [TEST] Ensure we can create resource for all drivers
    Also verify we can call resource.start or resource.stop

--- lib/tests/drivers/resources/fs/zfs/test_fs_zfs.py
+++ lib/tests/drivers/resources/fs/zfs/test_fs_zfs.py
@@ -3,9 +3,12 @@ import pytest
 from tests.drivers.resources.helpers import assert_resource_has_mandatory_methods
 
 
-OS_LIST = {'Linux', 'SunOS'}
+OS_LIST = {'Linux', 'SunOS', 'FreeBSD'}
+
 SCENARIOS = [
-    ('fs.flag', 'FsFlag', {}, 'fs.flag'),
+    ('fs.zfs', 'FsZfs', {'rid': '#1', 'mount_point': '/tmp/plop', 'fs_type': 'plop',
+                         'mount_options': None, 'device': '/dev/zvol/...'},
+     'fs'),
 ]
 
 
@@ -13,7 +16,7 @@ SCENARIOS = [
 @pytest.mark.usefixtures('osvc_path_tests')
 @pytest.mark.parametrize('sysname', OS_LIST)
 @pytest.mark.parametrize('scenario', SCENARIOS)
-class TestDriverFsFlagInstances:
+class TestDriverFsZfsInstances:
     @staticmethod
     def test_has_correct_type(create_driver_resource, sysname, scenario):
         assert create_driver_resource(sysname, scenario).type == scenario[3]
