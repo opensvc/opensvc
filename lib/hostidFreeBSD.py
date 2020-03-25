ad780a6d96390b0bf1bbc7aa3da335c7d4b7abd5
Christophe Varoqui <christophe.varoqui@opensvc.com>
2020-03-25 15:14:36 +0100

    Move hostid* to utilities.hostid

--- lib/hostidFreeBSD.py
+++
@@ -1,1 +0,0 @@
-hostidLinux.py
---
+++ lib/utilities/hostid/__init__.py
@@ -0,0 +1,6 @@
+import importlib
+import platform
+
+_sysname = platform.uname()[0].lower().replace("-", "")
+_os = importlib.import_module("." + _sysname, package=__package__)
+hostid = _os.hostid
