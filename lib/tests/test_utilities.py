cf8ee3ba43798b3f4838c00e2bd43a23dbdb1b78
Cyril Galibern <cyril.galibern@opensvc.com>
2020-03-25 17:44:45 +0100

    [test] Fix test_term for its new location

--- lib/tests/test_utilities.py
+++ lib/tests/test_utilities.py
@@ -1,7 +1,8 @@
 # coding: utf-8
 
-from rcUtilities import *
 import pytest
+
+from rcUtilities import *
 
 
 @pytest.mark.ci
@@ -555,40 +556,3 @@ class TestAbbrev:
     )
     def test_it_correctly_trim_nodes(input_nodes, expected_nodes):
         assert abbrev(input_nodes) == expected_nodes
-
-
-@pytest.mark.ci
-class TestTermWidth:
-    @staticmethod
-    def test_return_value_greater_than_78():
-        assert term_width() >= 78
-
-    @staticmethod
-    def test_with_python_ge_3_3(mocker):
-        if getattr(os, 'get_terminal_size', None) is None:
-            pytest.skip('this test required os.get_terminal_size')
-        mocker.patch.object(os, 'get_terminal_size', return_value=mocker.Mock(columns=100), autospec=True)
-        assert term_width() == 100
-
-    @staticmethod
-    def test_with_python_lt_3_3(mocker):
-        if getattr(os, 'get_terminal_size', None) is not None:
-            mocker.patch.object(os, 'get_terminal_size', side_effect=OSError, autospec=True)
-        mocker.patch('rcUtilities.justcall', return_value=('columns 101;', '', 0), autospec=True)
-        assert term_width() == 101
-
-    @staticmethod
-    def test_fallback_to_env_columns(mocker):
-        if getattr(os, 'get_terminal_size', None) is not None:
-            mocker.patch.object(os, 'get_terminal_size', side_effect=OSError, autospec=True)
-        mocker.patch('rcUtilities.justcall', return_value=('', '', 1), autospec=True)
-        mocker.patch.dict(os.environ, {'COLUMNS': '102'})
-        assert term_width() == 102
-
-    @staticmethod
-    def test_return_default_78_when_no_other_values_are_possible(mocker):
-        if getattr(os, 'get_terminal_size', None) is not None:
-            mocker.patch.object(os, 'get_terminal_size', side_effect=OSError, autospec=True)
-        mocker.patch('rcUtilities.justcall', return_value=('columns 0;', '', 0), autospec=True)
-        mocker.patch.dict(os.environ, {'COLUMNS': '0'})
-        assert term_width() == 78
