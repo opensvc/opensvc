840e7adecc6d06284fc223dcb2b34d3c862ada93
Cyril Galibern <cyril.galibern@opensvc.com>
2020-03-26 15:19:12 +0100

    [test] Fix test_nodemgr_daemon.py for new daemon.main
    Also reduce number of imported modules

--- lib/tests/test_nodemgr_daemon.py
+++ lib/tests/test_nodemgr_daemon.py
@@ -2,22 +2,17 @@ import os
 import time
 import json
 import nodemgr
-from core.node import Node
-from osvcd import main
 import pytest
 
-
-@pytest.fixture(scope='function')
-def has_privs(mocker):
-    mocker.patch('node.check_privs', return_value=None)
+from daemon.main import main as daemon_main
 
 
 @pytest.mark.ci
-@pytest.mark.usefixtures('has_privs', 'osvc_path_tests', 'has_node_config')
+@pytest.mark.usefixtures('osvc_path_tests', 'has_node_config')
 class TestNodemgrDaemonActions:
     @staticmethod
     @pytest.mark.slow
-    def test_start_status_restart_stop_with_running_check(mock_argv, mocker, capture_stdout, tmp_file):
+    def test_start_status_restart_stop_with_running_check(mocker, capture_stdout, tmp_file):
         wait_time_for_osvcd_ready = 8
         if 'OPENSVC_CI_EXTRA_TIME_OSVCD_STARTUP' in os.environ:
             # give extra time when slow ci
@@ -25,20 +20,21 @@ class TestNodemgrDaemonActions:
             print('wait extra time:  OPENSVC_CI_EXTRA_TIME_OSVCD_STARTUP=%s' % extra_wait_time)
             wait_time_for_osvcd_ready += extra_wait_time
 
-        def osvcd_main_start():
-            mock_argv([])
-            main()
+        def daemon_main_target():
+            daemon_main(args=[])
 
         def daemon_start_native(_):
             from multiprocessing import Process
-            proc = Process(target=osvcd_main_start)
+            proc = Process(target=daemon_main_target)
             proc.start()
             proc.join()
             return 0
 
-        mocker.patch.object(Node, 'daemon_start_native', daemon_start_native)
-        mocker.patch.object(Node, 'daemon_handled_by_systemd', return_value=False)
+        mocker.patch.object(nodemgr.Node, 'check_privs', return_value=None)
+        mocker.patch.object(nodemgr.Node, 'daemon_start_native', daemon_start_native)
+        mocker.patch.object(nodemgr.Node, 'daemon_handled_by_systemd', return_value=False)
         mocker.patch('rcAsset.Asset.get_boot_id', side_effect='fake_boot_id')
+
         print('daemon is not running')
         assert nodemgr.main(argv=["daemon", "running", "--debug"]) > 0
 
