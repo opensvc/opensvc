e41e1cba25bb0c80bc84a200d2b76b0f1fe60554
Christophe Varoqui <christophe.varoqui@opensvc.com>
2020-03-30 00:34:43 +0200

    Add pylint ignore undefined-variable on __spec__ use for py2
    This variable is not used on py2.

--- opensvc/utilities/packages/list/__init__.py
+++ opensvc/utilities/packages/list/__init__.py
@@ -2,7 +2,7 @@ import importlib
 import platform
 
 _sysname = platform.uname()[0].lower().replace("-", "")
-_package = __package__ or __spec__.name
+_package = __package__ or __spec__.name # pylint: disable=undefined-variable
 _os = importlib.import_module("." + _sysname, package=_package)
 listpkg = _os.listpkg
 listpatch = _os.listpatch
