4e94b6f0311080208076ff416136dd701ac7d465
Cyril Galibern <cyril.galibern@opensvc.com>
2020-03-30 16:40:32 +0200

    [test] Add test provision real actions for QA Linux with pytest
    o pytest -m linux opensvc/

--- opensvc/tests/test_provision.py
+++ opensvc/tests/test_provision.py
@@ -31,259 +31,114 @@ def has_svc():
     assert Mgr()(argv=["-s", SVCNAME, "create"]) == 0
 
 
+@pytest.mark.linux
 @pytest.mark.usefixtures('osvc_path_tests', 'has_privs', 'has_svc')
-class TestIt:
-    def test_001(self):
-        """
-        Provision, disk.loop
-        """
+@pytest.mark.parametrize('type,unprovision_rid,properties',
+                         [
+                             ['loop', 'disk#0', [
+                                 "--kw", "disk#0.type=loop",
+                                 "--kw", "disk#0.file=/var/tmp/{svcname}.dd",
+                                 "--kw", "disk#0.size=10m"
+                             ]],
+
+                             ['vg', 'disk#0,disk#1',[
+                                 "--kw", "disk#0.type=loop",
+                                 "--kw", "disk#0.file=/var/tmp/{svcname}.dd",
+                                 "--kw", "disk#0.size=10m",
+                                 "--kw", "disk#1.type=vg",
+                                 "--kw", "disk#1.name={svcname}",
+                                 "--kw", "disk#1.pvs={disk#0.file}",
+                             ]],
+
+                             ['lv', 'disk#0,disk#1,disk#2', [
+                                 "--kw", "disk#0.type=loop",
+                                 "--kw", "disk#0.file=/var/tmp/{svcname}.dd",
+                                 "--kw", "disk#0.size=10m",
+                                 "--kw", "disk#1.type=vg",
+                                 "--kw", "disk#1.name={svcname}",
+                                 "--kw", "disk#1.pvs={disk#0.file}",
+                                 "--kw", "disk#2.type=lv",
+                                 "--kw", "disk#2.name=init",
+                                 "--kw", "disk#2.vg={disk#1.name}",
+                                 "--kw", "disk#2.size=100%FREE",
+                             ]],
+
+                             ['md', 'disk#0,disk#1,disk#2', [
+                                 "--kw", "disk#0.type=loop",
+                                 "--kw", "disk#0.file=/var/tmp/{svcname}.1.dd",
+                                 "--kw", "disk#0.size=10m",
+                                 "--kw", "disk#1.type=loop",
+                                 "--kw", "disk#1.file=/var/tmp/{svcname}.2.dd",
+                                 "--kw", "disk#1.size=10m",
+                                 "--kw", "disk#2.type=md",
+                                 "--kw", "disk#2.level=raid0",
+                                 "--kw", "disk#2.devs={disk#0.exposed_devs[0]} {disk#1.exposed_devs[0]}",
+                             ]],
+
+                             ['ext4', 'disk#0,fs#0', [
+                                 "--kw", "disk#0.type=loop",
+                                 "--kw", "disk#0.file=/var/tmp/{svcname}.dd",
+                                 "--kw", "disk#0.size=10m",
+                                 "--kw", "fs#0.type=ext4",
+                                 "--kw", "fs#0.mkfs_opt=-L {svcname}.fs.0",
+                                 "--kw", "fs#0.dev={disk#0.file}",
+                                 "--kw", "fs#0.mnt=/var/tmp/{svcname}",
+                                 "--kw", "fs#0.mnt_opt=rw,noatime",
+                             ]],
+                             ['btrfs', 'disk#0,fs#0', [
+                                 "--kw", "disk#0.type=loop",
+                                 "--kw", "disk#0.file=/var/tmp/{svcname}.dd",
+                                 "--kw", "disk#0.size=1g",
+                                 "--kw", "fs#0.type=btrfs",
+                                 "--kw", "fs#0.dev={disk#0.file}",
+                                 "--kw", "fs#0.mnt=/var/tmp/{svcname}",
+                                 "--kw", "fs#0.mnt_opt=rw,noatime,subvol=init",
+                             ]],
+
+                             ['container.docker shared', 'container#0', [
+                                 "--kw", "docker_daemon_private=false",
+                                 "--kw", "container#0.type=docker",
+                                 "--kw", "container#0.image=alpine:latest",
+                                 "--kw", "container#0.run_args=-it --net=none",
+                                 "--kw", "container#0.run_command=/bin/sh",
+                             ]],
+                             ['ip', 'ip#0', [
+                                 "--kw", "ip#0.provisioner=collector",
+                                 "--kw", "ip#0.ipdev=lo",
+                                 "--kw", "ip#0.network=192.168.0.0",
+                             ]],
+                             ['ip.docker', 'container#0,ip#0', [
+                                 "--kw", "docker_daemon_private=false",
+                                 "--kw", "container#0.type=docker",
+                                 "--kw", "container#0.image=alpine:latest",
+                                 "--kw", "container#0.run_args=-it --net=none",
+                                 "--kw", "container#0.run_command=/bin/sh",
+                                 "--kw", "ip#0.type=docker",
+                                 "--kw", "ip#0.ipname=172.17.172.17",
+                                 "--kw", "ip#0.ipdev=docker0",
+                                 "--kw", "ip#0.network=172.17.0.0",
+                                 "--kw", "ip#0.netmask=16",
+                                 "--kw", "ip#0.container_rid=container#0",
+                             ]],
+])
+class TestProvisionUnprovision:
+    @staticmethod
+    def test_disk_loop(type, unprovision_rid,properties):
+        print('\nls...')
+        assert Mgr()(argv=["-s", SVCNAME, "ls"]) == 0
+        print('\nprint config...')
         assert Mgr()(argv=["-s", SVCNAME, "print", "config"]) == 0
-        ret = Mgr()(argv=["-s", SVCNAME, "set",
-                                "--kw", "disk#0.type=loop",
-                                "--kw", "disk#0.file=/var/tmp/{svcname}.dd",
-                                "--kw", "disk#0.size=10m",
-                               ])
-        assert ret == 0
-        ret = Mgr()(argv=["-s", SVCNAME, "provision", "--local"])
-        assert ret == 0
-        ret = Mgr()(argv=["-s", SVCNAME, "print", "resinfo"])
-        assert ret == 0
-        ret = Mgr()(argv=["-s", SVCNAME, "print", "devs"])
-        assert ret == 0
-
-    def test_002(self):
-        """
-        Unprovision, disk.loop
-        """
-        ret = Mgr()(argv=["-s", SVCNAME, "delete", "--unprovision", "--rid", "disk#0"])
-        assert ret == 0
-
-    def test_011(self):
-        """
-        Provision, disk.vg
-        """
-        ret = Mgr()(argv=["-s", SVCNAME, "set",
-                                "--kw", "disk#0.type=loop",
-                                "--kw", "disk#0.file=/var/tmp/{svcname}.dd",
-                                "--kw", "disk#0.size=10m",
-                                "--kw", "disk#1.type=vg",
-                                "--kw", "disk#1.name={svcname}",
-                                "--kw", "disk#1.pvs={disk#0.file}",
-                               ])
-        assert ret == 0
-        ret = Mgr()(argv=["-s", SVCNAME, "provision", "--local"])
-        assert ret == 0
-        ret = Mgr()(argv=["-s", SVCNAME, "print", "resinfo"])
-        assert ret == 0
-        ret = Mgr()(argv=["-s", SVCNAME, "print", "devs"])
-        assert ret == 0
-
-    def test_012(self):
-        """
-        Unprovision, disk.vg
-        """
-        ret = Mgr()(argv=["-s", SVCNAME, "delete", "--unprovision", "--rid", "disk#0,disk#1"])
-        assert ret == 0
-
-    def test_021(self):
-        """
-        Provision, disk.lv
-        """
-        ret = Mgr()(argv=["-s", SVCNAME, "set",
-                                "--kw", "disk#0.type=loop",
-                                "--kw", "disk#0.file=/var/tmp/{svcname}.dd",
-                                "--kw", "disk#0.size=10m",
-                                "--kw", "disk#1.type=vg",
-                                "--kw", "disk#1.name={svcname}",
-                                "--kw", "disk#1.pvs={disk#0.file}",
-                                "--kw", "disk#2.type=lv",
-                                "--kw", "disk#2.name=init",
-                                "--kw", "disk#2.vg={disk#1.name}",
-                                "--kw", "disk#2.size=100%FREE",
-                               ])
-        assert ret == 0
-        ret = Mgr()(argv=["-s", SVCNAME, "provision", "--local"])
-        assert ret == 0
-        ret = Mgr()(argv=["-s", SVCNAME, "print", "resinfo"])
-        assert ret == 0
-        ret = Mgr()(argv=["-s", SVCNAME, "print", "devs"])
-        assert ret == 0
-
-    def test_022(self):
-        """
-        Unprovision, disk.lv
-        """
-        ret = Mgr()(argv=["-s", SVCNAME, "delete", "--unprovision", "--rid", "disk#0,disk#1,disk#2"])
-        assert ret == 0
-
-    def test_031(self):
-        """
-        Provision, disk.md
-        """
-        ret = Mgr()(argv=["-s", SVCNAME, "set",
-                                "--kw", "disk#0.type=loop",
-                                "--kw", "disk#0.file=/var/tmp/{svcname}.1.dd",
-                                "--kw", "disk#0.size=10m",
-                                "--kw", "disk#1.type=loop",
-                                "--kw", "disk#1.file=/var/tmp/{svcname}.2.dd",
-                                "--kw", "disk#1.size=10m",
-                                "--kw", "disk#2.type=md",
-                                "--kw", "disk#2.level=raid0",
-                                "--kw", "disk#2.devs={disk#0.exposed_devs[0]} {disk#1.exposed_devs[0]}",
-                               ])
-        assert ret == 0
-        ret = Mgr()(argv=["-s", SVCNAME, "provision", "--local"])
-        assert ret == 0
-        ret = Mgr()(argv=["-s", SVCNAME, "print", "resinfo"])
-        assert ret == 0
-        ret = Mgr()(argv=["-s", SVCNAME, "print", "devs"])
-        assert ret == 0
-
-    def test_032(self):
-        """
-        Unprovision, disk.md
-        """
-        ret = Mgr()(argv=["-s", SVCNAME, "delete", "--unprovision", "--rid", "disk#0,disk#1,disk#2"])
-        assert ret == 0
-
-    def test_121(self):
-        """
-        Provision, fs.ext4
-        """
-        ret = Mgr()(argv=["-s", SVCNAME, "set",
-                                "--kw", "disk#0.type=loop",
-                                "--kw", "disk#0.file=/var/tmp/{svcname}.dd",
-                                "--kw", "disk#0.size=10m",
-                                "--kw", "fs#0.type=ext4",
-                                "--kw", "fs#0.mkfs_opt=-L {svcname}.fs.0",
-                                "--kw", "fs#0.dev={disk#0.file}",
-                                "--kw", "fs#0.mnt=/var/tmp/{svcname}",
-                                "--kw", "fs#0.mnt_opt=rw,noatime",
-                               ])
-        assert ret == 0
-        ret = Mgr()(argv=["-s", SVCNAME, "provision", "--local"])
-        assert ret == 0
-        ret = Mgr()(argv=["-s", SVCNAME, "print", "resinfo"])
-        assert ret == 0
-        ret = Mgr()(argv=["-s", SVCNAME, "print", "devs"])
-        assert ret == 0
-
-    def test_122(self):
-        """
-        Unprovision, fs.ext4
-        """
-        ret = Mgr()(argv=["-s", SVCNAME, "delete", "--unprovision", "--rid", "disk#0,fs#0"])
-        assert ret == 0
-
-    def test_131(self):
-        """
-        Provision, fs.btrfs
-        """
-        ret = Mgr()(argv=["-s", SVCNAME, "set",
-                                "--kw", "disk#0.type=loop",
-                                "--kw", "disk#0.file=/var/tmp/{svcname}.dd",
-                                "--kw", "disk#0.size=1g",
-                                "--kw", "fs#0.type=btrfs",
-                                "--kw", "fs#0.dev={disk#0.file}",
-                                "--kw", "fs#0.mnt=/var/tmp/{svcname}",
-                                "--kw", "fs#0.mnt_opt=rw,noatime,subvol=init",
-                               ])
-        assert ret == 0
-        ret = Mgr()(argv=["-s", SVCNAME, "provision", "--local"])
-        assert ret == 0
-        ret = Mgr()(argv=["-s", SVCNAME, "print", "resinfo"])
-        assert ret == 0
-        ret = Mgr()(argv=["-s", SVCNAME, "print", "devs"])
-        assert ret == 0
-
-    def test_132(self):
-        """
-        Unprovision, fs.btrfs
-        """
-        ret = Mgr()(argv=["-s", SVCNAME, "delete", "--unprovision", "--rid", "disk#0,fs#0"])
-        assert ret == 0
-
-    def test_201(self):
-        """
-        Provision, container.docker (shared)
-        """
-        ret = Mgr()(argv=["-s", SVCNAME, "set",
-                                "--kw", "docker_daemon_private=false",
-                                "--kw", "container#0.type=docker",
-                                "--kw", "container#0.image=alpine:latest",
-                                "--kw", "container#0.run_args=-it --net=none",
-                                "--kw", "container#0.run_command=/bin/sh",
-                               ])
-        assert ret == 0
-        ret = Mgr()(argv=["-s", SVCNAME, "provision", "--local"])
-        assert ret == 0
-        ret = Mgr()(argv=["-s", SVCNAME, "print", "resinfo"])
-        assert ret == 0
-        ret = Mgr()(argv=["-s", SVCNAME, "print", "devs"])
-        assert ret == 0
-
-    def test_202(self):
-        """
-        Unprovision, container.docker (shared)
-        """
-        ret = Mgr()(argv=["-s", SVCNAME, "delete", "--unprovision", "--rid", "container#0"])
-        assert ret == 0
-
-    def test_301(self):
-        """
-        Provision, ip
-        """
-        ret = Mgr()(argv=["-s", SVCNAME, "set",
-                                "--kw", "ip#0.provisioner=collector",
-                                "--kw", "ip#0.ipdev=lo",
-                                "--kw", "ip#0.network=192.168.0.0",
-                               ])
-        assert ret == 0
-        ret = Mgr()(argv=["-s", SVCNAME, "provision", "--local"])
-        assert ret == 0
-        ret = Mgr()(argv=["-s", SVCNAME, "print", "resinfo"])
-        assert ret == 0
-        ret = Mgr()(argv=["-s", SVCNAME, "print", "devs"])
-        assert ret == 0
-
-    def test_302(self):
-        """
-        Unprovision, ip
-        """
-        ret = Mgr()(argv=["-s", SVCNAME, "delete", "--unprovision", "--rid", "ip#0"])
-        assert ret == 0
-
-    def test_311(self):
-        """
-        Provision, ip.docker
-        """
-        ret = Mgr()(argv=["-s", SVCNAME, "set",
-                                "--kw", "docker_daemon_private=false",
-                                "--kw", "container#0.type=docker",
-                                "--kw", "container#0.image=alpine:latest",
-                                "--kw", "container#0.run_args=-it --net=none",
-                                "--kw", "container#0.run_command=/bin/sh",
-                                "--kw", "ip#0.type=docker",
-                                "--kw", "ip#0.ipname=172.17.172.17",
-                                "--kw", "ip#0.ipdev=docker0",
-                                "--kw", "ip#0.network=172.17.0.0",
-                                "--kw", "ip#0.netmask=16",
-                                "--kw", "ip#0.container_rid=container#0",
-                               ])
-        assert ret == 0
-        ret = Mgr()(argv=["-s", SVCNAME, "provision", "--local"])
-        assert ret == 0
-        ret = Mgr()(argv=["-s", SVCNAME, "print", "resinfo"])
-        assert ret == 0
-        ret = Mgr()(argv=["-s", SVCNAME, "print", "devs"])
-        assert ret == 0
-
-    def test_312(self):
-        """
-        Unprovision, ip.docker
-        """
-        ret = Mgr()(argv=["-s", SVCNAME, "delete", "--unprovision", "--rid", "container#0,ip#0"])
-        assert ret == 0
-
-    def test_399(self):
-        assert Mgr()(argv=["-s", SVCNAME, "delete", "--unprovision", "--local"]) == 0
+        print('\nset properties: ', properties)
+        assert Mgr()(argv=["-s", SVCNAME, "set"] + properties) == 0
+        print('\nprint config...')
+        assert Mgr()(argv=["-s", SVCNAME, "print", "config"]) == 0
+        print('\nprovision --local...')
+        assert Mgr()(argv=["-s", SVCNAME, "provision", "--local"]) == 0
+        print('\nprint resinfo...')
+        assert Mgr()(argv=["-s", SVCNAME, "print", "resinfo"]) == 0
+        print('\nprint status -r...')
+        assert Mgr()(argv=["-s", SVCNAME, "print", "status", "-r"]) == 0
+        print('\nprint devs...')
+        assert Mgr()(argv=["-s", SVCNAME, "print", "devs"]) == 0
+        # print('\ndelete --unprovision --rid ', unprovision_rid, '...')
+        # assert Mgr()(argv=["-s", SVCNAME, "delete", '--unprovision', '--rid', unprovision_rid]) == 0
