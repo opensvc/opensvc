548be8a4667fac642302039ad4720c8c0c6ee10b
Cyril Galibern <cyril.galibern@opensvc.com>
2020-03-30 12:32:12 +0200

    Move rcStatsCollect* to utilities.stats.collector/
    Add noop collect method for Aix

---
+++ opensvc/utilities/stats/collector/__init__.py
@@ -0,0 +1,7 @@
+import importlib
+import platform
+
+_sysname = platform.uname()[0].lower().replace("-", "")
+_package = __package__ or __spec__.name  # pylint: disable=undefined-variable
+_os = importlib.import_module("." + _sysname, package=_package)
+collect = _os.collect
