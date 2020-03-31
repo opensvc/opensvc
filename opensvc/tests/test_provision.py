0b7c6ea9a6f9b26a2a3e68247754fb85b1c4c6f3
Cyril Galibern <cyril.galibern@opensvc.com>
2020-03-31 12:35:37 +0200

    [test] Add test provision real actions for QA Linux with pytest
    o pytest -m linux opensvc/
    
    o provisioning object types:
      o disk type loop
      o disk type vg
      o disk type md
      o fs type ext4
      o container type docker
    
    o commands tested:
       o ['create', '--debug'],
       o ['set'] + properties,
       o ['provision', '--local', '--debug'],
       o ['print', 'status', '-r', '--debug'],
       o ['print', 'config', '--debug'],
       o ['print', 'resinfo', '--debug'],
       o ['print', 'devs', '--debug'],
       o ['delete', '--unprovision', '--rid', unprovision_rid, '--debug']

--- opensvc/tests/test_provision.py
+++ opensvc/tests/test_provision.py
@@ -1,144 +1,121 @@
-# coding: utf8
-
-from __future__ import print_function
-from __future__ import unicode_literals
-
-import logging
-import uuid
+import os
 
 import pytest
 
-try:
-    from StringIO import StringIO
-except ImportError:
-    from io import StringIO
 from commands.svc import Mgr
-import commands.mgr
 
+scenarios = [
+    ['loop', 'disk#0', [
+        "--kw", "disk#0.type=loop",
+        "--kw", "disk#0.file=/var/tmp/{svcname}.dd",
+        "--kw", "disk#0.size=10m"
+    ]],
 
-@pytest.fixture(scope='function')
-def has_privs(mocker):
-    mocker.patch.object(commands.mgr, 'check_privs')
+    ['vg', 'disk#0,disk#1', [
+        "--kw", "disk#0.type=loop",
+        "--kw", "disk#0.file=/var/tmp/{svcname}.dd",
+        "--kw", "disk#0.size=10m",
+        "--kw", "disk#1.type=vg",
+        "--kw", "disk#1.name={svcname}",
+        "--kw", "disk#1.pvs={disk#0.file}",
+    ]],
 
+    ['md', 'disk#0,disk#1,disk#2', [
+        "--kw", "disk#0.type=loop",
+        "--kw", "disk#0.file=/var/tmp/{svcname}.1.dd",
+        "--kw", "disk#0.size=10m",
+        "--kw", "disk#1.type=loop",
+        "--kw", "disk#1.file=/var/tmp/{svcname}.2.dd",
+        "--kw", "disk#1.size=10m",
+        "--kw", "disk#2.type=md",
+        "--kw", "disk#2.level=raid0",
+        "--kw", "disk#2.devs={disk#0.exposed_devs[0]} {disk#1.exposed_devs[0]}",
+    ]],
 
-logging.disable(logging.CRITICAL)
+    ['ext4', 'disk#0,fs#0', [
+        "--kw", "disk#0.type=loop",
+        "--kw", "disk#0.file=/var/tmp/{svcname}.dd",
+        "--kw", "disk#0.size=10m",
+        "--kw", "fs#0.type=ext4",
+        "--kw", "fs#0.mkfs_opt=-L {svcname}.fs.0",
+        "--kw", "fs#0.dev={disk#0.file}",
+        "--kw", "fs#0.mnt=/var/tmp/{svcname}",
+        "--kw", "fs#0.mnt_opt=rw,noatime",
+    ]],
 
-SVCNAME = "unittest-" + str(uuid.uuid4())
+    ['container-docker-shared', 'container#0', [
+        "--kw", "docker_daemon_private=false",
+        "--kw", "container#0.type=docker",
+        "--kw", "container#0.image=alpine:latest",
+        "--kw", "container#0.run_args=-it --net=none",
+        "--kw", "container#0.run_command=/bin/sh",
+    ]],
+    ['ip', 'ip#0', [
+        "--kw", "ip#0.provisioner=collector",
+        "--kw", "ip#0.ipdev=lo",
+        "--kw", "ip#0.network=192.168.0.0",
+    ]],
 
+]
 
-@pytest.fixture(scope='function')
-def has_svc():
-    assert Mgr()(argv=["-s", SVCNAME, "create"]) == 0
+skipped_scenarios = [
+    ['lv', 'disk#0,disk#1,disk#2', [
+        "--kw", "disk#0.type=loop",
+        "--kw", "disk#0.file=/var/tmp/{svcname}.dd",
+        "--kw", "disk#0.size=10m",
+        "--kw", "disk#1.type=vg",
+        "--kw", "disk#1.name={svcname}",
+        "--kw", "disk#1.pvs={disk#0.file}",
+        "--kw", "disk#2.type=lv",
+        "--kw", "disk#2.name=init",
+        "--kw", "disk#2.vg={disk#1.name}",
+        "--kw", "disk#2.size=100%FREE",
+    ]],
+    ['btrfs', 'disk#0,fs#0', [
+        "--kw", "disk#0.type=loop",
+        "--kw", "disk#0.file=/var/tmp/{svcname}.dd",
+        "--kw", "disk#0.size=1g",
+        "--kw", "fs#0.type=btrfs",
+        "--kw", "fs#0.dev={disk#0.file}",
+        "--kw", "fs#0.mnt=/var/tmp/{svcname}",
+        "--kw", "fs#0.mnt_opt=rw,noatime,subvol=init",
+    ]],
+
+    ['ip-docker', 'container#0,ip#0', [
+        "--kw", "docker_daemon_private=false",
+        "--kw", "container#0.type=docker",
+        "--kw", "container#0.image=alpine:latest",
+        "--kw", "container#0.run_args=-it --net=none",
+        "--kw", "container#0.run_command=/bin/sh",
+        "--kw", "ip#0.type=docker",
+        "--kw", "ip#0.ipname=172.17.172.17",
+        "--kw", "ip#0.ipdev=docker0",
+        "--kw", "ip#0.network=172.17.0.0",
+        "--kw", "ip#0.netmask=16",
+        "--kw", "ip#0.container_rid=container#0",
+    ]],
+]
 
 
 @pytest.mark.linux
-@pytest.mark.usefixtures('osvc_path_tests', 'has_privs', 'has_svc')
-@pytest.mark.parametrize('type,unprovision_rid,properties',
-                         [
-                             ['loop', 'disk#0', [
-                                 "--kw", "disk#0.type=loop",
-                                 "--kw", "disk#0.file=/var/tmp/{svcname}.dd",
-                                 "--kw", "disk#0.size=10m"
-                             ]],
-
-                             ['vg', 'disk#0,disk#1',[
-                                 "--kw", "disk#0.type=loop",
-                                 "--kw", "disk#0.file=/var/tmp/{svcname}.dd",
-                                 "--kw", "disk#0.size=10m",
-                                 "--kw", "disk#1.type=vg",
-                                 "--kw", "disk#1.name={svcname}",
-                                 "--kw", "disk#1.pvs={disk#0.file}",
-                             ]],
-
-                             ['lv', 'disk#0,disk#1,disk#2', [
-                                 "--kw", "disk#0.type=loop",
-                                 "--kw", "disk#0.file=/var/tmp/{svcname}.dd",
-                                 "--kw", "disk#0.size=10m",
-                                 "--kw", "disk#1.type=vg",
-                                 "--kw", "disk#1.name={svcname}",
-                                 "--kw", "disk#1.pvs={disk#0.file}",
-                                 "--kw", "disk#2.type=lv",
-                                 "--kw", "disk#2.name=init",
-                                 "--kw", "disk#2.vg={disk#1.name}",
-                                 "--kw", "disk#2.size=100%FREE",
-                             ]],
-
-                             ['md', 'disk#0,disk#1,disk#2', [
-                                 "--kw", "disk#0.type=loop",
-                                 "--kw", "disk#0.file=/var/tmp/{svcname}.1.dd",
-                                 "--kw", "disk#0.size=10m",
-                                 "--kw", "disk#1.type=loop",
-                                 "--kw", "disk#1.file=/var/tmp/{svcname}.2.dd",
-                                 "--kw", "disk#1.size=10m",
-                                 "--kw", "disk#2.type=md",
-                                 "--kw", "disk#2.level=raid0",
-                                 "--kw", "disk#2.devs={disk#0.exposed_devs[0]} {disk#1.exposed_devs[0]}",
-                             ]],
-
-                             ['ext4', 'disk#0,fs#0', [
-                                 "--kw", "disk#0.type=loop",
-                                 "--kw", "disk#0.file=/var/tmp/{svcname}.dd",
-                                 "--kw", "disk#0.size=10m",
-                                 "--kw", "fs#0.type=ext4",
-                                 "--kw", "fs#0.mkfs_opt=-L {svcname}.fs.0",
-                                 "--kw", "fs#0.dev={disk#0.file}",
-                                 "--kw", "fs#0.mnt=/var/tmp/{svcname}",
-                                 "--kw", "fs#0.mnt_opt=rw,noatime",
-                             ]],
-                             ['btrfs', 'disk#0,fs#0', [
-                                 "--kw", "disk#0.type=loop",
-                                 "--kw", "disk#0.file=/var/tmp/{svcname}.dd",
-                                 "--kw", "disk#0.size=1g",
-                                 "--kw", "fs#0.type=btrfs",
-                                 "--kw", "fs#0.dev={disk#0.file}",
-                                 "--kw", "fs#0.mnt=/var/tmp/{svcname}",
-                                 "--kw", "fs#0.mnt_opt=rw,noatime,subvol=init",
-                             ]],
-
-                             ['container.docker shared', 'container#0', [
-                                 "--kw", "docker_daemon_private=false",
-                                 "--kw", "container#0.type=docker",
-                                 "--kw", "container#0.image=alpine:latest",
-                                 "--kw", "container#0.run_args=-it --net=none",
-                                 "--kw", "container#0.run_command=/bin/sh",
-                             ]],
-                             ['ip', 'ip#0', [
-                                 "--kw", "ip#0.provisioner=collector",
-                                 "--kw", "ip#0.ipdev=lo",
-                                 "--kw", "ip#0.network=192.168.0.0",
-                             ]],
-                             ['ip.docker', 'container#0,ip#0', [
-                                 "--kw", "docker_daemon_private=false",
-                                 "--kw", "container#0.type=docker",
-                                 "--kw", "container#0.image=alpine:latest",
-                                 "--kw", "container#0.run_args=-it --net=none",
-                                 "--kw", "container#0.run_command=/bin/sh",
-                                 "--kw", "ip#0.type=docker",
-                                 "--kw", "ip#0.ipname=172.17.172.17",
-                                 "--kw", "ip#0.ipdev=docker0",
-                                 "--kw", "ip#0.network=172.17.0.0",
-                                 "--kw", "ip#0.netmask=16",
-                                 "--kw", "ip#0.container_rid=container#0",
-                             ]],
-])
-class TestProvisionUnprovision:
+@pytest.mark.slow
+@pytest.mark.usefixtures('osvc_path_tests')
+@pytest.mark.parametrize('name,unprovision_rid,properties', scenarios)
+class TestProvision:
     @staticmethod
-    def test_disk_loop(type, unprovision_rid,properties):
-        print('\nls...')
-        assert Mgr()(argv=["-s", SVCNAME, "ls"]) == 0
-        print('\nprint config...')
-        assert Mgr()(argv=["-s", SVCNAME, "print", "config"]) == 0
-        print('\nset properties: ', properties)
-        assert Mgr()(argv=["-s", SVCNAME, "set"] + properties) == 0
-        print('\nprint config...')
-        assert Mgr()(argv=["-s", SVCNAME, "print", "config"]) == 0
-        print('\nprovision --local...')
-        assert Mgr()(argv=["-s", SVCNAME, "provision", "--local"]) == 0
-        print('\nprint resinfo...')
-        assert Mgr()(argv=["-s", SVCNAME, "print", "resinfo"]) == 0
-        print('\nprint status -r...')
-        assert Mgr()(argv=["-s", SVCNAME, "print", "status", "-r"]) == 0
-        print('\nprint devs...')
-        assert Mgr()(argv=["-s", SVCNAME, "print", "devs"]) == 0
-        # print('\ndelete --unprovision --rid ', unprovision_rid, '...')
-        # assert Mgr()(argv=["-s", SVCNAME, "delete", '--unprovision', '--rid', unprovision_rid]) == 0
+    def test_create_provision_delete_unprovision(mocker, name, unprovision_rid, properties):
+        svcname = "pytest-" + name
+        mocker.patch.dict(os.environ, {'OSVC_DETACHED': '1'})
+        for args in [
+            ['create', '--debug'],
+            ['set'] + properties,
+            ['provision', '--local', '--debug'],
+            ['print', 'status', '-r', '--debug'],
+            ['print', 'config', '--debug'],
+            ['print', 'resinfo', '--debug'],
+            ['print', 'devs', '--debug'],
+            ['delete', '--unprovision', '--rid', unprovision_rid, '--debug']
+        ]:
+            cmd_args = ["-s", svcname] + args
+            print('run Mgr()(argv=%s)' % cmd_args)
+            assert Mgr()(argv=cmd_args) == 0
