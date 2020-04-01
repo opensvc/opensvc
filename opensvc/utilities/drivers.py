6ce29df681a05a0c1b7fcb5052da367df8797682
Christophe Varoqui <christophe.varoqui@opensvc.com>
2020-04-01 19:08:54 +0200

    Lookup drivers in a fallback package "site-opensvc" if not found in "drivers"
    And make "bin/om" honor the PYTHONPATH set in /etc/default/opensvc, in case
    the site-opensvc package is not in a standard location.

--- opensvc/utilities/drivers.py
+++ opensvc/utilities/drivers.py
@@ -5,6 +5,13 @@ from env import Env
 
 
 def driver_import(*args, **kwargs):
+    try:
+        return _driver_import(*args, **kwargs)
+    except ImportError as exc:
+        kwargs["head"] = "site-opensvc.drivers"
+        return _driver_import(*args, **kwargs)
+
+def _driver_import(*args, head="drivers", fallback=True, initial_modname=None):
     def fmt_element(s):
         if s is None:
             return ""
@@ -14,7 +21,7 @@ def driver_import(*args, **kwargs):
         return s.lower().replace("-", "")
 
     def fmt_modname(args):
-        l = ["drivers"]
+        l = [head]
         for i, e in enumerate(args):
             if e == "":
                 continue
@@ -35,17 +42,19 @@ def driver_import(*args, **kwargs):
                 pass
 
     modname = fmt_modname(args)
-    mod = import_mod(modname)
+    if not initial_modname:
+        initial_modname = modname
 
+    mod = import_mod(modname)
     if mod:
+        if args[0] == "resource" and not hasattr(mod, "adder"):
+            raise ImportError("no module found: %s" % initial_modname)
         return mod
-    if not kwargs.get("head"):
-        kwargs["head"] = modname
-    if kwargs.get("fallback", True) and len(args) > 2:
+    if fallback and len(args) > 2:
         args = args[:-1]
-        return driver_import(*args, **kwargs)
+        return _driver_import(*args, fallback=fallback, initial_modname=initial_modname)
     else:
-        raise ImportError("no module found: %s" % kwargs["head"])
+        raise ImportError("no module found: %s" % initial_modname)
 
 
 def iter_drivers(groups=None):
