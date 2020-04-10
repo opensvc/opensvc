abd949db607bc0b6f66afc6dc2233080793b4753
Christophe Varoqui <christophe.varoqui@opensvc.com>
2020-04-10 14:20:20 +0200

    Speedup the os-declined module import time
    Using the Env.module_sysname instead of calling platform.uname() for each of
    these modules.

--- opensvc/core/sysreport/__init__.py
+++ opensvc/core/sysreport/__init__.py
@@ -1,7 +1,6 @@
 import importlib
-import platform
+from env import Env
 
-_sysname = platform.uname()[0].lower().replace("-", "")
 _package = __package__ or __spec__.name # pylint: disable=undefined-variable
-_os = importlib.import_module("." + _sysname, package=_package)
+_os = importlib.import_module("." + Env.module_sysname, package=_package)
 SysReport = _os.SysReport
