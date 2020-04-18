2366f42e43af49ad9e46d671ca50100cdbdaea7e
Cyril Galibern <cyril.galibern@opensvc.com>
2020-04-18 15:56:16 +0200

    [test] Add temp mock nfs share capability during TestDriverShareNfsInstances

--- opensvc/tests/resource/share/nfs/test_share_nfs.py
+++ opensvc/tests/resource/share/nfs/test_share_nfs.py
@@ -10,8 +10,15 @@ SCENARIOS = [
 ]
 
 
+@pytest.fixture(scope='function')
+def share_capability(mocker):
+    return mocker.patch('core.capabilities.capabilities',
+                        new=['node.x.exportfs', 'node.x.share'])
+
+
 @pytest.mark.ci
-@pytest.mark.usefixtures('osvc_path_tests', 'which')
+@pytest.mark.usefixtures('osvc_path_tests')
+@pytest.mark.usefixtures('share_capability')
 @pytest.mark.parametrize('sysname', OS_LIST)
 @pytest.mark.parametrize('scenario', SCENARIOS)
 class TestDriverShareNfsInstances:
