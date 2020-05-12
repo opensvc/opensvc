cce30c33c23bf74a05ec72ce53f7349aa376c3f1
Cyril Galibern <cyril.galibern@opensvc.com>
2020-05-12 17:36:15 +0200

    [drivers.container.zone] Fix get zonepath value errors (when zone2clone is not yet created)
    This may occur during creation of zone2clone source image

--- opensvc/tests/resource/container/zone/test_zone.py
+++ opensvc/tests/resource/container/zone/test_zone.py
@@ -7,17 +7,18 @@ LIB_CLASS = 'drivers.resource.container.zone.ContainerZone'
 
 
 @pytest.fixture(scope='function')
-def svc(mocker):
+def svc(mocker, tmp_path):
     svc = mocker.Mock(name='svc')
     svc.namespace = ''
     svc.name = 's'
     svc.loggerpath = 'something'
+    svc.var_d = tmp_path
     return svc
 
 
 @pytest.fixture(scope='function')
 def zone(svc):
-    zone = ContainerZone(rid='container#1')
+    zone = ContainerZone(rid='container#1', zonepath='/z/zonepath_kw')
     zone.svc = svc
     return zone
 
@@ -43,3 +44,36 @@ class TestContainerZoneProvisionWhenZoneIsInstalled:
     def test_returns_without_actions(zone_configure, zone):
         zone.provisioner()
         assert zone_configure.call_count == 0
+
+
+@pytest.mark.zone
+@pytest.mark.ci
+@pytest.mark.usefixtures('osvc_path_tests')  # for cache
+class TestContainerZonepath:
+    @staticmethod
+    def test_is_value_from_zoneadm(zone, with_installed_zone):
+        with_installed_zone['zonepath'] = '/z/fromzoneadm'
+        assert zone.zonepath == '/z/fromzoneadm'
+
+    @staticmethod
+    def test_fallback_to_zonecfg(mocker, zone):
+        """Not sure this is possible issue"""
+        mocker.patch.object(ContainerZone, 'get_zonepath_from_zonecfg_cmd',
+                            return_value='/z/zonepath_from_zonecfg')
+        assert zone.zonepath == '/z/zonepath_from_zonecfg'
+
+    @staticmethod
+    def test_fallback_to_zonecfg_exported(mocker, zone):
+        mocker.patch.object(ContainerZone, 'get_zonepath_from_zonecfg_export',
+                            return_value='/z/zonecfg_exported')
+        assert zone.zonepath == '/z/zonecfg_exported'
+
+    @staticmethod
+    def test_fallback_to_resource_zonepath_kw(zone):
+        assert zone.zonepath == '/z/zonepath_kw'
+
+    @staticmethod
+    def test_fallback_to_none(svc):
+        zone = ContainerZone(rid='container#1')
+        zone.svc = svc
+        assert zone.zonepath is None
