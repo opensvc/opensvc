8311ddc8a7568f43de3c912975809c0e9a9a411a
Christophe Varoqui <christophe.varoqui@opensvc.com>
2020-05-13 10:44:22 +0200

    Move the "om mon" path selector trimming to format_path_selector()
    Add a new "maxlen" kwarg, use it in utilities.render.cluster

--- opensvc/utilities/render/cluster/__init__.py
+++ opensvc/utilities/render/cluster/__init__.py
@@ -967,9 +967,7 @@ def format_cluster(paths=None, node=None, data=None, prev_stats_data=None,
         context = os.environ.get("OSVC_CONTEXT", "")
         if context:
             selectors.append(context)
-        buff = format_path_selector(selector, namespace)
-        if len(buff) > 15:
-            buff = buff[:12] + "..."
+        buff = format_path_selector(selector, namespace, maxlen=15)
         selectors.append(buff)
         load_header([
             "/".join(selectors),
