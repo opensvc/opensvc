149b8e8df5edd6f68e99419f7665018c259bea88
Cyril Galibern <cyril.galibern@opensvc.com>
2020-05-14 20:05:42 +0200

    [drivers.container.zone] Only allow provisioning when container_origin (sol10 and sol11) or snapof on solaris 10

--- opensvc/tests/resource/container/zone/test_zone.py
+++ opensvc/tests/resource/container/zone/test_zone.py
@@ -7,6 +7,26 @@ LIB_CLASS = 'drivers.resource.container.zone.ContainerZone'
 
 
 @pytest.fixture(scope='function')
+def zone_configure(mocker):
+    return mocker.patch.object(ContainerZone, 'zone_configure')
+
+
+@pytest.fixture(scope='function')
+def create_snaped_zone(mocker):
+    return mocker.patch.object(ContainerZone, 'create_snaped_zone')
+
+
+@pytest.fixture(scope='function')
+def create_cloned_zone(mocker, tmp_path):
+    return mocker.patch.object(ContainerZone, 'create_cloned_zone')
+
+
+@pytest.fixture(scope='function')
+def zone_boot(mocker):
+    return mocker.patch.object(ContainerZone, 'zone_boot')
+
+
+@pytest.fixture(scope='function')
 def svc(mocker, tmp_path):
     svc = mocker.Mock(name='svc')
     svc.namespace = ''
@@ -75,3 +95,32 @@ class TestContainerZonepath:
         zone = ContainerZone(rid='container#1')
         zone.svc = svc
         assert zone.zonepath is None
+
+
+@pytest.mark.zone
+@pytest.mark.ci
+@pytest.mark.usefixtures('osvc_path_tests')  # for cache
+class TestContainerProvision:
+    @staticmethod
+    def test_return_none_when_invalid_kw_without_doing_anything(zone_configure, create_snaped_zone,
+                                                                create_cloned_zone, zone_boot,
+                                                                zone):
+        result = zone.provisioner()
+
+        assert result is False
+        assert zone_configure.assert_not_called
+        assert create_snaped_zone.assert_not_called
+        assert create_cloned_zone.assert_not_called
+        assert zone_boot.assert_not_called
+
+    @staticmethod
+    def test_return_true_when_already_provisioned(with_installed_zone, zone_configure,
+                                                  create_snaped_zone, create_cloned_zone,
+                                                  zone_boot, zone):
+        result = zone.provisioner() is True
+
+        assert result is True
+        assert zone_configure.assert_not_called
+        assert create_snaped_zone.assert_not_called
+        assert create_cloned_zone.assert_not_called
+        assert zone_boot.assert_not_called
