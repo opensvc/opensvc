opensvc/opensvc

commit 4605f672a67e1fef1475ba8870d2e30fc2d5eea1

[drivers.resource.ip.host] Fix netmask value for Solaris
Avoid use of ifconfig ... netmask +
Instead use detected netmask value

Reason:
 netmask + use detected net networks in /etc/inet/netmask
 when network is not found in netmask file, ifconfig netmask +
 will set netmask from standard ip classes

 This may lead to start ip with incorrect netmask, and then
 have a service print status that complain about invalid netmask

opensvc/drivers/resource/ip/host/sunos.py
@@ -1,4 +1,5 @@
 import utilities.ping
+from utilities.net.converters import cidr_to_dotted, hexmask_to_dotted
 
 from .. import Ip
 
@@ -24,7 +25,7 @@ class IpHost(Ip):
         cmd = [
             "/usr/sbin/ifconfig", self.stacked_dev,
             "plumb", self.addr,
-            "netmask", "+", "broadcast", "+", "up",
+            "netmask", hexmask_to_dotted(self.netmask), "broadcast", "+", "up",
         ]
         return self.vcall(cmd)
 

opensvc/tests/resource/ip/test_ip_host_sunos.py
@@ -1,0 +1,53 @@
+import pytest
+
+from utilities.drivers import driver_import, driver_class
+
+IFCONFIG = """lo0: flags=2001000849<UP,LOOPBACK,RUNNING,MULTICAST,IPv4,VIRTUAL> mtu 8232 index 1
+\tinet 127.0.0.1 netmask ff000000
+net0: flags=100001004843<UP,BROADCAST,RUNNING,MULTICAST,DHCP,IPv4,PHYSRUNNING> mtu 1500 index 2
+\tinet 10.0.2.10 netmask ffffff00 broadcast 10.0.2.255
+\tether 2:8:20:b2:e8:3a
+net1: flags=100001000843<UP,BROADCAST,RUNNING,MULTICAST,IPv4,PHYSRUNNING> mtu 1500 index 2
+\tinet 10.22.0.11 netmask ffff0000 broadcast 10.0.255.255
+\tether 2:8:20:b2:e8:3b
+net2: flags=100001000843<UP,BROADCAST,RUNNING,MULTICAST,IPv4,PHYSRUNNING> mtu 1500 index 2
+\tinet 10.22.0.12 netmask ff000000 broadcast 10.0.2.255
+\tether 2:8:20:b2:e8:3c
+"""
+
+
+@pytest.fixture(scope='function')
+def ip_class(mocker, mock_sysname):
+    mock_sysname('SunOS')
+    klass = driver_class(driver_import('resource', 'ip', 'Host'))
+    from utilities.ifconfig.sunos import Ifconfig
+    ifconfig = Ifconfig(ifconfig=str(IFCONFIG))
+    log = mocker.Mock('log', info=mocker.Mock('info'), debug=mocker.Mock('debug'))
+    mocker.patch.object(klass, 'vcall', mocker.Mock('vcall'))
+    mocker.patch.object(klass, 'log', log)
+    mocker.patch.object(klass, 'get_ifconfig', mocker.Mock('get_ifconfig', return_value=ifconfig))
+    return klass
+
+
+@pytest.mark.ci
+@pytest.mark.usefixtures('osvc_path_tests')
+class TestIpStartCmd:
+    @staticmethod
+    @pytest.mark.parametrize('ipdev,expected_netmask',
+                             [('net0', '255.255.255.0'),
+                              ('net1', '255.255.0.0'),
+                              ('net2', '255.0.0.0'),
+                              ])
+    def test_call_with_correct_netmask(ip_class, ipdev, expected_netmask):
+        ip = ip_class(ipname='192.168.0.149', ipdev=ipdev)
+        ip.addr = ip.ipname
+        ip.get_stack_dev()
+        ip.startip_cmd()
+
+        expected_cmd = ["/usr/sbin/ifconfig", ipdev + ':1',
+                        "plumb", '192.168.0.149',
+                        "netmask", expected_netmask,
+                        "broadcast", "+",
+                        "up",
+        ]
+        ip.vcall.assert_called_once_with(expected_cmd)

opensvc/drivers/resource/ip/__init__.py
@@ -587,17 +587,10 @@ class Ip(Resource):
         """
         The start codepath fragment protected by the startip lock.
         """
-        ifconfig = self.get_ifconfig()
-        self.get_mask(ifconfig)
-        if 'noalias' in self.tags:
-            self.stacked_dev = self.ipdev
-        else:
-            self.stacked_dev = ifconfig.get_stacked_dev(self.ipdev,\
-                                                        self.addr,\
-                                                        self.log)
+        self.get_stack_dev()
         if self.stacked_dev is None:
             raise ex.Error("could not determine a stacked dev for parent "
-                              "interface %s" % self.ipdev)
+                           "interface %s" % self.ipdev)
 
         arp_announce = True
         try:
@@ -612,6 +605,16 @@ class Ip(Resource):
             raise ex.Error("failed")
 
         return arp_announce
+
+    def get_stack_dev(self):
+        ifconfig = self.get_ifconfig()
+        self.get_mask(ifconfig)
+        if 'noalias' in self.tags:
+            self.stacked_dev = self.ipdev
+        else:
+            self.stacked_dev = ifconfig.get_stacked_dev(self.ipdev,
+                                                        self.addr,
+                                                        self.log)
 
     def dns_update(self):
         """

commit eb1ba65bce04cd415ea364cc7e6c98e35ec4ae03

[drivers.resource.ip.host] Fix regression in netmask value for Solaris
It didn't respect anymore netmask keyword value from service ip config

The regression was introduce by 4605f672a67e1fef1475ba8870d2e30fc2d5eea1 [drivers.resource.ip.host] Fix netmask value for Solaris)

opensvc/drivers/resource/ip/host/sunos.py
@@ -1,5 +1,5 @@
 import utilities.ping
-from utilities.net.converters import cidr_to_dotted, hexmask_to_dotted
+from utilities.net.converters import cidr_to_dotted, to_cidr
 
 from .. import Ip
 
@@ -25,11 +25,10 @@ class IpHost(Ip):
         cmd = [
             "/usr/sbin/ifconfig", self.stacked_dev,
             "plumb", self.addr,
-            "netmask", hexmask_to_dotted(self.netmask), "broadcast", "+", "up",
+            "netmask", cidr_to_dotted(to_cidr(self.netmask)), "broadcast", "+", "up",
         ]
         return self.vcall(cmd)
 
     def stopip_cmd(self):
         cmd = ["/usr/sbin/ifconfig", self.stacked_dev, "unplumb"]
         return self.vcall(cmd)
-

opensvc/tests/resource/ip/test_ip_host_sunos.py
@@ -33,13 +33,18 @@ def ip_class(mocker, mock_sysname):
 @pytest.mark.usefixtures('osvc_path_tests')
 class TestIpStartCmd:
     @staticmethod
-    @pytest.mark.parametrize('ipdev,expected_netmask',
-                             [('net0', '255.255.255.0'),
-                              ('net1', '255.255.0.0'),
-                              ('net2', '255.0.0.0'),
+    @pytest.mark.parametrize('ipdev, netmask, expected_netmask',
+                             [('net0', None, '255.255.255.0'),
+                              ('net1', None, '255.255.0.0'),
+                              ('net2', None, '255.0.0.0'),
+                              ('net2', '24', '255.255.255.0'),
+                              ('net2', '8', '255.0.0.0'),
                               ])
-    def test_call_ifconfig_with_correct_netmask_value(ip_class, ipdev, expected_netmask):
-        ip = ip_class(ipname='192.168.0.149', ipdev=ipdev)
+    def test_call_ifconfig_with_correct_netmask_value(ip_class, ipdev, netmask, expected_netmask):
+        kwargs = {'ipname': '192.168.0.149', 'ipdev': ipdev}
+        if netmask:
+            kwargs['netmask'] = netmask
+        ip = ip_class(**kwargs)
         ip.addr = ip.ipname
         ip.get_stack_dev()
         ip.startip_cmd()