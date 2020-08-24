54cff2340dd6da87380a248898c39514c62f35ec
Cyril Galibern <cyril.galibern@opensvc.com>
2020-08-24 22:09:24 +0200

    Fix fs flag provisioner for SunOS
    This commit will fix following stack during provision fs flag on SunOS
    ```
            getattr(self, action)()
          File "/usr/share/opensvc/lib/resources.py", line 1172, in provision
            self._provision()
          File "/usr/share/opensvc/lib/resources.py", line 1193, in _provision
            if self.is_provisioned(refresh=self.refresh_provisioned_on_provision) is True:
          File "/usr/share/opensvc/lib/resources.py", line 1254, in is_provisioned
            value = self.prov.is_provisioned()
          File "/usr/share/opensvc/lib/provFs.py", line 45, in is_provisioned
            if "bind" in self.r.mount_options or self.r.fs_type in ("bind", "lofs"):
        AttributeError: 'Fs' object has no attribute 'mount_options'
    ```

---
+++ lib/prov_fs_flag_abstract.py
@@ -0,0 +1,18 @@
+import provisioning
+
+
+class Prov(provisioning.Prov):
+    def __init__(self, r):
+        provisioning.Prov.__init__(self, r)
+    
+    def is_provisioned(self):
+        flag = self.r.is_provisioned_flag()
+        if flag is None:
+            return False
+        return flag
+
+    def provisioner(self):
+        pass
+
+    def unprovisioner(self):
+        pass
