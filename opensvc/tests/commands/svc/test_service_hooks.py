40bb52bf3e098e238b36a9fcc00d4138c6e5c44c
Cyril Galibern <cyril.galibern@opensvc.com>
2020-09-08 10:44:28 +0200

    Add service hooks for startstandby action
    With this commit following hook may be defined in service:
      o pre_startstandby
      o post_startstandby
      o blocking_pre_startstandby
      o blocking_post_startstandby
    
    Add test spec for service hooks calls
     TestSvcHooks:
       o test_call_one_action_hook
           o [provision-pre]
           o [provision-post]
           o [provision-blocking_pre]
           o [provision-blocking_post]
           o [unprovision-pre]
           o [unprovision-post]
           o [unprovision-blocking_pre]
           o [unprovision-blocking_post] PASSED
           o [start-pre]
           o [start-post]
           o [start-blocking_pre]
           o [start-blocking_post]
           o [startstandby-pre]
           o [startstandby-post]
           o [startstandby-blocking_pre] PASSED
           o [startstandby-blocking_post] PASSED
           o [stop-pre]
           o [stop-post]
           o [stop-blocking_pre]
           o [stop-blocking_post]
           o [sync_nodes-pre]
           o [sync_nodes-post]
           o [sync_nodes-blocking_pre]
           o [sync_nodes-blocking_post]
           o [sync_drp-pre]
           o [sync_drp-post]
           o [sync_drp-blocking_pre]
           o [sync_drp-blocking_post]
           o [sync_resync-pre]
           o [sync_resync-post]
           o [sync_resync-blocking_pre]
           o [sync_resync-blocking_post] PASSED
           o [sync_update-pre]
           o [sync_update-post]
           o [sync_update-blocking_pre]
           o [sync_update-blocking_post] PASSED
           o [sync_restore-pre]
           o [sync_restore-post]
           o [sync_restore-blocking_pre] PASSED
           o [sync_restore-blocking_post] PASSED
           o [run-pre]
           o [run-post]
           o [run-blocking_pre]
           o [run-blocking_post]
        o test_call_shutdown_action_stop_hook
           o [pre]
           o [post]
           o [blocking_pre]
           o [blocking_post]
    
         o test_cmd_all_action_hooks
           o [start]
           o [stop]
           o [startstandby]
           o [provision]
           o [unprovision]

---
+++ opensvc/tests/commands/svc/test_service_hooks.py
@@ -0,0 +1,84 @@
+import os
+import pytest
+
+from commands.svc import Mgr
+import utilities.proc
+
+
+def assert_run_cmd_success(svcname, svc_cmd_args):
+    cmd_args = ["-s", svcname] + svc_cmd_args
+    print('--------------')
+    print('run Mgr()(argv=%s)' % cmd_args)
+    assert Mgr()(argv=cmd_args) == 0
+
+
+@pytest.fixture(scope='function')
+def check_io_timeout(mocker):
+    """Improve test time"""
+    mocker.patch.object(utilities.proc, 'LCALL_CHECK_IO_TIMEOUT', new=0.001)
+
+
+@pytest.mark.ci
+@pytest.mark.usefixtures('osvc_path_tests')
+@pytest.mark.usefixtures('has_euid_0')
+@pytest.mark.usefixtures('check_io_timeout')
+class TestSvcHooks:
+    @staticmethod
+    @pytest.mark.parametrize(
+        'prefix',
+        ['pre', 'post', 'blocking_pre', 'blocking_post']
+    )
+    @pytest.mark.parametrize(
+        'action',
+        [
+            'provision',
+            'unprovision',
+            'start',
+            'startstandby',
+            'stop',
+            'sync_nodes',
+            'sync_drp',
+            # 'sync_all', no sync_all keyword defined yet
+            'sync_resync',
+            'sync_update',
+            'sync_restore',
+            'run',
+        ]
+    )
+    def test_call_one_action_hook(mocker, osvc_path_tests, action, prefix):
+        svcname = "pytest"
+        mocker.patch.dict(os.environ, {'OSVC_DETACHED': '1'})
+        hook = '%s_%s' % (prefix, action)
+        assert_run_cmd_success(svcname, ['create', '--kw', 'DEFAULT.%s=/usr/bin/touch {private_var}/%s' % (hook, hook)])
+        assert_run_cmd_success(svcname, [action, '--local', '--debug'])
+        assert os.path.exists(os.path.join(str(osvc_path_tests), 'var', 'svc', svcname, hook))
+
+    @staticmethod
+    @pytest.mark.parametrize(
+        'prefix',
+        ['pre', 'post', 'blocking_pre', 'blocking_post']
+    )
+    def test_call_shutdown_action_stop_hook(mocker, osvc_path_tests, prefix):
+        svcname = "pytest"
+        mocker.patch.dict(os.environ, {'OSVC_DETACHED': '1'})
+        hook = '%s_stop' % prefix
+        assert_run_cmd_success(svcname, ['create', '--kw', 'DEFAULT.%s=/usr/bin/touch {private_var}/%s' % (hook, hook)])
+        assert_run_cmd_success(svcname, ['shutdown', '--local', '--debug'])
+        assert os.path.exists(os.path.join(str(osvc_path_tests), 'var', 'svc', svcname, hook))
+
+    @staticmethod
+    @pytest.mark.parametrize(
+        'action',
+        ['start', 'stop', 'startstandby', 'provision', 'unprovision']
+    )
+    def test_cmd_all_action_hooks(mocker, osvc_path_tests, action):
+        svcname = "pytest"
+        mocker.patch.dict(os.environ, {'OSVC_DETACHED': '1'})
+        service_hooks = ["%s_%s" % (prefix, action) for prefix in ['pre', 'blocking_pre', 'post', 'blocking_post']]
+        create_args = ['create']
+        for name in service_hooks:
+            create_args.extend(['--kw', 'DEFAULT.%s=/usr/bin/touch {private_var}/%s' % (name, name)])
+        assert_run_cmd_success(svcname, create_args)
+        assert_run_cmd_success(svcname, [action, '--local', '--debug'])
+        for name in service_hooks:
+            assert os.path.exists(os.path.join(str(osvc_path_tests), 'var', 'svc', svcname, str(name)))
