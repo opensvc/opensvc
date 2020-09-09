opensvc/opensvc

commit 20cfc94ff39f6f774e4600a9e30a6f242612e644

Log finer origin of service action in encap nodes
The "user" and "daemon" origins currently logged on service action is
confusing, because when the master daemon triggered action is relayed in
the encap node it appears as a "user" action.

This patch introduces two new origins : "master user" and "master daemon".

opensvc/core/objects/svc.py
@@ -1338,11 +1338,9 @@ class BaseSvc(Crypt, ExtConfigMixin):
                 else:
                     _begin = 1
                 argv = self.log_action_obfuscate_secret(argv)
-                runlog = "do "+" ".join(argv[_begin:]).replace("%", "%%")
-                if os.environ.get("OSVC_ACTION_ORIGIN") == "daemon":
-                    runlog += " (daemon origin)"
-                else:
-                    runlog += " (user origin)"
+                cmd = " ".join(argv[_begin:]).replace("%", "%%")
+                origin = os.environ.get("OSVC_ACTION_ORIGIN", "user")
+                runlog = "do %s (%s origin)" % (cmd, origin)
                 self.log.info(runlog, {"f_stream": False})
         except IndexError:
             pass
@@ -4142,7 +4140,7 @@ class Svc(BaseSvc):
         if verbose:
             self.log.info(" ".join(cmd))
 
-        cmd = ["env", "OSVC_DETACHED=1"] + cmd
+        cmd = ["env", "OSVC_DETACHED=1", "OSVC_ACTION_ORIGIN=master %s" % os.environ.get("OSVC_ACTION_ORIGIN", "user")] + cmd
         if container is not None and hasattr(container, "rcmd") and callable(container.rcmd):
             try:
                 out, err, ret = container.rcmd(cmd)

commit 4559d7d48cd1b7b39565b1c5bbb6304d5eaba402

Fix encap action failure (regression in 20cfc94ff39f6f774e4600a9e30a6f242612e644)
OSVC_ACTION_ORIGIN=master user /usr/bin/om ...
instead of
OSVC_ACTION_ORIGIN='master user' /usr/bin/om ...

opensvc/core/objects/svc.py
@@ -4144,7 +4144,7 @@ class Svc(BaseSvc):
         if verbose:
             self.log.info(" ".join(cmd))
 
-        cmd = ["env", "OSVC_DETACHED=1", "OSVC_ACTION_ORIGIN=master %s" % os.environ.get("OSVC_ACTION_ORIGIN", "user")] + cmd
+        cmd = ["env", "OSVC_DETACHED=1", "OSVC_ACTION_ORIGIN='master %s'" % os.environ.get("OSVC_ACTION_ORIGIN", "user")] + cmd
         if container is not None and hasattr(container, "rcmd") and callable(container.rcmd):
             try:
                 out, err, ret = container.rcmd(cmd)