6e79850f8ba02add672dee30b6041a9b4846551c
Cyril Galibern <cyril.galibern@opensvc.com>
2020-09-17 16:32:37 +0200

    Fix api client for python2

--- opensvc/utilities/requester/api_client.py
+++ opensvc/utilities/requester/api_client.py
@@ -8,14 +8,13 @@ class ApiClient:
         self._auth_factory = auth_factory
         self._url_util = url_util
 
-    def action(self, method, url, auth_info=None, headers=None, *args, **kwargs):
+    def action(self, method='get', url=None, auth_info=None, headers=None, **kwargs):
         new_header = {}
         new_url = self._url_util.get_url(url)
         new_header.update(self._url_util.get_header(url))
-        new_header.update(self._auth_factory.get_header(auth_info or {}))
+        new_header.update(self._auth_factory.get_headers(auth_info or {}))
         new_header.update(headers or {})
 
         return getattr(requests, method)(url=new_url,
                                          headers=new_header,
-                                         *args,
                                          **kwargs)
