376446bc8b851a3ede2cb6121422271d6d77849b
Cyril Galibern <cyril.galibern@opensvc.com>
2020-09-18 04:15:52 +0200

    Add noop_log
    This may be used to define default fake logger
    
    import opensvc.utilities.noop_log as noop_log
    
    noop_log.info('message')
    or
    def foo(log=noop_log):
        log.error('Hi')

---
+++ opensvc/utilities/noop_log/__init__.py
@@ -0,0 +1,18 @@
+# noinspection PyUnusedLocal
+def info(*args, **kwargs):
+    pass
+
+
+# noinspection PyUnusedLocal
+def warn(*args, **kwargs):
+    pass
+
+
+# noinspection PyUnusedLocal
+def debug(*args, **kwargs):
+    pass
+
+
+# noinspection PyUnusedLocal
+def error(*args, **kwargs):
+    pass
