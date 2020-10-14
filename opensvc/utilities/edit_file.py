fb2558686ca0570a1ed5c4581b2e9a869750fa42
Cyril Galibern <cyril.galibern@opensvc.com>
2020-10-15 00:44:01 +0200

    [lint] Disable pylint no-member for python 2.7 (edit_file.py)

--- opensvc/utilities/edit_file.py
+++ opensvc/utilities/edit_file.py
@@ -2,4 +2,5 @@ import os
 
 
 def edit_file(editor, fpath):
-    os.system(' '.join((editor, fpath)))
+    # for some pylint disable no-member error (python 2.7)
+    os.system(' '.join((editor, fpath)))  # pylint: disable=no-member
