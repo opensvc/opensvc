3e7e7a2ae99e14627a29e1589da9a34ed31e177c
Cyril Galibern <cyril.galibern@opensvc.com>
2020-10-14 23:49:47 +0200

    try remove pylint error

--- opensvc/utilities/edit_file.py
+++ opensvc/utilities/edit_file.py
@@ -1,5 +1,9 @@
+"""
+edit_file function
+"""
+
 import os
 
 
 def edit_file(editor, fpath):
-    os.system(' '.join((editor, fpath)))
+    os.system(' '.join((editor, fpath)))  # pylint: disable=no-member
