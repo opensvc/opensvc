c28fd63e4831e2344f6a15637180f313d172b4df
Cyril Galibern <cyril.galibern@opensvc.com>
2020-10-15 14:05:18 +0200

    [test] Ensure string secret created from files can be edited
    o opensvc/tests/test mgr.py::TestCfgSecEdit
      o test can edit editable objects created from file (sec)

--- opensvc/utilities/edit_file.py
+++ opensvc/utilities/edit_file.py
@@ -2,4 +2,5 @@ import os
 
 
 def edit_file(editor, fpath):
-    os.system(' '.join((editor, fpath)))
+    # for python 2.7 pylint disable no-member
+    os.system(' '.join((editor, fpath)))  # pylint: disable=no-member
