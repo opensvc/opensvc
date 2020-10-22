57fec71f2ae0cfcc4403495c85e39624460df7ff
Christophe Varoqui <christophe.varoqui@opensvc.com>
2020-10-22 12:08:48 +0200

    Add a threadsafe implementation of lazy

--- opensvc/utilities/lazy.py
+++ opensvc/utilities/lazy.py
@@ -1,3 +1,25 @@
+from threading import RLock
+
+_missing = object()
+
+class threadsafe_lazy(object):
+    def __init__(self, func):
+        self.__name__ = "_lazy_" + func.__name__
+        self.__module__ = func.__module__
+        self.__doc__ = func.__doc__
+        self.func = func
+        self.lock = RLock()
+
+    def __get__(self, obj, type=None):
+        if obj is None:
+            return self
+        with self.lock:
+            value = obj.__dict__.get(self.__name__, _missing)
+            if value is _missing:
+                value = self.func(obj)
+                obj.__dict__[self.__name__] = value
+            return value
+
 def lazy(fn):
     """
     A decorator for on-demand initialization of a property
@@ -12,15 +34,12 @@ def lazy(fn):
 
     return _lazyprop
 
-
 def lazy_initialized(self, attr):
     """
     Return True if the lazy property has been initialized
     """
     attr_name = '_lazy_' + attr
-    if hasattr(self, attr_name):
-        return True
-    return False
+    return hasattr(self, attr_name)
 
 
 def set_lazy(self, attr, value):
@@ -38,7 +57,10 @@ def unset_all_lazy(self, exclude=None):
     exclude = exclude or []
     for attr in [attr for attr in self.__dict__]:
         if attr.startswith("_lazy_") and attr[6:] not in exclude:
-            delattr(self, attr)
+            try:
+                delattr(self, attr)
+            except AttributeError:
+                pass
 
 
 def unset_lazy(self, attr):
@@ -46,8 +68,10 @@ def unset_lazy(self, attr):
     Unset <attr> lazy property hidden property, iow flush the cache
     """
     attr_name = '_lazy_' + attr
-    if hasattr(self, attr_name):
+    try:
         delattr(self, attr_name)
+    except AttributeError:
+        pass
 
 
 
