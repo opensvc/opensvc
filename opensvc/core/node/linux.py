5dd1a469eda3beaca53143e725f0b34d48f194e3
Christophe Varoqui <christophe.varoqui@opensvc.com>
2020-10-27 15:35:30 +0100

    Switch between "ip" and "ip -6" commands as necessary in "om net setup" codepath

--- opensvc/core/node/linux.py
+++ opensvc/core/node/linux.py
@@ -112,17 +112,21 @@ class Node(BaseNode):
     def network_route_add(self, dst=None, gw=None, dev=None, local_ip=None, brdev=None, brip=None, table=None, tunnel="auto", **kwargs):
         if dst is None:
             return
+        if ":" in dst:
+            cmd = ["ip", "-6"]
+        else:
+            cmd = ["ip"]
         if tunnel == "auto":
             if gw is not None:
-                cmd = ["ip", "route", "replace", dst, "via", gw, "table", table]
+                cmd += ["route", "replace", dst, "via", gw, "table", table]
             elif dev is not None:
-                cmd = ["ip", "route", "replace", dst, "dev", dev, "table", table]
+                cmd += ["route", "replace", dst, "dev", dev, "table", table]
             out, err, ret = justcall(cmd)
         else:
             err = ""
         if tunnel == "always" or "invalid gateway" in err or "is unreachable" in err:
             tun = self.network_tunnel_ipip_add(local_ip, gw)
-            cmd = ["ip", "route", "replace", dst, "dev", tun["dev"], "src", brip.split("/")[0], "table", table]
+            cmd += ["route", "replace", dst, "dev", tun["dev"], "src", brip.split("/")[0], "table", table]
             self.vcall(cmd)
         else:
             self.log.info(" ".join(cmd))
