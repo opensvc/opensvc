85fae790d53b66fcaaf55da3a7f04e22f2782e46
Cyril Galibern <cyril.galibern@opensvc.com>
2020-12-18 18:51:16 +0100

    [test] Refacto before adding more orchestrator tests

--- opensvc/tests/daemon/monitor/test_orchestrator_start.py
+++ opensvc/tests/daemon/monitor/test_orchestrator_start.py
@@ -102,33 +102,73 @@ def mock_daemon(mocker, osvc_path_tests):
     shared.DAEMON = Daemon()
 
 
-def assert_nmon_status(log_info, status, message='reason'):
-    log_info('COMMENT: ASSERT monitor status is "%s" (reason: %s), full value is %s',
-             status, message, json.dumps(shared.NMON_DATA))
-    assert shared.NMON_DATA['status'] == status
+class MonitorTest(object):
+    def __init__(self, mocker=None, cluster_nodes=None):
+        self.mocker = mocker
+        if mocker:
+            self.mocker.patch.object(Monitor, 'monitor_period', 0.000001)
+        self.monitor = Monitor()
+        self.monitor._lazy_cluster_nodes = cluster_nodes or []
+        self.execs = {"service_command_exe": env.Env.syspaths.true}
+        self.service_command = None
 
+    def set_service_command_cmd(self, cmd):
+        self.execs["service_command_exe"] = cmd
 
-def assert_smon_status(log_info, svcname, status, message='reason'):
-    log_info('COMMENT: ASSERT smon status for svc %s is "%s" (reason: %s), full value is %s',
-             svcname, status, message, json.dumps(shared.SMON_DATA[svcname]))
-    assert shared.SMON_DATA[svcname]['status'] == status
+    def service_command_factory(self):
+        def service_command_mock(*args, **kwargs):
+            self.log('detect mock service_command(%s, %s)' % (args, kwargs))
+            return Popen(self.execs["service_command_exe"])
 
+        self.service_command = self.mocker.patch.object(Monitor,
+                                                        'service_command',
+                                                        side_effect=service_command_mock)
+        return self.service_command
 
-def assert_smon_local_expect(log_info, svcname, local_expect, message='reason'):
-    log_info('COMMENT: ASSERT smon local_expect for svc %s is "%s" (reason: %s), full value is %s',
-             svcname, local_expect, message, json.dumps(shared.SMON_DATA[svcname]))
-    assert shared.SMON_DATA[svcname]['local_expect'] == local_expect
+    def log(self, *args, **kwargs):
+        if self.monitor and getattr(self.monitor, 'log'):
+            self.monitor.log.info(*args, **kwargs)
+        else:
+            shared.NODE.log.info(*args, **kwargs)
 
+    def create_svc_config(self, s):
+        pathetc = env.Env.paths.pathetc
+        if not os.path.exists(pathetc):
+            os.mkdir(pathetc)
+        with open(os.path.join(pathetc, '%s.conf' % s), mode='w+') as svc_file:
+            svc_file.write(SERVICES[s])
+        self.log('COMMENT: created %s service with config \n%s\n', s, SERVICES[s])
 
-def assert_command_has_been_launched(log_info, call_list, call_args_list):
-    for call in call_list:
-        log_info('COMMENT: ASSERT call %s has been called', call)
-        assert call in call_args_list
+    def do(self):
+        self.log('\nCOMMENT: monitor.do()')
+        time.sleep(0.01)  # Simulate time elapsed between 2 do() iteration
+        self.monitor.do()
+        self.log('COMMENT: proc ps count = %s', len(self.monitor.procs))
 
-def assert_command_has_not_been_launched(log_info, call_list, call_args_list):
-    for call in call_list:
-        log_info('COMMENT: ASSERT call %s has not been called', call)
-        assert call not in call_args_list
+    def assert_nmon_status(self, status, message='reason'):
+        self.log('COMMENT: ASSERT monitor status is "%s" (reason: %s), full value is %s',
+                 status, message, json.dumps(shared.NMON_DATA))
+        assert shared.NMON_DATA['status'] == status
+
+    def assert_smon_status(self, svcname, status, message='reason'):
+        self.log('COMMENT: ASSERT smon status for svc %s is "%s" (reason: %s), full value is %s',
+                 svcname, status, message, json.dumps(shared.SMON_DATA[svcname]))
+        assert shared.SMON_DATA[svcname]['status'] == status
+
+    def assert_smon_local_expect(self, svcname, local_expect, message='reason'):
+        self.log('COMMENT: ASSERT smon local_expect for svc %s is "%s" (reason: %s), full value is %s',
+                 svcname, local_expect, message, json.dumps(shared.SMON_DATA[svcname]))
+        assert shared.SMON_DATA[svcname]['local_expect'] == local_expect
+
+    def assert_command_has_been_launched(self, call_list):
+        for call in call_list:
+            self.log('COMMENT: ASSERT call %s has been called', call)
+            assert call in self.service_command.call_args_list
+
+    def assert_command_has_not_been_launched(self, call_list):
+        for call in call_list:
+            self.log('COMMENT: ASSERT call %s has not been called', call)
+            assert call not in self.service_command.call_args_list
 
 
 @pytest.mark.ci
@@ -252,32 +292,11 @@ class TestMonitorOrchestratorStart(object):
             transition_status1,
             transition_status2,
             final_status):
-        execs = {"service_command_exe": env.Env.syspaths.true}
-        mocker.patch.object(Monitor, 'monitor_period', 0.000001)
 
-        def set_service_command_cmd(cmd):
-            execs["service_command_exe"] = cmd
-
-        def service_command_mock(*args, **kwargs):
-            log_comment('detect mock service_command(%s, %s)' % (args, kwargs))
-            return Popen(execs["service_command_exe"])
-
-        def log_comment(*args, **kwargs):
-             if monitor and getattr(monitor, 'log'):
-                 monitor.log.info(*args, **kwargs)
-             else:
-                 shared.NODE.log.info(*args, **kwargs)
-
-        def create_svc_config(s):
-            pathetc = env.Env.paths.pathetc
-            if not os.path.exists(pathetc):
-                os.mkdir(pathetc)
-            with open(os.path.join(pathetc, '%s.conf' % s), mode='w+') as svc_file:
-                svc_file.write(SERVICES[s])
-            log_comment('COMMENT: created %s service with config \n%s\n', s, SERVICES[s])
-
-        service_command = mocker.patch.object(Monitor, 'service_command', side_effect=service_command_mock)
-        monitor = Monitor()
+        monitor_test = MonitorTest(mocker=mocker, cluster_nodes=[env.Env.nodename, "node2"])
+        service_command = monitor_test.service_command_factory()
+        monitor = monitor_test.monitor
+        log = monitor_test.log
 
         if '/no node2 status/' not in title:
             monitor_node2 = deepcopy(shared.NMON_DATA)
@@ -292,61 +311,54 @@ class TestMonitorOrchestratorStart(object):
                 "services": {"status": {k: v for k, v in other_node_status.items()},  "monitor": {"status": "up"}, }
             }
         else:
-            log_comment('COMMENT: hack rejoin_grace_period to 0.001')
+            log('COMMENT: hack rejoin_grace_period to 0.001')
             monitor._lazy_rejoin_grace_period = 0.001
 
-        def do():
-            log_comment('\nCOMMENT: monitor.do()')
-            time.sleep(0.01)  # Simulate time elapsed between 2 do() iteration
-            monitor.do()
-            log_comment('COMMENT: proc ps count = %s', len(monitor.procs))
-
-        log_comment('\n===============================')
-        log_comment('COMMENT: starting test for %s', title)
-        log_comment('COMMENT: audited service "%s" expected transition 1: %s, 2: %s, final: %s',
-                    svcname, transition_status1, transition_status2, final_status)
-        create_svc_config(svcname)
-        log_comment('COMMENT: other services')
+        log('\n===============================')
+        log('COMMENT: starting test for %s', title)
+        log('COMMENT: audited service "%s" expected transition 1: %s, 2: %s, final: %s',
+                         svcname, transition_status1, transition_status2, final_status)
+        monitor_test.create_svc_config(svcname)
+        log('COMMENT: other services')
         for s in svcnames:
-            create_svc_config(s)
-        monitor._lazy_cluster_nodes = [env.Env.nodename, "node2"]
+            monitor_test.create_svc_config(s)
 
-        log_comment('COMMENT: monitor.init()')
+        log('COMMENT: monitor.init()')
         monitor.init()
         time.sleep(0.01)  # simulate time elapsed
-        assert_nmon_status(log_comment, 'init', 'after init()')
-        log_comment('COMMENT: asserting all services are refreshed: (service_command.call_args_list is correct: %s)',
-                    service_command.call_args_list[0])
+        monitor_test.assert_nmon_status('init', 'after init()')
+        log('COMMENT: asserting all services are refreshed: (service_command.call_args_list is correct: %s)',
+            service_command.call_args_list[0])
         assert service_command.call_count == 1
         for s in svcnames + [svcname, 'cluster']:
             assert s in service_command.call_args_list[0][0][0].split(',')
         assert service_command.call_args_list[0][0][1] == ['status', '--parallel', '--refresh']
         assert service_command.call_args_list[0][1] == {"local": False}
 
-        log_comment('COMMENT: create ccfg/cluster status.json')
+        log('COMMENT: create ccfg/cluster status.json')
         if not os.path.exists(svc_pathvar("ccfg/cluster")):
             os.makedirs(svc_pathvar("ccfg/cluster"))
-        open(svc_pathvar("ccfg/cluster", "status.json"), 'w').\
+        open(svc_pathvar("ccfg/cluster", "status.json"), 'w'). \
             write(json.dumps({"updated": time.time() + 1, "kind": "ccfg"}))
 
         for i in range(3):
-            do()
-            assert_nmon_status(log_comment, 'init', 'after do(), when all status.json not yet present')
+            monitor_test.do()
+            monitor_test.assert_nmon_status('init', 'after do(), when all status.json not yet present')
 
         for s in svcnames + [svcname]:
-            do()
-            assert_nmon_status(log_comment, 'init', 'after do(), when all status.json not yet present')
+            monitor_test.do()
+            monitor_test.assert_nmon_status('init', 'after do(), when all status.json not yet present')
             if "local parent is avail" in title and s in svcnames:
                 avail = "up"
             else:
                 avail = "down"
             if 'local frozen' in title and s == svcname:
                 frozen = time.time()
-                log_comment('COMMENT: create frozen flag for %s', s)
+                log('COMMENT: create frozen flag for %s', s)
                 open(svc_pathvar(s, "frozen"), 'w').close()
             else:
                 frozen = 0
-            log_comment('COMMENT: create local status.json for %s with avail: %s', s, avail)
+            log('COMMENT: create local status.json for %s with avail: %s', s, avail)
             status = deepcopy(other_node_status.get(s, {}))
             status['avail'] = avail
             status['updated'] = time.time()
@@ -357,24 +369,24 @@ class TestMonitorOrchestratorStart(object):
         svc = monitor.get_service(svcname)
         assert svc
 
-        do()
-        assert_nmon_status(log_comment, 'idle', 'after status.json created')
-        assert_smon_status(log_comment, svcname, transition_status1)
+        monitor_test.do()
+        monitor_test.assert_nmon_status('idle', 'after status.json created')
+        monitor_test.assert_smon_status(svcname, transition_status1)
 
-        log_comment('COMMENT: hack ready_period to 0.001')
+        log('COMMENT: hack ready_period to 0.001')
         monitor._lazy_ready_period = 0.001
 
         if "/start failed/" in title:
-            log_comment('COMMENT: hack service command mock to %s', env.Env.syspaths.false)
-            set_service_command_cmd(env.Env.syspaths.false)
-        do()
-        assert_smon_status(log_comment, svcname, transition_status2)
+            log('COMMENT: hack service command mock to %s', env.Env.syspaths.false)
+            monitor_test.set_service_command_cmd(env.Env.syspaths.false)
+        monitor_test.do()
+        monitor_test.assert_smon_status(svcname, transition_status2)
 
         if "/has start/" in title:
-            assert_command_has_been_launched(log_comment, [((svcname, ['start']), {}), ], service_command.call_args_list)
+            monitor_test.assert_command_has_been_launched([((svcname, ['start']), {}), ])
             if "/start failed/" not in title:
                 for svcname in [svcname]:
-                    log_comment('COMMENT: create status.json for %s with avail up', svcname)
+                    log('COMMENT: create status.json for %s with avail up', svcname)
                     status = deepcopy(other_node_status[svcname])
                     status['avail'] = "up"
                     status['updated'] = time.time()
@@ -382,24 +394,18 @@ class TestMonitorOrchestratorStart(object):
         else:
             assert ((svcname, ['start']), {}) not in service_command.call_args_list
 
-        do()
-        assert_smon_status(log_comment, svcname, final_status)
+        monitor_test.do()
+        monitor_test.assert_smon_status(svcname, final_status)
 
         for i in range(3):
-            do()
-            assert_smon_status(log_comment, svcname, final_status)
+            monitor_test.do()
+            monitor_test.assert_smon_status(svcname, final_status)
             if '/no start/' in title:
-                assert_command_has_not_been_launched(
-                    log_comment,
-                    [((svcname, ['start']), {}), ],
-                    service_command.call_args_list)
+                monitor_test.assert_command_has_not_been_launched([((svcname, ['start']), {}), ])
             if '/has start/' in title:
-                assert_command_has_been_launched(
-                    log_comment,
-                    [((svcname, ['start']), {}), ],
-                    service_command.call_args_list)
+                monitor_test.assert_command_has_been_launched([((svcname, ['start']), {}), ])
                 if "/start failed/" not in title:
-                    assert_smon_local_expect(log_comment, svcname, 'started')
+                    monitor_test.assert_smon_local_expect(svcname, 'started')
 
         if "/node frozen/" in title:
             assert monitor.node_frozen > 0
