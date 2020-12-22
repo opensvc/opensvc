c3386e30c015d9fe1eeee0f6835d6dbf2066a4bd
Cyril Galibern <cyril.galibern@opensvc.com>
2020-12-22 11:57:28 +0100

    Enable fs flag tests on FreeBSD

--- opensvc/tests/resource/fs/flag/test_fs_flag.py
+++ opensvc/tests/resource/fs/flag/test_fs_flag.py
@@ -3,7 +3,7 @@ import pytest
 from tests.helpers import assert_resource_has_mandatory_methods
 
 
-OS_LIST = {'Linux', 'SunOS', 'Darwin'}
+OS_LIST = {'Linux', 'SunOS', 'Darwin', 'FreeBSD'}
 SCENARIOS = [
     ('fs.flag', {}, 'fs.flag'),
 ]
