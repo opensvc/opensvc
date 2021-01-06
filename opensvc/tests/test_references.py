33d78aa80f883974c85593c5bc967bc6a98709d6
Cyril Galibern <cyril.galibern@opensvc.com>
2021-01-07 00:15:13 +0100

    [test] Re-enable references checks and add new examples

--- opensvc/tests/test_references.py
+++ opensvc/tests/test_references.py
@@ -1,95 +1,69 @@
-# coding: utf8
-
-from __future__ import print_function
-from __future__ import unicode_literals
-
-import socket
-import sys
+import os
 import uuid
 
-try:
-    from StringIO import StringIO
-except ImportError:
-    from io import StringIO
+import pytest
 
 from commands.svc import Mgr
-from utilities.string import try_decode
+from env import Env
 
+NODENAME = Env.nodename
 
-UNICODE_STRING = "bêh"
-NODENAME=socket.gethostname()
-SVCNAME = "unittest-" + str(uuid.uuid4())
-
-REFS = [
-    ("env.ref0", "1 2 3", "1 2 3"),
-    ("env.ref1", "{nodes}", NODENAME),
-    ("env.ref2", "{ref1}", NODENAME),
-    ("env.ref3", "$(1+2)", "3"),
-    ("env.ref4", "1", "1"),
-    ("env.ref5", "$({env.ref4}+2)", "3"),
-    ("env.ref6", "$({env.ref0[#]}+2)", "5"),
-    ("env.ref7", "{env.ref0[1]}", "2"),
+REFS = [  # (name, value, expected_evaluated_value),
+    ("ref0", "a b 33", "a b 33"),
+    ("string_index0", "{env.ref0[0]}", "a"),
+    ("string_index1", "{env.ref0[1]}", "b"),
+    ("string_index2", "{env.ref0[2]}", "33"),
+    ("string_number_of_words", "{#env.ref0}", "3"),
+    ("ref01", "{ref02}", "ref-order-safe"),
+    ("ref02", "ref-order-safe", "ref-order-safe"),
+    ("ref1", "{nodes}", NODENAME),
+    ("ref2", "{ref1}", NODENAME),
+    ("ref_add", "$(1+2)", "3"),
+    ("ref4", "1", "1"),
+    ("add_ref", "$({env.ref4}+2)", "3"),
+    # ("ref6", "$({env.ref0[#]}+2)", "5"),
+    ("add_ref_len", "$({#env.ref0}+2)", "5"),
+    ("nb", "3", "3"),
+    ("ref8", "host{1...{#nodes}}/disk{1...{nb}}", "host{1...1}/disk{1...3}"),
+    ("ref9", "{1...8}", "{1...8}"),
+    ("accept_unref", "{abcd}", "{abcd}"),
+    ("number_of_nodes", "{#nodes}", "1"),
 ]
 
-class TestReferences:
 
-    @classmethod
-    def setup_class(cls):
-        pass
-
-    @classmethod
-    def teardown_class(cls):
-        pass
-
-    def test_001_create_empty(self):
-        """
-        Create a trivial service
-        """
-        ret = Mgr()(argv=["create", "-s", SVCNAME])
-        assert ret == 0
-
-    def test_002_set_default(self):
-        """
-        Set references
-        """
-        refs = ["--kw", "nodes=%s" % NODENAME]
-        for idx, (name, val, exp_val) in enumerate(REFS):
-            refs += ["--kw", "%s=%s" % (name, val)]
-        ret = Mgr()(argv=["-s", SVCNAME, "set"] + refs)
-        assert ret == 0
-
-    def test_003_ref_0(self): self.__get_ref(0)
-    def test_003_ref_1(self): self.__get_ref(1)
-    def test_003_ref_2(self): self.__get_ref(2)
-    def test_003_ref_3(self): self.__get_ref(3)
-    def test_003_ref_4(self): self.__get_ref(4)
-    def test_003_ref_5(self): self.__get_ref(5)
-    def test_003_ref_6(self): self.__get_ref(6)
-    def test_003_ref_7(self): self.__get_ref(7)
-
-    def __get_ref(self, idx):
-        """
-        Get ref
-        """
-        name, val, exp_val = REFS[idx]
-        _stdout = sys.stdout
-
-        try:
-            out = StringIO()
-            sys.stdout = out
-            ret = Mgr()(argv=["-s", SVCNAME, "get", "--eval", "--kw", name])
-            output = out.getvalue().strip()
-        finally:
-            sys.stdout = _stdout
-        print(ret, output, exp_val)
-        assert ret == 0
-        assert try_decode(output) == exp_val
-
-    def test_004_delete(self):
-        """
-        Delete local service instance
-        """
-        ret = Mgr()(argv=["delete", "-s", SVCNAME, "--local"])
-        assert ret == 0
+@pytest.fixture(scope='function')
+def has_svc_with_ref(has_cluster_config):
+    with open(os.path.join(Env.paths.pathetc, "svc.conf"), "w") as svc_conf:
+        config_lines = [
+            '[DEFAULT]',
+            'id = ' + str(uuid.uuid4()),
+            "nodes = %s" % NODENAME,
+            '[env]'
+        ] + ["%s = %s" % (name, value) for name, value, _ in REFS]
+        svc_conf.write("\n".join(config_lines))
 
 
+@pytest.mark.ci
+@pytest.mark.usefixtures("has_svc_with_ref")
+@pytest.mark.usefixtures("has_euid_0")
+class TestReferencesConfig(object):
+    @staticmethod
+    def test_validate_config():
+        assert Mgr()(argv=["-s", "svc", "validate", "config"]) == 0
+
+
+@pytest.mark.ci
+@pytest.mark.usefixtures("has_svc_with_ref")
+@pytest.mark.usefixtures("has_euid_0")
+class TestReferencesGet(object):
+    @staticmethod
+    @pytest.mark.parametrize("name, value", [[name, value] for name, value, _ in REFS])
+    def test_can_get_native_value(capsys, name, value):
+        assert Mgr()(argv=["-s", "svc", "get", "--kw", "env.%s" % name]) == 0
+        assert capsys.readouterr().out.strip() == value
+
+    @staticmethod
+    @pytest.mark.parametrize("name, expected_eval", [[name, expected_eval] for name, _, expected_eval in REFS])
+    def test_can_get_evaluated_value(capsys, name, expected_eval):
+        assert Mgr()(argv=["-s", "svc", "get", "--eval", "--kw", "env.%s" % name]) == 0
+        assert capsys.readouterr().out.strip() == expected_eval
