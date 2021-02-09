d429b78b39b36928afea1f82eb9d4dedcadfe2f1
Cyril Galibern <cyril.galibern@opensvc.com>
2021-02-09 12:12:46 +0100

    [test] Update test helper with ANY and call imports

--- opensvc/tests/helpers.py
+++ opensvc/tests/helpers.py
@@ -1,3 +1,19 @@
+try:
+    # noinspection PyCompatibility
+    from unittest.mock import call
+except ImportError:
+    # noinspection PyUnresolvedReferences
+    from mock import call
+
+
+try:
+    # noinspection PyCompatibility
+    from unittest.mock import ANY
+except ImportError:
+    # noinspection PyUnresolvedReferences
+    from mock import ANY
+
+
 def assert_resource_has_mandatory_methods(resource):
     for method in ['start', 'stop']:
         assert callable(getattr(resource, method))
