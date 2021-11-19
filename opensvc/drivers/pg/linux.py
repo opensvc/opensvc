opensvc/opensvc

commit 91221a45aee9f68d45d4516c0d19eec82f405e05

Support cgroup fs head path on distros where unified is default
deb11
rhel9
...

opensvc/drivers/pg/linux.py
@@ -4,9 +4,17 @@ import glob
 import core.exceptions as ex
 from utilities.converters import convert_size
 
-UNIFIED_MNT = "/sys/fs/cgroup/unified"
-UNIFIED = os.path.exists(UNIFIED_MNT)
 DRIVER_BASENAME = 'pg'
+
+if os.path.exists("/sys/fs/cgroup/cgroup.procs"):
+    UNIFIED_MNT = "/sys/fs/cgroup"
+    UNIFIED = True
+elif os.path.exists("/sys/fs/cgroup/unified"):
+    UNIFIED_MNT = "/sys/fs/cgroup/unified"
+    UNIFIED = True
+else:
+    UNIFIED_MNT = "/sys/fs/cgroup/unified"
+    UNIFIED = False
 
 CONTROLLERS = [
     "blkio",
@@ -25,7 +33,7 @@ CONTROLLERS = [
 ]
 
 def get_cgroup_mntpt(t):
-    if UNIFIED and t is None:
+    if UNIFIED:
         return UNIFIED_MNT
     p = '/proc/mounts'
     if not os.path.exists(p):

commit b1a0658412dde22d951548bfb63de5bba0545059

Fix a pg regression introduced by 91221a45aee9f68d45d4516c0d19eec82f405e05
The v1/v2 mix is no longer working.

Fix that, and also initialize properly the subtree_control files when we
mkdir in v2.

opensvc/drivers/pg/linux.py
@@ -9,9 +9,6 @@ DRIVER_BASENAME = 'pg'
 if os.path.exists("/sys/fs/cgroup/cgroup.procs"):
     UNIFIED_MNT = "/sys/fs/cgroup"
     UNIFIED = True
-elif os.path.exists("/sys/fs/cgroup/unified"):
-    UNIFIED_MNT = "/sys/fs/cgroup/unified"
-    UNIFIED = True
 else:
     UNIFIED_MNT = "/sys/fs/cgroup/unified"
     UNIFIED = False
@@ -350,7 +347,10 @@ def create_cgroup(cgp, log=None):
             pass
         else:
             raise
-    set_sysfs(cgp+"/cgroup.clone_children", "1", log=log)
+    if UNIFIED:
+        set_sysfs(cgp+"/cgroup.subtree_control", "+cpuset +cpu +io +memory +pids", log=log)
+    else:
+        set_sysfs(cgp+"/cgroup.clone_children", "1", log=log)
     for parm in ("cpus", "mems"):
         parent_val = get_sysfs(cgp+"/../cpuset."+parm)
         set_sysfs(cgp+"/cpuset."+parm, parent_val, log=log)